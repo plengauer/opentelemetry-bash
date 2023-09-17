e43fe6457fa593dbcf0fd7e8ddb9e03d20ede798
plengauer <p.lengauer@gmail.com>
2023-09-17 18:56:32 +0000

    auto create spans

--- package/usr/bin/opentelemetry_bash.sh
+++ package/usr/bin/opentelemetry_bash.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 source /usr/bin/opentelemetry_bash_sdk.sh
+
+otel_init
+
+otel_span_start SERVER $(ps -p $$ -o args | grep -v COMMAND)
+
+function otel_on_script_end {
+  exit_code=$?
+  if [ "$exit_code" -ne "0" ]; then
+    otel_span_error "process.exit_code"=$exit_code
+  fi
+  otel_span_end
+  otel_shutdown
+}
+trap otel_on_script_end EXIT
