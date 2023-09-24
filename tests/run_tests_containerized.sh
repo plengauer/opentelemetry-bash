cd6c9d5437e80962ebcccb57b98f7e4d8faed52b
plengauer <p.lengauer@gmail.com>
2023-09-24 12:46:41 +0000

    added dependencies in tockerized tests

--- tests/run_tests_containerized.sh
+++ tests/run_tests_containerized.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-sudo apt-get update
-sudo apt-get remove -y containerd.io
-sudo apt-get install -y docker.io
 sudo docker build . --build-arg image=$1 --build-arg shell=$2 --tag test
 sudo docker run test --name test
 sudo docker container rm test
