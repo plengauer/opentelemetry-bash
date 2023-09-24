6332d963ec7617a0b0738033894acce6f4776148
plengauer <p.lengauer@gmail.com>
2023-09-24 12:18:55 +0000

    re-enable tests dockerized to work around github runner bug

---
+++ tests/run_tests_containerized.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+sudo docker build . --build-arg image=$1 --build-arg shell=$2 --tag test
+sudo docker run test --name test
+sudo docker container rm test
+sudo docker image rm test
