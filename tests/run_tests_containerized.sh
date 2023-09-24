9acb218c88d71ad30465d4553892ec5419078e31
plengauer <p.lengauer@gmail.com>
2023-09-24 12:32:15 +0000

    reordered steps to avoid deletion of package

--- tests/run_tests_containerized.sh
+++ tests/run_tests_containerized.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-sudo apt-get intall -y docker.io
+sudo apt-get install -y docker.io
 sudo docker build . --build-arg image=$1 --build-arg shell=$2 --tag test
 sudo docker run test --name test
 sudo docker container rm test
