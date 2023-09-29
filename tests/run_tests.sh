01fe64a401021a29083e73365376cc94d2ecc896
plengauer <p.lengauer@gmail.com>
2023-09-29 19:34:51 +0000

    use pipe to merge output of several SDKs properly

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,11 +8,12 @@ fi
 
 (while sleep 15; do pstree -a -c -p; done) &
 for file in $(find . -iname 'test_*.shell') $(find . -iname 'test_*.'$SHELL); do
-  export OTEL_TRACES_LOCATION=$(mktemp -u)
-  export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$OTEL_TRACES_LOCATION
+  export OTEL_TRACES_LOCATION=$(mktemp -u).sdk.out
+  export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u).pipe
   export OTEL_TRACES_CONSOLE_EXPORTER=TRUE
-  touch $OTEL_TRACES_LOCATION
+  mkfifo $OTEL_SHELL_SDK_OUTPUT_REDIRECT
+  cat $OTEL_SHELL_SDK_OUTPUT_REDIRECT > $OTEL_TRACES_LOCATION &
   echo "running $file"
-  timeout 60 $SHELL -x $file && echo "SUCCEEDED" || (echo "FAILED" && cat $OTEL_TRACES_LOCATION  && exit 1)
+  timeout 60 $SHELL $file && echo "SUCCEEDED" || (echo "FAILED" && cat $OTEL_TRACES_LOCATION && exit 1)
 done
 echo "ALL TESTS SUCCESSFUL"
