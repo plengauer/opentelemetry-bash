5d9775857016a0b85510f48d2b3659dea15d22bb
plengauer <p.lengauer@gmail.com>
2023-09-30 19:46:57 +0000

    more checks for subprocess attributes

--- tests/auto/test_auto_injection.zsh
+++ tests/auto/test_auto_injection.zsh
@@ -4,6 +4,7 @@
 zsh auto/fail_no_auto.shell
 assert_equals 0 $?
 sleep 3
-assert_not_equals "null" $(cat $OTEL_TRACES_LOCATION | jq '.name')
+assert_equals "zsh auto/fail_no_auto.shell" "$(cat $OTEL_TRACES_LOCATION | jq -r '.name')"
+assert_equals "SpanKind.INTERNAL" $(cat $OTEL_TRACES_LOCATION | jq -r '.kind')
 zsh auto/fail.shell 42
 assert_equals 42 $?
--- tests/auto/test_auto_instrument.sh
+++ tests/auto/test_auto_instrument.sh
@@ -11,4 +11,7 @@ assert_equals "SpanKind.INTERNAL" $(\echo "$data" | jq -r '.kind')
 assert_not_equals "null" $(\echo "$data" | jq -r '.parent_id')
 assert_equals "UNSET" $(\echo "$data" | jq -r '.status.status_code')
 assert_equals "echo hello world" "$(\echo "$data" | jq -r '.attributes."subprocess.command"')"
+assert_equals "hello world" "$(\echo "$data" | jq -r '.attributes."subprocess.command_args"')"
+# assert_equals "echo" "$(\echo "$data" | jq -r '.attributes."executable.name"')"
+# assert_equals "/usr/bin/echo" "$(\echo "$data" | jq -r '.attributes."executable.path"')" # built-in
 assert_equals "0" $(\echo "$data" | jq -r '.attributes."subprocess.exit_code"')
