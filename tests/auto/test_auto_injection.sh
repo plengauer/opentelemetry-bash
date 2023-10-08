8738c21b834eaf864255081f41dec38f473a7bb3
plengauer <p.lengauer@gmail.com>
2023-10-08 21:03:43 +0000

    fixed injection tests

--- tests/auto/test_auto_injection.sh
+++ tests/auto/test_auto_injection.sh
@@ -4,7 +4,7 @@
 sh auto/fail_no_auto.shell
 assert_equals 0 $?
 span="$(resolve_span '.resource.attributes."process.command" == "sh auto/fail_no_auto.shell"')"
-assert_equals "sh auto/fail_no_auto.shell" "$(\echo "$span" | jq -r '.name')"
+assert_equals "myspan" "$(\echo "$span" | jq -r '.name')"
 assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
 sh auto/fail.shell 42
 assert_equals 42 $?
--- tests/auto/test_auto_injection.zsh
+++ tests/auto/test_auto_injection.zsh
@@ -4,7 +4,7 @@
 zsh auto/fail_no_auto.shell
 assert_equals 0 $?
 span="$(resolve_span '.resource.attributes."process.command" == "zsh auto/fail_no_auto.shell"')"
-assert_equals "zsh auto/fail_no_auto.shell" "$(\echo "$span" | jq -r '.name')"
+assert_equals "myspan" "$(\echo "$span" | jq -r '.name')"
 assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
 zsh auto/fail.shell 42
 assert_equals 42 $?
