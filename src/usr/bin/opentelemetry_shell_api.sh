aa4b035bb48d95ab07c3b07253e1526db4680227
plengauer <p.lengauer@gmail.com>
2023-10-08 14:31:55 +0000

    fixed redirect

--- src/usr/bin/opentelemetry_shell_api.sh
+++ src/usr/bin/opentelemetry_shell_api.sh
@@ -96,7 +96,7 @@ otel_init() {
   fi
   local otel_remote_sdk_pipe=$(mktemp -u)_opentelemetry_shell_$$.pipe
   \mkfifo $otel_remote_sdk_pipe
-  \exec 3> $otel_remove_sdk_pipe
+  \exec 3> $otel_remote_sdk_pipe
   . /opt/opentelemetry_bash/venv/bin/activate
   \python3 /usr/bin/opentelemetry_shell_sdk.py $otel_remote_sdk_pipe "shell" $otel_sdk_version > $sdk_output &
   otel_sdk_pid=$!
