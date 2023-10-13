2ad301c5b5edb508e5026f034c35fc0c251cbc65
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-13 08:44:28 +0200

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,6 +10,7 @@ fi
 for file in $(find . -iname 'test_*.shell') $(find . -iname 'test_*.'$SHELL); do
   export OTEL_TRACES_LOCATION=$(mktemp -u).sdk.out
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u).pipe
+  export OTEL_SHELL_TRACES_ENABLE=TRUE
   export OTEL_TRACES_CONSOLE_EXPORTER=TRUE
   export OTEL_METRICS_CONSOLE_EXPORTER=TRUE
   export OTEL_LOGS_CONSOLE_EXPORTER=TRUE
