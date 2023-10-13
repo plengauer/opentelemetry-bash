a89bc852ddb3746135827286eeb8da6ce8ac5427
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-13 14:12:18 +0200

    Update assert.sh

--- tests/assert.sh
+++ tests/assert.sh
@@ -14,6 +14,15 @@ assert_not_equals() {
   fi
 }
 
+assert_ends_with() {
+  reverse_string=$(\echo "$2" | \rev)
+  reverse_suffix=$(\echo "$1" | \rev)
+  if [ "${reverse_string#"$reverse_suffix"}" == "$reverse_string" ] ; then
+    \echo "$1 !~= $2"
+    exit 1
+  fi
+}
+
 resolve_span() {
   local selector="$1"
   if [ -n "$selector" ]; then
