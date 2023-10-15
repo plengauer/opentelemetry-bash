55acb541c263c142958f4909059b629e09c7b41b
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-15 10:28:50 +0200

    do not prompt for install

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,8 +3,8 @@ set -e
 package=$(mktemp -u)_opentelemetry_shell.deb
 wget -O $package $(curl --no-progress-meter https://api.github.com/repos/plengauer/opentelemetry-bash/releases/latest | jq -r '.assets[].browser_download_url')
 if [ "$(whoami)" = "root" ]; then
-  apt-get install $package
+  apt-get install -y $package
 else
-  sudo apt-get install $package
+  sudo apt-get install -y $package
 fi
 rm $package
