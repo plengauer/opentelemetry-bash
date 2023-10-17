a87cc2e3323a5473e770ed89b2f39ae9fc1fd79f
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-17 13:20:37 +0200

    added shebang

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 set -e
 package=$(mktemp -u)_opentelemetry_shell.deb
 wget -O $package $(curl --no-progress-meter https://api.github.com/repos/plengauer/opentelemetry-bash/releases/latest | jq -r '.assets[].browser_download_url') || \
