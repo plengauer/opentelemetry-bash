c285d1a3300dc10caa01aa047d7b7c5abc794a92
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-17 14:22:56 +0200

    enabled metrics in tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,6 +11,7 @@ for file in $(find . -iname 'test_*.shell') $(find . -iname 'test_*.'$SHELL); do
   export OTEL_EXPORT_LOCATION=$(mktemp -u).sdk.out
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u).pipe
   export OTEL_SHELL_TRACES_ENABLE=TRUE
+  export OTEL_SHELL_METRICS_ENABLE=TRUE
   export OTEL_SHELL_LOGS_ENABLE=TRUE
   export OTEL_TRACES_CONSOLE_EXPORTER=TRUE
   export OTEL_METRICS_CONSOLE_EXPORTER=TRUE
