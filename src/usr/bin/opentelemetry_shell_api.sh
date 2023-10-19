0e326289b05e8e7f45b5371523a4952e45d8377b
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-19 11:00:48 +0200

    Update opentelemetry_shell_api.sh

--- src/usr/bin/opentelemetry_shell_api.sh
+++ src/usr/bin/opentelemetry_shell_api.sh
@@ -224,7 +224,7 @@ otel_observe() {
   local span_id=$(otel_span_start $kind "$name")
   otel_span_attribute $span_id subprocess.executable.name=$(\echo "$command" | \cut -d' ' -f1 | \rev | \cut -d'/' -f1 | \rev)
   if [ "$otel_shell" != "zsh" ]; then
-    otel_span_attribute $span_id subprocess.executable.path="$(which $(\echo "$command" | \cut -d' ' -f1))"
+    otel_span_attribute $span_id subprocess.executable.path="$(\which $(\echo "$command" | \cut -d' ' -f1))"
   fi
   otel_span_attribute $span_id subprocess.command="$command"
   otel_span_attribute $span_id subprocess.command_args="$(\echo "$command" | \cut -d' ' -f2-)"
