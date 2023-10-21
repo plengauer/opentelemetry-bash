1bf0ebeeb95f4d98016ffdec5e5886790280ba1a
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-21 12:26:10 +0200

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -49,8 +49,8 @@ otel_outstrument() {
 otel_filter_instrumentations() {
 #  if [ "-f" "$0" ] && [ "$(\grep -qF '. 
 #source ' "$0" && \echo 'TRUE' || \echo 'FALSE')" = "FALSE" ]; then
-  if [ "-f" "${BASH_SOURCE[1]}" ]; then
-    \grep -xF "$(\tr -s ' ' '\n' < "${BASH_SOURCE[1]}" | \grep -E '^[a-zA-Z0-9 ._-]*$')"
+  if [ "-f" "${BASH_SOURCE[0]}" ]; then
+    \grep -xF "$(\tr -s ' ' '\n' < "${BASH_SOURCE[0]}" | \grep -E '^[a-zA-Z0-9 ._-]*$')"
   elif [ "-f" "$0" ]; then
     \grep -xF "$(\tr -s ' ' '\n' < "$0" | \grep -E '^[a-zA-Z0-9 ._-]*$')"
   else
