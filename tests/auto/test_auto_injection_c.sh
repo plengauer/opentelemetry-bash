a331b1b541800a099b238d9e56e9dd6bc590845d
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-22 11:46:41 +0200

    Update test_auto_injection_c.sh

--- tests/auto/test_auto_injection_c.sh
+++ tests/auto/test_auto_injection_c.sh
@@ -3,6 +3,6 @@
 
 sh -c 'echo hello world'
 assert_equals 0 $?
-span="$(resolve_span '.resource.attributes."process.command" == "sh -c echo hello world -x"')"
+span="$(resolve_span '.resource.attributes."process.command" == "sh -c echo hello world"')"
 assert_equals "echo hello world" "$(\echo "$span" | jq -r '.name')"
 assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
