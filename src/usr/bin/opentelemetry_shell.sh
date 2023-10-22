c5c53681c861b5f79c95eff89ae7c5499f3c6e00
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-22 16:36:06 +0200

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -52,8 +52,8 @@ otel_outstrument() {
 
 otel_filter_instrumentations() {
   local file_hint="$1"
-  if [ "-f" "$file_hint" ]; then
-    \grep -xF "$(\tr -s ' $=";(){}[]/\\!#~^'"'" '\n' < "$file_hint" | \grep -E '^[a-zA-Z0-9 ._-]*$')"
+  if [ -f "$file_hint" ]; then
+    \grep -xF "$(\tr -s ' $=";(){}[]/\\!#~^'\' '\n' < "$file_hint" | \grep -E '^[a-zA-Z0-9 ._-]*$')"
   else
     \cat
   fi
