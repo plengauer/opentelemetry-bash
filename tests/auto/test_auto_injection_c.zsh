5969c4ed26a271e59311b42b8007a4c828e789d5
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-22 09:37:16 +0200

    Update test_auto_injection_c.zsh

--- tests/auto/test_auto_injection_c.zsh
+++ tests/auto/test_auto_injection_c.zsh
@@ -1,7 +1,7 @@
 . ./assert.sh
 . /usr/bin/opentelemetry_shell.sh
 
-zsh -c 'echo hello world' -x
+zsh -c 'echo hello world'
 assert_equals 0 $?
 span="$(resolve_span '.resource.attributes."process.command" == "zsh -c echo hello world -x"')"
 assert_equals "echo hello world" "$(\echo "$span" | jq -r '.name')"
