183d4232eca3c715c2e33f0e1c47c3f3d75f48b4
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-28 17:30:17 +0200

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,7 +7,7 @@ if [ "$SHELL" == "" ]; then
 fi
 
 (while sleep 15; do pstree -a -c -p; done) &
-for file in $(find . -iname 'test_*.shell') $(find . -iname 'test_*.'$SHELL); do
+for file in "sdk/test_sdk_init_shutdown.shell" $(find . -iname 'test_*.shell') $(find . -iname 'test_*.'$SHELL); do
   export OTEL_EXPORT_LOCATION=$(mktemp -u).sdk.out
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u).pipe
   export OTEL_SHELL_TRACES_ENABLE=TRUE
