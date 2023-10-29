c9735dddd755a0c03c545ae0790d333f51dc4c07
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-29 11:11:31 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -74,9 +74,9 @@ otel_list_path_commands() {
 otel_list_alias_commands() {
   \echo 'DEBUG' >&2
   \alias >&2
-  \alias | \cut -d' ' -f2- | \sed "s/^'\(.*\)'$/\1/" | while read alias_entry; do #   # TODO we should sed somewhere! and in sh there is no alias prefix so the split by whitespace looks fishy!
+  \alias | \cut -d' ' -f2- | while read alias_entry; do #   # TODO we should sed somewhere! and in sh there is no alias prefix so the split by whitespace looks fishy!
     local alias_key="$(\printf '%s' "$alias_entry" | \cut -d= -f1)"
-    local alias_val="$(\printf '%s' "$alias_entry" | \cut -d= -f2-)"
+    local alias_val="$(\printf '%s' "$alias_entry" | \cut -d= -f2- | \sed "s/^'\(.*\)'$/\1/")"
     # also look for this error message: xargs: unmatched single quote; by default quotes are special to xargs unless you use the -0 option
     if [ "$(\printf '%s' "$alias_val" | \tr ' ' '\n' | \grep -vP '\b(OTEL_|otel_)\w*\b' | \xargs)" != "\\$alias_key" ]; then \echo $alias_key; fi
   done
