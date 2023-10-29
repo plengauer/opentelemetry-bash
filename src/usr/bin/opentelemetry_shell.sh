fa0f9fa9eaf487b4c830a54b0c0accc5f93d5fd4
plengauer <100447901+plengauer@users.noreply.github.com>
2023-10-29 08:56:26 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -72,7 +72,7 @@ otel_list_path_commands() {
 }
 
 otel_list_alias_commands() {
-  \alias | \cut -d' ' -f2- | while read alias_entry; do #  | \sed "s/^'\(.*\)'$/\1/" # TODO we should sed somewhere! and in sh there is no alias prefix so the split by whitespace looks fishy!
+  \alias | \cut -d' ' -f2- | \sed "s/^'\(.*\)'$/\1/" | while read alias_entry; do #   # TODO we should sed somewhere! and in sh there is no alias prefix so the split by whitespace looks fishy!
     local alias_key="$(\printf '%s' "$alias_entry" | \cut -d= -f1)"
     local alias_val="$(\printf '%s' "$alias_entry" | \cut -d= -f2-)"
     # also look for this error message: xargs: unmatched single quote; by default quotes are special to xargs unless you use the -0 option
