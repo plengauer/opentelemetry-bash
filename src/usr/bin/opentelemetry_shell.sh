b782523ba41731d41eb74c17f185fb0047eb064c
plengauer <100447901+plengauer@users.noreply.github.com>
2023-11-03 11:49:42 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -293,7 +293,7 @@ otel_record_exec() {
   local span_id=$(otel_span_start INTERNAL "$command")
   otel_span_attribute $span_id code.filepath=$0
   otel_span_attribute $span_id code.lineno=$LINENO
-  if [ "$(\printf '%s' "$command" | \sed 's/ [0-9]*>.*$//')" = "exec" ]; then
+  if [ "$(\printf '%s' "$command" | \sed 's/ [0-9]*>.*$//')" != "exec" ]; then
     otel_span_activate $span_id
   fi
   otel_span_end $span_id
