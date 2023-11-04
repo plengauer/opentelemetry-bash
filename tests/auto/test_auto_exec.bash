adb4c3b4b92d8b545b1491b73390e9627c8c5a5a
plengauer <100447901+plengauer@users.noreply.github.com>
2023-11-04 10:13:12 +0100

    Update test_auto_exec.bash

--- tests/auto/test_auto_exec.bash
+++ tests/auto/test_auto_exec.bash
@@ -4,3 +4,4 @@ bash auto/exec.shell
 assert_equals 0 $?
 span="$(resolve_span '.name == "bash auto/exec.shell"')"
 assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
+resolve_span '.name == "exec echo hello world"'
