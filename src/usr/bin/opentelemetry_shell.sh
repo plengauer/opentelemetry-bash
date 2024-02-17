f272848dc9a292541c234b5538f5bd06346f12e7
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-02-17 13:14:57 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -81,9 +81,9 @@ otel_deshebangify() {
   local shebang="$(otel_shebang $1)" # e.g., "/bin/bash -x"
   if [ -z "$shebang" ]; then return 2; fi
   local shebang_cmd="$(\echo "$shebang" | \cut -d' ' -f1 | \rev | \cut -d/ -f1 | \rev)" # e.g., "bash"
-  local aliased_shebang="$(\alias $shebang_cmd 2> /dev/null | \cut -d' ' -f2- | \cut -d= -f2- | otel_unquote)" # e.g., "otel_inject_shell_with_source bash"
+  local aliased_shebang="$(\alias "$shebang_cmd" 2> /dev/null | \cut -d' ' -f2- | \cut -d= -f2- | otel_unquote)" # e.g., "otel_inject_shell_with_source bash"
   if [ -z "$aliased_shebang" ]; then return 3; fi
-  \alias $1="$aliased_shebang $(\echo $shebang | \cut -s -d ' ' -f2-) $(\which $1)"  # e.g., upgrade => otel_inject_shell_with_source bash -x /usr/bin/upgrade
+  \alias $1="$aliased_shebang $(\echo "$shebang" | \cut -s -d ' ' -f2-) $(\which $1)"  # e.g., upgrade => otel_inject_shell_with_source bash -x /usr/bin/upgrade
 }
 
 otel_instrument() {
