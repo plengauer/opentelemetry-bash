7e5ce6aff2d335d204787972646809f0566162f5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-02-17 20:40:45 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -346,7 +346,13 @@ otel_inject_sudo() {
     local executable=$1
   fi
   shift
-  otel_inject_inner_command $executable $(\printenv | \grep '^OTEL_' | otel_line_join) "$@"
+  local exit_code=0
+  export OTEL_SHELL_AUTO_INJECTED=TRUE
+  OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_SPAN_NAME_OVERRIDE="$cmdline" OTEL_SHELL_SPAN_ATTRIBUTES_OVERRIDE="$OTEL_SHELL_SPAN_ATTRIBUTES_OVERRIDE" \
+    OTEL_SHELL_SUPPRESS_LOG_COLLECTION=TRUE $executable $(\printenv | \grep '^OTEL_' | otel_line_join) sh -c ". /usr/bin/opentelemetry_shell.sh
+$(otel_escape_args "$@")" || local exit_code=$?
+  unset OTEL_SHELL_AUTO_INJECTED
+  return $exit_code
 }
 
 otel_record_exec() {
