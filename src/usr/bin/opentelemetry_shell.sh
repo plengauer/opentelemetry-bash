efe567c17d511fde5a8be5eb0de81d11660bf190
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-02-18 13:55:45 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -336,7 +336,7 @@ otel_inject_inner_command() {
     shift
   done
   local exit_code=0
-  if [ -n "$*" ]; then
+  if [ -z "$*" ]; then
     OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_SPAN_NAME_OVERRIDE="$cmdline" OTEL_SHELL_SPAN_ATTRIBUTES_OVERRIDE="$OTEL_SHELL_SPAN_ATTRIBUTES_OVERRIDE" $executable || local exit_code=$?
   else
     export OTEL_SHELL_AUTO_INJECTED=TRUE
