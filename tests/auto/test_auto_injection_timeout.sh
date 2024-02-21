993372529beb44cce71c254d05ce4999603bfe54
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-02-21 08:06:27 +0100

    merge

---
+++ tests/auto/test_auto_injection_timeout.sh
@@ -0,0 +1,16 @@
+. ./assert.sh
+. /usr/bin/opentelemetry_shell.sh
+
+timeout 60s echo hello world 1
+assert_equals 0 $?
+span="$(resolve_span '.name == "timeout 60s echo hello world 1"')"
+assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
+span="$(resolve_span '.name == "echo hello world 1"')"
+assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
+
+timeout -v 60s echo hello world 2
+assert_equals 0 $?
+span="$(resolve_span '.name == "timeout -v 60s echo hello world 2"')"
+assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
+span="$(resolve_span '.name == "echo hello world 2"')"
+assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
