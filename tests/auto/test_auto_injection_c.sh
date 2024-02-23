5aa62ad2e5c2850e5e348b2816bd17ee6f795deb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-02-23 12:41:40 +0100

    Update test_auto_injection_c.sh

--- tests/auto/test_auto_injection_c.sh
+++ tests/auto/test_auto_injection_c.sh
@@ -6,3 +6,6 @@ assert_equals 0 $?
 span="$(resolve_span '.resource.attributes."process.command" == "sh -c echo hello world"')"
 assert_equals "echo hello world" "$(\echo "$span" | jq -r '.name')"
 assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
+
+assert_equals "foo" "$(sh -c 'echo $0' 'foo' 'bar')"
+assert_equals "bar" "$(sh -c 'echo $1' 'foo' 'bar')"
