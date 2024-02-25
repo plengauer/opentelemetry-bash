2edc8f271f2eb837fd8cfe3ca3abce0760d46a7f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-02-25 21:05:08 +0100

    Update opentelemetry_shell_api.sh

--- src/usr/bin/opentelemetry_shell_api.sh
+++ src/usr/bin/opentelemetry_shell_api.sh
@@ -198,17 +198,14 @@ otel_log_record() {
 }
 
 _otel_escape() {
-  \printf 'DEBUG ARG ESCAPE? >>>%s<<<\n' "$1" >&2
-   # https://stackoverflow.com/questions/16991270/newlines-at-the-end-get-removed-in-shell-scripts-why
+   # that SO article shows why this is extra fun! https://stackoverflow.com/questions/16991270/newlines-at-the-end-get-removed-in-shell-scripts-why
   local do_escape=0
   if [ -z "$1" ]; then
     local do_escape=1
-  elif [ "$1X" != "$(\echo "$1")"X ]; then
+  elif [ "$1X" != "$(\echo "$1")"X ]; then # fancy check for "contains linefeed"
     local do_escape=1
   else
     case "$1X" in
-#      *"
-#"*) local do_escape=1 ;;
       *[[:space:]\&\<\>\|\'\"\(\)\`!\$\;]*) local do_escape=1 ;;
       *) local do_escape=0 ;;
     esac
@@ -241,9 +238,6 @@ _otel_escape_args() {
 _otel_call() {
   # old versions of dash dont set env vars properly
   # more specifically they do not make variables that are set in front of commands part of the child process env vars but only of the local execution environment
-  \printf 'DEBUG CMD %s\n' "$*" >&2
-  for arg in "$@"; do \printf 'DEBUG ARG %s\n' "$arg" >&2; done
-  \echo DEBUG "$*" '=>' "$({ \printenv; \set; } | \grep '^OTEL_' | \sort -u | \tr '\n' ' ' | _otel_escape_in)" "\\$(_otel_escape_args "$@")" >&2
   \eval "$({ \printenv; \set; } | \grep '^OTEL_' | \sort -u | \tr '\n' ' ' | _otel_escape_in)" "\\$(_otel_escape_args "$@")"
 }
 
