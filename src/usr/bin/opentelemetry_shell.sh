0495ca97ff4efa241616eaa23e0db1fb8cfa93d2
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-10 17:02:21 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -11,7 +11,7 @@ if \[ "$_otel_shell_injected" = "TRUE" ]; then
 fi
 _otel_shell_injected=TRUE
 
-. /usr/bin/opentelemetry_shell_api.sh
+\. /usr/bin/opentelemetry_shell_api.sh
 
 if \[ "$_otel_shell" = "bash" ] && \[ -n "$BASHPID" ] && \[ "$$" != "$BASHPID" ]; then
   echo "WARNING The OpenTelemetry shell file for auto-instrumentation is sourced in a subshell, automatic instrumentation will only be active within that subshell!" >&2
