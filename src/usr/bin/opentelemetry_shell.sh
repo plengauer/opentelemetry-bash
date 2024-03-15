4de228b820dcd5d4dc8f10867f3e744f364928c6
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-15 23:42:32 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -109,15 +109,14 @@ _otel_dealiasify() {
   # e.g., alias ai=bash-ai
   # e.g., alias bash-ai='/bin/bash -x /usr/bin/bash-ai'
   local cmd=$1 # e.g., "upgrade", "ai"
+echo "DEBUG DEALISIFY $cmd" >&2
+set -x
   local cmd_alias="$(_otel_resolve_alias_cmd $cmd)" # e.g., upgrade => bash, ai => bash-ai # additional indirection here needed
   if \[ -z "$cmd_alias" ]; then return 1; fi
   if \[ "$cmd" != "$cmd_alias" ] && _otel_has_alias $cmd_alias && ! _otel_resolve_alias $cmd_alias | _otel_line_split | \grep -q '^_otel_'; then # e.g., bash => no, bash-ai => yes
     # this check "feels" like there may be cases where we potentially expand aliases too much
-    # echo "DEBUG DEALISIFY $cmd" >&2
-    # set -x
     \alias $cmd="$(_otel_resolve_alias_cmd $cmd_alias) $(_otel_resolve_alias_cmd $cmd | cut -d' ' -f2-)" # e.g., alias ai='/bin/bash -x /usr/bin/bash-ai'
     _otel_dealiasify $cmd
-    # set +x
     return $?
   fi
   local cmd_aliased="$(_otel_resolve_alias $cmd_alias)" # e.g., bash => _otel_inject_shell bash
@@ -125,6 +124,7 @@ _otel_dealiasify() {
   local otel_cmds="$(\echo "$cmd_aliased" | _otel_line_split | \grep -q '^_otel_' | \grep -q -v '^_otel_observe' | _otel_line_join)" # e.g., _otel_inject_shell bash => _otel_inject_shell
   if \[ -z "$otel_cmds" ]; then return 3; fi
   _otel_alias_prepend $cmd "$otel_cmds" # e.g., alias upgrade='_otel_inject_shell /bin/bash -x /usr/bin/upgrade', e.g., alias ai='_otel_inject_shell /bin/bash -x /usr/bin/bash-ai'
+set +x
 }
 
 _otel_observe() {
