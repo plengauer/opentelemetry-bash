cf94522ba7577e31d4d351bf9e11f3183345270a
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-17 16:18:35 +0100

    Update opentelemetry_shell.custom.parallel.sh

--- src/usr/bin/opentelemetry_shell.custom.parallel.sh
+++ src/usr/bin/opentelemetry_shell.custom.parallel.sh
@@ -5,8 +5,12 @@
 # parallel -j 10 -- 'rm ./file1.txt' 'rm ./file2.txt' 'rm ./file3.txt' => parallel -j 10 -- 'sh -c \'. /otel.sh; rm -f ./file1.txt\' parallel' 'sh -c \'. /otel.sh; rm -f ./file2.txt\' parallel' 'sh -c \'. /otel.sh; rm -f ./file3.txt\' parallel'
 
 _otel_inject_parallel_moreutils_arguments() {
-  if \[ "$1" = "_otel_observe" ]; then \echo -n "$1 "; shift; fi
-  \echo -n "$1" ; shift
+  case "$1" in
+    "\\"*) \echo -n "$1";;
+    *) _otel_escape_arg "$1";;
+  esac
+  if \[ "$1" = "_otel_observe" ]; then shift; \echo -n " "; _otel_escape_arg "$1"; fi
+  shift
   local in_exec=0
   local explicit_pos=0
   local inject_all_args=0
@@ -48,8 +52,12 @@ $arg'\'' parallel'"
 # evidence this doesnt work: parallel -v sh -c 'echo $0 A$1O' parallel ':::' c1 c2 c3
 
 _otel_inject_parallel_gnu_arguments() {
-  if \[ "$1" = "_otel_observe" ]; then \echo -n "$1 "; shift; fi
-  \echo -n "$1" ; shift
+  case "$1" in
+    "\\"*) \echo -n "$1";;
+    *) _otel_escape_arg "$1";;
+  esac
+  if \[ "$1" = "_otel_observe" ]; then shift; \echo -n " "; _otel_escape_arg "$1"; fi
+  shift
   local in_exec=0
   for arg in "$@"; do
     \echo -n ' '
@@ -86,7 +94,7 @@ _otel_inject_parallel_arguments() {
 _otel_inject_parallel() {
     local cmdline="$({ set -- "$@"; if \[ "$1" = "_otel_observe" ]; then shift; fi; \echo -n "$*"; })"
     OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_SPAN_NAME_OVERRIDE="$cmdline" OTEL_SHELL_AUTO_INJECTED=TRUE OTEL_SHELL_AUTO_INSTRUMENTATION_HINT="$*" \
-      eval "$(_otel_inject_parallel_arguments "$@")"
+      \eval "$(_otel_inject_parallel_arguments "$@")"
 }
 
 _otel_alias_prepend parallel _otel_inject_parallel
