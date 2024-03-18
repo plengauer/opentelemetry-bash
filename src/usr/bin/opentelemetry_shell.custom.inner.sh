3bdaf12cf56f13bd79a6cff92fb6c3a68a353dbe
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-18 21:58:50 +0100

    Update opentelemetry_shell.custom.inner.sh

--- src/usr/bin/opentelemetry_shell.custom.inner.sh
+++ src/usr/bin/opentelemetry_shell.custom.inner.sh
@@ -26,7 +26,7 @@ _otel_inject_inner_command_args() {
   \echo -n " sh -c '. /usr/bin/opentelemetry_shell.sh
 "
   while \[ "$#" -gt 0 ]; do \echo -n " "; no_quote=1 _otel_escape_arg "$(_otel_escape_arg "$1")"; shift; done
-  \printf '%s' "' $(_otel_escape_arg "$command")"
+  \printf '%s' "' $(_otel_escape_arg "${command#\\}")"
 }
 
 _otel_inject_inner_command() {
