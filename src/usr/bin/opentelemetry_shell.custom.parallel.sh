6fcd645b019a8d3c18dd29f07dba4e6109149735
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-23 16:53:29 +0100

    Update opentelemetry_shell.custom.parallel.sh

--- src/usr/bin/opentelemetry_shell.custom.parallel.sh
+++ src/usr/bin/opentelemetry_shell.custom.parallel.sh
@@ -92,7 +92,7 @@ _otel_inject_parallel_arguments() {
 }
 
 _otel_inject_parallel() {
-    local cmdline="$({ set -- "$@"; if \[ "$1" = "_otel_observe" ]; then shift; fi; IFS=' ' \printf '%s' "$*"; })"
+    local cmdline="$({ set -- "$@"; if \[ "$1" = "_otel_observe" ]; then shift; fi; \printf '%s' "$*"; })"
     OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_SPAN_NAME_OVERRIDE="$cmdline" OTEL_SHELL_AUTO_INJECTED=TRUE OTEL_SHELL_AUTO_INSTRUMENTATION_HINT="$*" \
       \eval "$(_otel_inject_parallel_arguments "$@")"
 }
