01074b9756d14e2b6bc9ffe59b6207f00bd9a1f6
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-24 11:07:51 +0100

    Update opentelemetry_shell.custom.parallel.sh

--- src/usr/bin/opentelemetry_shell.custom.parallel.sh
+++ src/usr/bin/opentelemetry_shell.custom.parallel.sh
@@ -69,7 +69,7 @@ _otel_inject_parallel_gnu_arguments() {
     if \[ "$in_exec" -eq 0 ] && ! \[ "${arg%"${arg#?}"}" = "-" ] && \[ "$_otel_shell" = bash ] && \type "$arg" 2> /dev/null | \head -n1 | \grep -q ' function$'; then
       local in_exec=1
       local func_name=_otel_func_wrapper_"$arg"
-      \eval "$func_name"'() { \source /usr/bin/opentelemetry_shell.sh; '"$arg"' "$@" }'
+      \eval "$func_name"'() { \source /usr/bin/opentelemetry_shell.sh; '"$arg"' "$@"; }'
       \export -f "$func_name"
       no_quote=1 _otel_escape_arg "$func_name"
     elif \[ "$in_exec" -eq 1 ] && \[ "$arg" = ":::${arg#":::"}" ]; then
