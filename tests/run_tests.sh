efc029125e4c99cdd4e2e1a030f30815c34f67f7
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-24 21:23:04 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,12 +11,9 @@ for dir in unit sdk auto integration; do
   for file in $(find $dir -iname 'test_*.shell') $(find $dir -iname 'test_*.'$SHELL); do
     export OTEL_EXPORT_LOCATION=$(mktemp -u).sdk.out
     export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u).pipe
-    export OTEL_SHELL_TRACES_ENABLE=TRUE
-    export OTEL_SHELL_METRICS_ENABLE=TRUE
-    export OTEL_SHELL_LOGS_ENABLE=TRUE
-    export OTEL_TRACES_CONSOLE_EXPORTER=TRUE
-    export OTEL_METRICS_CONSOLE_EXPORTER=TRUE
-    export OTEL_LOGS_CONSOLE_EXPORTER=TRUE
+    export OTEL_TRACES_EXPORTER=console
+    export OTEL_METRICS_EXPORTER=console
+    export OTEL_LOGS_EXPORTER=console
     mkfifo $OTEL_SHELL_SDK_OUTPUT_REDIRECT
     ( while true; do cat $OTEL_SHELL_SDK_OUTPUT_REDIRECT >> $OTEL_EXPORT_LOCATION; done ) &
     echo "running $file"
