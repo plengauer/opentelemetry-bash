1a6a62b14888569cfb78a6a8c1a84fb3030ce1dd
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-25 14:40:35 +0100

    Update opentelemetry_shell.sh

--- src/usr/bin/opentelemetry_shell.sh
+++ src/usr/bin/opentelemetry_shell.sh
@@ -302,8 +302,8 @@ _otel_start_script() {
     otel_span_attribute $otel_root_span_id http.target="$SCRIPT_NAME"
     otel_span_attribute $otel_root_span_id http.url="$(\echo "$SERVER_PROTOCOL" | \cut -d / -f 1 | \tr '[:upper:]' '[:lower:]')://$SERVER_NAME:$SERVER_PORT$SCRIPT_NAME"
     otel_span_attribute $otel_root_span_id net.peer.ip="$REMOTE_ADDR"
-  elif _otel_command_self | \grep -q '/var/lib/dpkg/info' > /dev/null; then
-    local cmdline="$(_otel_command_self | \sed 's/^.* \(\/var\/lib\/dpkg\/info\/.*\)$/\1/')"
+  elif _otel_command_self | \grep -q '/var/lib/dpkg/' > /dev/null; then
+    local cmdline="$(_otel_command_self | \sed 's/^.* \(\/var\/lib\/dpkg\/.*\)$/\1/')"
     otel_root_span_id="$(otel_span_start SERVER "$(\echo "$cmdline" | \cut -d . -f 2- | \cut -d ' ' -f 1)")"
     otel_span_attribute $otel_root_span_id debian.package.name="$(\echo "$cmdline" | \rev | \cut -d / -f 1 | \rev | \cut -d . -f 1)"
     otel_span_attribute $otel_root_span_id debian.package.operation="$(\echo "$cmdline" | \cut -d . -f 2-)"
