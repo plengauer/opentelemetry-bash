122e9624c60fcf038e3fc0344002c2b1c63d55fb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-03-31 16:11:13 +0200

    Update opentelemetry_shell.custom.curl.sh

--- src/usr/bin/opentelemetry_shell.custom.curl.sh
+++ src/usr/bin/opentelemetry_shell.custom.curl.sh
@@ -3,19 +3,16 @@
 # curl -v https://www.google.at => curl -v https://www.google.at -H 'traceparent: 00-XXXXXX-01'
 
 _otel_propagate_curl() {
-  local command="$(_otel_dollar_star "$@" | \sed 's/^_otel_observe //')"
-  local url=$(\printf '%s' "$command" | \awk '{for(i=1;i<=NF;i++) if ($i ~ /^http/) print $i}')
-  local scheme=http # TODO
-  local target=$(\printf '%s' /${url#*//*/})
-  local host=$(\printf '%s' ${url} | \awk -F/ '{print $3}')
-  local method=$(\printf '%s' "$command" | \awk '{for(i=1;i<=NF;i++) if ($i == "-X") print $(i+1)}')
-  if \[ -z "$method" ]; then
-    local method=GET
-  fi
-  OTEL_SHELL_SPAN_NAME_OVERRIDE="$command" OTEL_SHELL_SPAN_KIND_OVERRIDE=CLIENT OTEL_SHELL_COMMANDLINE_OVERRIDE="$command" \
-    OTEL_SHELL_SPAN_ATTRIBUTES_OVERRIDE="http.url=$url,http.scheme=$scheme,http.host=$host,http.target=$target,http.method=$method,$OTEL_SHELL_SPAN_ATTRIBUTES_OVERRIDE" \
-    OTEL_SHELL_ADDITIONAL_ARGUMENTS_POST_0='-H' OTEL_SHELL_ADDITIONAL_ARGUMENTS_POST_1='traceparent: $OTEL_TRACEPARENT' \
-    _otel_call "$@"
+  local url=$(_otel_dollar_star "$@" | \awk '{for(i=1;i<=NF;i++) if ($i ~ /^http/) print $i}')
+  local span_id="$(otel_span_current)"
+  otel_span_attribute "$span_id" "http.url=$url"
+  otel_span_attribute "$span_id" "http.scheme=http" # TODO
+  otel_span_attribute "$span_id" "http.host=$(\printf '%s' "$url" | \cut -d / -f 3)"
+  otel_span_attribute "$span_id" "http.target=/${url#*//*/}"
+  local method="$(\printf '%s' "$command" | \awk '{for(i=1;i<=NF;i++) if ($i == "-X") print $(i+1)}')" # TODO could also be a -d to imply post, or --head"
+  otel_span_attribute "$span_id" "http.method=${method:-GET}"
+  # TODO lets check if we can read the output and parse something
+  _otel_call "$@" -H "traceparent: $OTEL_TRACEPARENT"
 }
 
-_otel_alias_prepend curl _otel_propagate_curl
+_otel_alias_prepend curl 'OTEL_SHELL_SPAN_KIND_OVERRIDE=CLIENT _otel_propagate_curl'
