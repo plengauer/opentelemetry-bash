d2d92caa9bfc8fb426f4dc96ca28c5af19a8e67e
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-01 17:09:45 +0200

    Update opentelemetry_shell.custom.curl.sh

--- src/usr/bin/opentelemetry_shell.custom.curl.sh
+++ src/usr/bin/opentelemetry_shell.custom.curl.sh
@@ -62,14 +62,14 @@ _otel_parse_curl_output() {
   local span_id="$1"
   local line="$2"
   if _otel_string_starts_with "$line" "* Connected to "; then
-    otel_span_attribute "$span_id" network.peer.address="$(\printf '%s' "$line" | \cut -d '|' -f 5 | \tr -d '()')"
-    otel_span_attribute "$span_id" network.peer.port="$(\printf '%s' "$line" | \cut -d '|' -f 7)"    
+    otel_span_attribute "$span_id" network.peer.address="$(\printf '%s' "$line" | \cut -d ' ' -f 5 | \tr -d '()')"
+    otel_span_attribute "$span_id" network.peer.port="$(\printf '%s' "$line" | \cut -d ' ' -f 7)"    
   elif _otel_string_starts_with "$line" "> HTTP/"; then
     otel_span_attribute "$span_id" http.response.status_code="$(\printf '%s' "$line" | \cut -d ' ' -f 3)"
   elif _otel_string_starts_with "$line" "{ [" && _otel_string_contains "bytes data]"; then
     otel_span_attribute "$span_id" http.response.body.size="$(\printf '%s' "$line" | \cut -d ' ' -f 2 | \tr -d '[')"
   elif _otel_string_starts_with "$(\printf '%s' "$line" | \tr '[:upper:]' '[:lower:]')" "> user-agent: "; then
-    otel_span_attribute "$span_id" user_agent.original="$(\printf '%s' "$line" | \cut -d ' ' -f 2-)"
+    otel_span_attribute "$span_id" user_agent.original="$(\printf '%s' "$line" | \cut -d ' ' -f 3-)"
   elif _otel_string_starts_with "$(\printf '%s' "$line" | \tr '[:upper:]' '[:lower:]')" "> content-length: "; then
     otel_span_attribute "$span_id" http.request.body.size="$(\printf '%s' "$line" | \cut -d ' ' -f 2-)"
   elif _otel_string_starts_with "$(\printf '%s' "$line" | \tr '[:upper:]' '[:lower:]')" "< content-length: "; then
