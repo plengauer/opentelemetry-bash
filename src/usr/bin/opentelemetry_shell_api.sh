6f678563ca48f432c636ef9984472323c57a2fc9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-04 23:26:54 +0200

    Update opentelemetry_shell_api.sh

--- src/usr/bin/opentelemetry_shell_api.sh
+++ src/usr/bin/opentelemetry_shell_api.sh
@@ -331,9 +331,9 @@ _otel_call_and_record_pipes() {
   local stderr_bytes_pid="$!"
   \wc -l < "$stderr_lines" > "$stderr_lines_result" &
   local stderr_lines_pid="$!"
-  \tee "$stdout_bytes" "$stdout_lines" < "$stdout" &
+  \tee "$stdout_bytes" "$stdout_lines" < "$stdout" 2> /dev/null &
   local stdout_pid="$!"
-  \tee "$stderr_bytes" "$stderr_lines" < "$stderr" >&2 &
+  \tee "$stderr_bytes" "$stderr_lines" < "$stderr" 2> /dev/null >&2 &
   local stderr_pid="$!"
   if \[ "$(\readlink -f /proc/self/fd/0)" = /dev/null ] || \[ "$(\readlink -f /proc/self/fd/0)" = "/proc/$$/fd/0" ]; then
     \echo -n '' > "$stdin_bytes"
