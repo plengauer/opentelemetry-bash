6a0575e3f131be0be0d938740389689d237df655
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-06 19:36:43 +0200

    Update opentelemetry_shell_api.sh

--- src/usr/bin/opentelemetry_shell_api.sh
+++ src/usr/bin/opentelemetry_shell_api.sh
@@ -212,8 +212,13 @@ otel_observe() {
   otel_span_attribute "$span_handle" shell.command_line="$command"
   if _otel_string_contains "$command" " "; then local command_name="${command%% *}"; else  local command_name="$command"; fi # "$(\printf '%s' "$command" | \cut -sd ' ' -f 2-)" # this returns the command if there are no args, its the cut -s that cant be done via expansion alone
   if \[ -z "$command_type" ]; then local command_type="$(_otel_command_type "$command_name")"; fi
-  otel_span_attribute "$span_handle" shell.command.type="$command_type"
   otel_span_attribute "$span_handle" shell.command="$command_name"
+  otel_span_attribute "$span_handle" shell.command.type="$command_type"
+  if _otel_string_contains "$command_name" / then
+    otel_span_attribute "$span_handle" shell.command.name=""${command_name##*/}""
+  else
+    otel_span_attribute "$span_handle" shell.command.name="$command_name"
+  fi
   if \[ "$command_type" = file ]; then
     local executable_path="$(_otel_string_contains "$command_name" / && \echo "$command_name" || \which "$command_name")"
     otel_span_attribute "$span_handle" subprocess.executable.path="$executable_path"
