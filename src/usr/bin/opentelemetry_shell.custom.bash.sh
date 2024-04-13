18b9493f1d27539ab4b508f40caefc71a851cb31
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-13 10:31:19 +0200

    Update opentelemetry_shell.custom.bash.sh

--- src/usr/bin/opentelemetry_shell.custom.bash.sh
+++ src/usr/bin/opentelemetry_shell.custom.bash.sh
@@ -38,12 +38,13 @@ $1"; \echo -n " "; local found_inner=1; local dollar_zero=""; break
 
 _otel_inject_shell_with_c_flag() {
   local cmdline="$(_otel_dollar_star "$@")"
+  local cmdline="${cmdline#\\}"
   # be careful about setting the instrumentation hint, setting it is only possible if its a -c invocation, not a script invocation
   # we could be safe and not set it. better have slow performance on -c injection that no spans at all from a script injection
   # we use an ugly hack here to optimize for single most common case
   if _otel_string_contains "$cmdline" " -c "; then export OTEL_SHELL_AUTO_INSTRUMENTATION_HINT="$cmdline"; fi
   local exit_code=0
-  OTEL_SHELL_AUTO_INJECTED=TRUE \eval _otel_call "$(_otel_inject_shell_args_with_c_flag "$@")" || local exit_code=$?
+  OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="$$" OTEL_SHELL_AUTO_INJECTED=TRUE \eval _otel_call "$(_otel_inject_shell_args_with_c_flag "$@")" || local exit_code=$?
   unset OTEL_SHELL_AUTO_INSTRUMENTATION_HINT
   return $exit_code
 }
