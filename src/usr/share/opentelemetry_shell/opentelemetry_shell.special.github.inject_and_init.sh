fe2e5c1b5ef73224c0d78e99bf1076d3422c8726
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-19 23:44:29 +0200

    Update opentelemetry_shell.special.github.inject_and_init.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.special.github.inject_and_init.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.special.github.inject_and_init.sh
@@ -1,7 +1,15 @@
 #!/bin/sh -e
 if [ -z "$GITHUB_ENV" ]; then exit 1; fi
 
-# start span and keep process
+root_pid_file="$(mktemp -u | rev | cut -d / -f 2- | rev)/opentelemetry_shell_$GITHUB_RUN_ID.pid"
+traceparent_file="$(mktemp)"
+sh root.sh "$traceparent_file" &
+echo "$!" > "$root_pid_file"
+
+while ! [ -f "$traceparent_file" ]; do
+  sleep 1
+done
+export OTEL_TRACEPARENT="$(cat "$traceparent_file")"
 
 printenv | grep '^OTEL_' >> "$GITHUB_ENV"
 echo "$GITHUB_ENV" | rev | cut -d / -f 3- | rev | xargs find | grep '*.sh$' | while read -r file; do
