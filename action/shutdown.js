8bfbc781e1ba4b9f0ef40f5f18c12d6d8625dffe
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-19 23:29:55 +0200

    Update shutdown.js

--- action/shutdown.js
+++ action/shutdown.js
@@ -1,1 +1,18 @@
+const { spawn } = require('child_process');
+const core = require('@actions/core');
 
+function run(executable, args = []) {
+  return new Promise((resolve, reject) => {
+    const process = spawn(executable, args);
+    process.stdout.on('data', data => console.log(data));
+    process.stderr.on('data', data => console.error(data));
+    process.on('close', code => code == 0 ? resolve(code) : reject(code));
+    process.on('error', error => reject(new Error(error)));
+  });
+}
+
+try {
+  await run('/usr/share/opentelemetry_shell/opentelemetry_shell.special.github.shutdown');
+} catch (error) {
+  core.setFailed(error.message);
+}
