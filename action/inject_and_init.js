8178e496484967da7dac6074dc6e522bbb14c54c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-19 23:25:37 +0200

    Update inject_and_init.js

--- action/inject_and_init.js
+++ action/inject_and_init.js
@@ -20,39 +20,8 @@ function run(executable, args = []) {
 }
 
 try {
-  // download and install
   await run('/bin/sh', ['-c', 'wget -O - https://raw.githubusercontent.com/plengauer/opentelemetry-bash/main/INSTALL.sh | sh -E']);
-
-  // inject
-  const script_dir = '/home/runner/work/_temp';
-  for (let file of fs.readdirSync(script_dir)) {
-    if (!file.endsWith('.sh')) continue;
-    let script = '' + fs.readFileSync(script_dir + '/' + file);
-    script = '. otel.sh\n' + script;
-    fs.writeFileSync(script_dir + '/' + file);
-  }
-
-  // init
-  // start a process
-  // init otel (and make a root span)
-  // get the traceparent of the root span
-  // make sure the process doesn die so i can find it again later in shutdown.js and end the span
-  let traceparent = '';
-
-  // setup env
-  const env_dir = script_dir + '/_runner_file_commands';
-  let env = {};
-  env['OTEL_SERVICE_NAME'] = core.getInput('service-name');
-  env['OTEL_TRACEPARENT'] = traceparent;
-  for (let file of fs.readdirSync(env_dir)) {
-    if (!file.startsWith('set_env_')) continue;
-    for (let key in env) {
-      let value = env[key];
-      fs.appendFileSync(env_dir + '/' + file, '\n' + key + '=' + value);
-    }
-  }
-  
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  await run('/usr/share/opentelemetry_shell/opentelemetry_shell.special.github.inject_and_init');
 } catch (error) {
   core.setFailed(error.message);
 }
