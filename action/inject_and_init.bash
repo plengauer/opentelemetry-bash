6aedc27c23e4dc9b91a24ec26cd989dc57502155
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-20 00:40:03 +0200

    Update inject_and_init.bash

--- action/inject_and_init.bash
+++ action/inject_and_init.bash
@@ -20,6 +20,7 @@ root4job() {
   otel_span_attribute "$span_handle" github.workflow.ref="$GITHUB_WORKFLOW_REF"
   otel_span_attribute "$span_handle" github.workflow.name="$GITHUB_WORKFLOW"
   otel_span_attribute "$span_handle" github.workflow.job.name="$GITHUB_JOB"
+  # TODO how many of the above should be resource attributes?
   dummy() { ; }
   trap dummy SIGINT
   wait
