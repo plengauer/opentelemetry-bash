9bf7a618977d121f318f491510059b4f93e986d9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-19 23:48:27 +0200

    Update opentelemetry_shell.special.github.shutdown.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.special.github.shutdown.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.special.github.shutdown.sh
@@ -1,4 +1,3 @@
+#/bin/sh -e
 root_pid_file="$(mktemp -u | rev | cut -d / -f 2- | rev)/opentelemetry_shell_$GITHUB_RUN_ID.pid"
-traceparent_file="$(mktemp)"
-sh root.sh "$traceparent_file" &
-echo "$!" > "$root_pid_file"
+kill -11 "$(cat $root_pid_file)"
