cfc63ff054d3b6f623b70f2aa6964afeeb4a5699
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-20 14:22:30 +0200

    Update inject_and_init.bash

--- actions/instrument/job/inject_and_init.bash
+++ actions/instrument/job/inject_and_init.bash
@@ -11,24 +11,10 @@ root4job() {
   traceparent_file="$1"
   . otelapi.sh
   otel_init
-  span_kind=CONSUMER
-  span_name="$GITHUB_JOB"
-  if [ -n "$OTEL_TRACEPARENT" ]; then span_kind=INTERNAL; span_name="$GITHUB_WORKFLOW / $span_name"; fi
-  span_handle="$(otel_span_start "$span_kind" "$span_name")"
+  span_handle="$(otel_span_start CONSUMER "$GITHUB_WORKFLOW / $GITHUB_JOB")"
   otel_span_activate "$span_handle"
   echo "$OTEL_TRACEPARENT" > "$traceparent_file"
   otel_span_deactivate "$span_handle"
-  otel_span_attribute "$span_handle" github.repository="$GITHUB_REPOSITORY"
-  otel_span_attribute "$span_handle" github.ref="$GITHUB_REF"
-  otel_span_attribute "$span_handle" github.actor.id="$GITHUB_ACTOR_ID"
-  otel_span_attribute "$span_handle" github.actor.name="$GITHUB_ACTOR"
-  otel_span_attribute "$span_handle" github.event.name="$GITHUB_EVENT_NAME"  
-  otel_span_attribute "$span_handle" github.workflow.run_id="$GITHUB_RUN_ID"
-  otel_span_attribute "$span_handle" github.workflow.ref="$GITHUB_WORKFLOW_REF"
-  otel_span_attribute "$span_handle" github.workflow.name="$GITHUB_WORKFLOW"
-  otel_span_attribute "$span_handle" github.job.name="$GITHUB_JOB"
-  # TODO how many of the above should be resource attributes?
-  # TODO owner name and id, github repiostory id, also add and adjust in root span detection
   trap root4job_end SIGUSR1
   while true; do sleep 60; done
 }
