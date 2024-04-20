605eb94395d1b8e61e912aa1c1e52ef9a762e1c1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-20 09:27:52 +0200

    Update shutdown.js

--- actions/instrument/job/shutdown.js
+++ actions/instrument/job/shutdown.js
@@ -3,8 +3,8 @@ const { spawn } = require('child_process');
 function run(executable, args = []) {
   return new Promise((resolve, reject) => {
     const process = spawn(executable, args);
-    process.stdout.on('data', data => console.log(data));
-    process.stderr.on('data', data => console.error(data));
+    process.stdout.on('data', data => console.log('' + data));
+    process.stderr.on('data', data => console.error('' + data));
     process.on('close', code => code == 0 ? resolve(code) : reject(code));
     process.on('error', error => reject(new Error(error)));
   });
