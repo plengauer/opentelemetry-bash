918dd4d0234d5453fcf41d883d07b0c09979ee67
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-21 10:29:11 +0200

    Update forward.c

--- actions/instrument/job/forward.c
+++ actions/instrument/job/forward.c
@@ -6,6 +6,9 @@
 #error must define executable
 #endif
 
+#define STR(s) XSTR(s)
+#define XSTR(s) #s
+
 int main(int argc, char **argv) {
     int new_argc = argc;
 #ifdef ARG1
@@ -17,19 +20,19 @@ int main(int argc, char **argv) {
     
     char **new_argv = (char**) calloc((size_t) new_argc + 1, sizeof(char*));
     int i = 0;
-    new_argv[i++] = #EXECUTABLE;
+    new_argv[i++] = STR(EXECUTABLE);
 #ifdef ARG1
-    new_argv[i++] = #ARG1;
+    new_argv[i++] = STR(ARG1);
 #endif
 #ifdef ARG2
-    new_argv[i++] = #ARG2;
+    new_argv[i++] = STR(ARG2);
 #endif
     for (int j = 1; j < argc; j++) {
         new_argv[i++] = argv[j];
     }
     new_argv[i++] = NULL;
     
-    execv(#EXECUTABLE, new_argv);
+    execv(STR(EXECUTABLE), new_argv);
     perror("execv failed");
     free(new_argv);
     return 1;
