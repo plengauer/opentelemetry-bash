c86fad1f7de7601603307cf130d4aff22a72fd37
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-21 09:42:44 +0200

    Update forward.c

--- actions/instrument/job/forward.c
+++ actions/instrument/job/forward.c
@@ -6,21 +6,21 @@
 
 int main(int argc, char **argv) {
     int new_argc = argc;
-#ifdef ARG_1
+#ifdef ARG1
     new_argc++;
 #endif
-#ifdef ARG_2
+#ifdef ARG2
     new_argc++;
 #endif
     
     char **new_argv = (char**) calloc((size_t) new_argc, sizeof(char*) + 1);
     int i = 0;
     new_argv[i++] = EXECUTABLE;
-#ifdef ARG_1
-    new_argv[i++] = ARG_1;
+#ifdef ARG1
+    new_argv[i++] = ARG1;
 #endif
-#ifdef ARG_1
-    new_argv[i++] = ARG_2;
+#ifdef ARG2
+    new_argv[i++] = ARG2;
 #endif
     for (int j = 1; j < argc; j++) {
         new_argv[i++] = argv[j];
