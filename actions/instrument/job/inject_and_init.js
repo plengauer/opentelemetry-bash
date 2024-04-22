589b503a0b929cac73c3e42f4655a37941766595
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-22 22:50:02 +0200

    Update inject_and_init.js

--- actions/instrument/job/inject_and_init.js
+++ actions/instrument/job/inject_and_init.js
@@ -2,9 +2,7 @@ const { spawn } = require('child_process');
 
 function run(executable, args = []) {
   return new Promise((resolve, reject) => {
-    const process = spawn(executable, args);
-    process.stdout.on('data', data => console.log('' + data));
-    process.stderr.on('data', data => console.error('' + data));
+    const process = spawn(executable, args, { stdio: 'inherit' });
     process.on('close', code => code == 0 ? resolve(code) : reject(code));
     process.on('error', error => reject(new Error(error)));
   });
