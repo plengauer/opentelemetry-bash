deebf888b800214b45025569418a91e5527eaa47
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 15:09:36 +0200

    Update upload_artifact.js

--- actions/instrument/workflow/upload_artifact.js
+++ actions/instrument/workflow/upload_artifact.js
@@ -1,6 +1,4 @@
 const { DefaultArtifactClient } = require('@actions/artifact');
-const path = require('path');
-const fs = require('fs');
 const artifactName = process.argv[2];
 const fullPath = process.argv[3];
 new DefaultArtifactClient().uploadArtifact(artifactName, [ fullPath ]);
