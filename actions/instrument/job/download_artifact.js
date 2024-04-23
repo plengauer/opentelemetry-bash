39946be095339951426cce36f41dab6c3d37de49
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 13:35:04 +0200

    Update download_artifact.js

--- actions/instrument/job/download_artifact.js
+++ actions/instrument/job/download_artifact.js
@@ -1,4 +1,4 @@
 const artifact = require('@actions/artifact');
 const artifactName = process.argv[2];
 const outputPath = process.argv[3];
-artifact.create().downloadArtifact(artifactName, outputPath);
+artifact.downloadArtifact(artifactName, outputPath);
