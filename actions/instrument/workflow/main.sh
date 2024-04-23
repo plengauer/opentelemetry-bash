f8193d8dd57923eebccef66ca293099236a97784
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 08:57:57 +0200

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -1,1 +1,20 @@
+set -e
 
+if [ -z "$GITHUB_ACTION_REPOSITORY" ]; then export GITHUB_ACTION_REPOSITORY="$GITHUB_REPOSITORY"; fi
+action_tag_name="$(echo "$GITHUB_ACTION_REF" | cut -sd @ -f 2-)"
+if [ -n "$action_tag_name" ]; then
+  debian_file="$(mktemp)"
+  curl "$GITHUB_API_URL"/repos/"$GITHUB_ACTION_REPOSITORY"/releases | { if [ "$action_tag_name" = main ]; then jq '.[0]'; else jq '.[] | select(.tag_name=="'"$action_tag_name"'")'; fi } | jq -r '.assets[] | .browser_download_url' | xargs wget -O "$debian_file"
+  sudo apt-get install -y "$debian_file"
+  rm "$debian_file"
+else
+  wget -O - https://raw.githubusercontent.com/"$GITHUB_ACTION_REPOSITORY"/main/INSTALL.sh | sh -E
+fi
+
+. otelapi.sh
+otel_init
+span_handle="$(otel_span_start CONSUMER "$GITHUB_WORKFLOW")"
+while [ "$(curl "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.status')" != completed ]; do sleep 1; done
+if [ "$(curl "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID" | jq -r '.conclusion')" = failure ]; then otel_span_error "$span_handle"; fi
+otel_span_end "$span_handle"
+otel_shutdown
