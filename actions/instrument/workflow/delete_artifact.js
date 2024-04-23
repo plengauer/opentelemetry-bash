fd2324c7e4c0b7fb262a89e16c99a629b2e853f7
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 11:21:23 +0200

    Update delete_artifact.js

--- actions/instrument/workflow/delete_artifact.js
+++ actions/instrument/workflow/delete_artifact.js
@@ -1,1 +1,4 @@
-const artifcat = require('@actions/artifact');
+const artifact = require('@actions/artifact');
+const artifactClient = artifact.create();
+const artifactName = process.argv[2];
+artifactClient.deleteArtifact(artifactName);
