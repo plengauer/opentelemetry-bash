f9b11b080514d3b687ae769e56686f1c892471d4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 11:24:01 +0200

    Update upload_artifact.js

--- actions/instrument/workflow/upload_artifact.js
+++ actions/instrument/workflow/upload_artifact.js
@@ -1,12 +1,10 @@
 const artifact = require('@actions/artifact');
+const path = require('path');
+const fs = require('fs');
 const artifactClient = artifact.create();
-
 const artifactName = process.argv[2];
-const file = process.argv[3];
-
-const rootDirectory = file; // adapt this if your file is not at the root
-const options = {
-  continueOnError: false
-};
-
-artifactClient.uploadArtifact(artifactName, [file], rootDirectory, options);
+const filePath = process.argv[3];
+const fullPath = path.resolve(filePath);
+const rootDirectory = path.dirname(fullPath);
+const options = { continueOnError: false };
+artifactClient.uploadArtifact(artifactName, [ fullPath ], rootDirectory, options);
