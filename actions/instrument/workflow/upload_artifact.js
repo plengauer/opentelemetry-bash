4757ea03f427677ef8415d9842f41f4942d9b59c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 14:34:48 +0200

    Update upload_artifact.js

--- actions/instrument/workflow/upload_artifact.js
+++ actions/instrument/workflow/upload_artifact.js
@@ -1,4 +1,4 @@
-const artifact = require('@actions/artifact');
+const { DefaultArtifactClient } = require('@actions/artifact');
 const path = require('path');
 const fs = require('fs');
 const artifactName = process.argv[2];
@@ -6,4 +6,4 @@ const filePath = process.argv[3];
 const fullPath = path.resolve(filePath);
 const rootDirectory = path.dirname(fullPath);
 const options = { continueOnError: false };
-artifact.create().uploadArtifact(artifactName, [ fullPath ], rootDirectory, options);
+new DefaultArtifactClient().uploadArtifact(artifactName, [ fullPath ], rootDirectory, options);
