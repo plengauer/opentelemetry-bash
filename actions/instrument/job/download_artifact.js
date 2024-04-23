0d6cb4e4e9c1687927750305c87ca1b9cbd02db9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 15:07:14 +0200

    Update download_artifact.js

--- actions/instrument/job/download_artifact.js
+++ actions/instrument/job/download_artifact.js
@@ -1,7 +1,8 @@
+const path = require('path');
 const { DefaultArtifactClient } = require('@actions/artifact');
 const artifactName = process.argv[2];
 const outputPath = process.argv[3];
 const client = new DefaultArtifactClient()
 client.listArtifacts()
   .then(response => response.artifacts.find(artifact => artifact.name == artifactName).id)
-  .then(id => client.downloadArtifact(id, { path: TODO })
+  .then(id => client.downloadArtifact(id, { path: path.dirname(outputPath) })
