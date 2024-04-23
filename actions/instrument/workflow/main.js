0bd2f707bcd70bdc3339bda3b25a18c7ff708997
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-23 08:41:29 +0200

    Create main.js

---
+++ actions/instrument/workflow/main.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process');
+
+function run(executable, args = []) {
+  return new Promise((resolve, reject) => {
+    const process = spawn(executable, args,  { stdio: 'inherit' });
+    process.on('close', code => code == 0 ? resolve(code) : reject(code));
+    process.on('error', error => reject(new Error(error)));
+  });
+}
+
+run('/bin/sh', [ '-e', __dirname + '/init.sh' ])
