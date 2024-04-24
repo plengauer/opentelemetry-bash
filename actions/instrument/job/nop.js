2c0f2f5abe3622cd95ad744a3dbd30dbbd93b7e3
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-24 06:52:30 +0200

    GitHub action must download correct otel4bash version according to its own tag

--- actions/instrument/job/nop.js
+++ actions/instrument/job/nop.js
@@ -1,5 +1,5 @@
 const process = require('process');
-if (process.env['GITHUB_REPOSITORY'] == 'plengauer/opentelemetry-bash') {
+if (!process.env['GITHUB_ACTION_REPOSITORY'] || process.env['GITHUB_REPOSITORY'] == process.env['GITHUB_ACTION_REPOSITORY']) {
   // action is local and pre steps is not supported because the action needs to be checked out first and then its too let to execute a pre step
   require('./inject_and_init.js');
 }
