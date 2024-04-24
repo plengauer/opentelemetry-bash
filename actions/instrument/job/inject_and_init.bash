918416232e8150d6a4cadd456f24f92a9f08a9bd
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-24 18:49:23 +0200

    Update inject_and_init.bash

--- actions/instrument/job/inject_and_init.bash
+++ actions/instrument/job/inject_and_init.bash
@@ -24,6 +24,7 @@ echo "$new_path_dir" >> "$GITHUB_PATH"
 
 # TODO apparently this ends without an artifact being present
 curl --no-progress-meter --fail "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq .
+curl --no-progress-meter --fail "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/artifacts | jq .
 while curl --no-progress-meter --fail "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r '.jobs[] | select(.status != "completed") | .name' | grep -q '^observe$' && curl --no-progress-meter --fail "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/artifacts | jq -r '.artifacts[].name' | grep -qv '^opentelemetry$'; do sleep 1; done
 env_dir="$(mktemp -d)"
 node download_artifact.js opentelemetry "$env_dir" || true
