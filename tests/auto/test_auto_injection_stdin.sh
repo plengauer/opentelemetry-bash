f1810f4fef30487a3ce45285ff1335c23ef53a1b
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-25 13:28:29 +0200

    Update test_auto_injection_stdin.sh

--- tests/auto/test_auto_injection_stdin.sh
+++ tests/auto/test_auto_injection_stdin.sh
@@ -3,6 +3,6 @@
 
 cat auto/fail_no_auto.shell | sh
 assert_equals 0 $?
-span="$(resolve_span '.resource.attributes."process.command_line" == "sh auto/fail_no_auto.shell"')"
+span="$(resolve_span '.resource.attributes."process.command_line" == "sh"')"
 assert_equals "myspan" "$(\echo "$span" | jq -r '.name')"
 assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
