acfeb8671b94782244cc024f1a8d5f12ec0b5db1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-26 23:54:23 +0200

    Update test_auto_cache.bash

--- tests/auto/test_auto_cache.bash
+++ tests/auto/test_auto_cache.bash
@@ -1,9 +1,13 @@
 . ./assert.sh
 
-time bash -c '. otel.sh; auto/echo_arg.shell hello world 0'
-time bash -c '. otel.sh; auto/echo_arg.shell hello world 1'
-time bash -c '. otel.sh; auto/echo_arg.shell hello world 2'
-time bash -c '. otel.sh; auto/echo_arg.shell hello world 3'
+time bash -c '. otel.sh
+bash auto/echo_arg.shell hello world 0'
+time bash -c '. otel.sh
+bash auto/echo_arg.shell hello world 1'
+time bash -c '. otel.sh
+bash auto/echo_arg.shell hello world 2'
+time bash -c '. otel.sh
+bash auto/echo_arg.shell hello world 3'
 
 span="$(resolve_span '.name == "echo hello world 0"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
