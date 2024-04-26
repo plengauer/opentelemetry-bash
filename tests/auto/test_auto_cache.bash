6d02efbb7bf08bb5b013dd63571b0e69d7ce3dab
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-26 23:46:58 +0200

    Update and rename test_auto_cache.sh to test_auto_cache.bash

--- tests/auto/test_auto_cache.bash
+++ tests/auto/test_auto_cache.bash
@@ -1,10 +1,9 @@
 . ./assert.sh
-. /usr/bin/opentelemetry_shell.sh
 
-time sh auto/echo_arg.shell hello world 0
-time sh auto/echo_arg.shell hello world 1
-time sh auto/echo_arg.shell hello world 2
-time sh auto/echo_arg.shell hello world 3
+time bash -c '. otel.sh; auto/echo_arg.shell hello world 0'
+time bash -c '. otel.sh; auto/echo_arg.shell hello world 1'
+time bash -c '. otel.sh; auto/echo_arg.shell hello world 2'
+time bash -c '. otel.sh; auto/echo_arg.shell hello world 3'
 
 span="$(resolve_span '.name == "echo hello world 0"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
