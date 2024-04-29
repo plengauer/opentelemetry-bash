dc5c03925729f507174eb34f8d4cea66d91394f0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-29 22:36:32 +0200

    Update decorate_action_node.sh

--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
+export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
 . otelapi.sh
 otel_init
-span_handle="$(otel_span_start INTERNAL "$GITHUB_WORKFLOW / $GITHUB_JOB / $GITHUB_ACTION")"
+span_handle="$(otel_span_start SERVER "$GITHUB_WORKFLOW / $GITHUB_JOB / $GITHUB_STEP")"
 otel_span_activate "$span_handle"
 exit_code=0
 otel_observe "$@" || exit_code="$?"
 if [ "$exit_code" -ne 0 ]; then otel_span_error "$span_handle"; fi
-# TODO it can exit with 0 and still fail
 otel_span_deactivate "$span_handle"
 otel_span_end "$span_handle"
 otel_shutdown
