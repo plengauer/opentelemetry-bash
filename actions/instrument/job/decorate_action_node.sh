56461ae16849e7fbe2820bc44c485787dd2cc2bd
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-30 10:44:15 +0200

    Update decorate_action_node.sh

--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$(\cat /proc/$PPID/cmdline | \tr '\000-\037' ' ' | \cut -d ' ' -f 1 | \rev | \cut -d / -f 1 | \rev)" = "Runner.Worker" ]; then
+if [ -n "$GITHUB_ACTION" ] && [ "$(\cat /proc/$PPID/cmdline | \tr '\000-\037' ' ' | \cut -d ' ' -f 1 | \rev | \cut -d / -f 1 | \rev)" = "Runner.Worker" ]; then
   export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
   . otelapi.sh
   otel_init
