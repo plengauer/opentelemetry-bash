0c929199fbd18f93dda9c3c7aecf0e80515ca5ee
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-04-30 10:43:11 +0200

    Update decorate_action_node.sh

--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
-"$@"
-exit 0
-export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
-. otelapi.sh
-otel_init
-span_handle="$(otel_span_start SERVER "$GITHUB_WORKFLOW / $GITHUB_JOB / $GITHUB_STEP")"
-otel_span_activate "$span_handle"
-exit_code=0
-otel_observe "$@" || exit_code="$?"
-if [ "$exit_code" -ne 0 ]; then otel_span_error "$span_handle"; fi
-otel_span_deactivate "$span_handle"
-otel_span_end "$span_handle"
-otel_shutdown
-exit "$exit_code"
+if [ "$(\cat /proc/$PPID/cmdline | \tr '\000-\037' ' ' | \cut -d ' ' -f 1 | \rev | \cut -d / -f 1 | \rev)" = "Runner.Worker" ]; then
+  export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
+  . otelapi.sh
+  otel_init
+  span_handle="$(otel_span_start SERVER "$GITHUB_WORKFLOW / $GITHUB_JOB / $GITHUB_STEP")"
+  otel_span_activate "$span_handle"
+  exit_code=0
+  otel_observe "$@" || exit_code="$?"
+  if [ "$exit_code" -ne 0 ]; then otel_span_error "$span_handle"; fi
+  otel_span_deactivate "$span_handle"
+  otel_span_end "$span_handle"
+  otel_shutdown
+  exit "$exit_code"
+else
+  exec "$@"
+fi
