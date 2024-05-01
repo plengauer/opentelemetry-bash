5fe97e7fea584c2d9dad131d17e13c8377846fa1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-01 07:59:37 +0200

    Update decorate_action_node.sh

--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo "$*" >&2
-exec "$@"
+echo "+ $*" >&2
+"$@"
+exit_code="$?"
+echo "- $* => $exit_code" >&2
+exit "$exit_code"
+
 #if [ -n "$GITHUB_ACTION" ] && [ "$(\cat /proc/$PPID/cmdline | \tr '\000-\037' ' ' | \cut -d ' ' -f 1 | \rev | \cut -d / -f 1 | \rev)" = "Runner.Worker" ]; then
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT=/dev/null # not necessary probably
   export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs 2> /dev/null | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
