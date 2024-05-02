bf4575190aaa4f01316e4d0726bab67c1c207ffa
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-02 09:07:56 +0200

    Update decorate_action_run.sh

--- actions/instrument/job/decorate_action_run.sh
+++ actions/instrument/job/decorate_action_run.sh
@@ -2,7 +2,7 @@
 set -e
 # sh|dash|bash -e /.../*.sh
 if [ -n "$GITHUB_RUN_ID" ] && [ -f "$3" ] && [ "$(echo "$3" | rev | cut -d . -f 1 | rev)" = "sh" ] && [ "$(echo "$GITHUB_ENV" | rev | cut -d / -f 3- | rev)" = "$(echo "$3" | rev | cut -d / -f 2- | rev)" ]; then
-  sleep 10
+  sleep 60
   export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors -H 'Cache-Control: no-cache' "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
   file="$3"
   script="$(cat "$file")"
