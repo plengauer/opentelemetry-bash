2e76887cad8835813098f2388e063381f97ca7b5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-08 20:17:05 +0200

    Update opentelemetry_shell.custom.node.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
@@ -1,1 +1,20 @@
+const child_process = require('child_process');
+const _spawn = child_process.spawn;
+const _exec = child_process.exec;
+const _execFile = child_process.execFile;
 
+child_process.spawn = function(command, args, options) {
+  args = args ?? [];
+  args = [ '-e', '-c', '. otel.sh\n' + command + ' "$@"', 'node', ...args];
+  return _spawn('/bin/sh', args, options);
+}
+
+child_process.exec = function(command, options, callback) {
+  return _exec('. otel.sh\n' + command, options, callback);
+}
+
+child_process.execFile = function(file, args, options, callback) {
+  args = args ?? [];
+  args = [ '-e', '-c', '. otel.sh\n' + file + ' "$@"', 'node', ...args];
+  return execFile('/bin/sh', args, options, callback);
+}
