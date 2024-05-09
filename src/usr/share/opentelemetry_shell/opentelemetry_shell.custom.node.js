da578be8e214dcb030c67ec873ca030b0570a10c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-09 08:09:31 +0200

    Update opentelemetry_shell.custom.node.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
@@ -1,25 +1,29 @@
 const child_process = require('node:child_process');
 const _spawn = child_process.spawn;
 const _exec = child_process.exec;
-const _execFile = child_process.execFile;
+const _execFile = child_process.execFile;+
+
+// TODO commandline override? here or in the node call?
 
 child_process.spawn = function(command, args, options) {
   args = [ '-e', '-c', '. otel.sh\n' + command + ' "$@"', 'node'].concat(args ?? []);
-  options = options ?? {};
-  options.env = options.env ?? { ... options.env };
-  options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
+  // options = options ?? {};
+  // options.env = options.env ?? { ... options.env };
+  // options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
   return _spawn('/bin/sh', args, options);
 }
 
 child_process.exec = function(command, options, callback) {
-  options = options ?? {};
-  options.env = options.env ?? { ... options.env };
-  options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
+  // options = options ?? {};
+  // options.env = options.env ?? { ... options.env };
+  // options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
   return _exec('. otel.sh\n' + command, options, callback);
 }
 
+/*
 child_process.execFile = function(file, args, options, callback) {
   args = args ?? [];
   args = [ '-e', '-c', '. otel.sh\n' + file + ' "$@"', 'node'].concat(args);
   return _execFile('/bin/sh', args, options, callback);
 }
+*/
