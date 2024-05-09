be67e96eb31ecd45c6c978a2dcb4ff24031a90da
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-09 09:22:22 +0200

    Update decorate_action_node.sh

--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -2,7 +2,7 @@
 # need to use bash over sh, because it propagates invalid env vars correctly (env vars with dashes in keys)
 # export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs 2> /dev/null | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
 . otelapi.sh
-. /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
+eval "$(cat /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh | grep -v '^_otel_prepend_alias ')"
 otel_init
 span_handle="$(otel_span_start INTERNAL "$GITHUB_WORKFLOW / $GITHUB_JOB / $GITHUB_ACTION")"
 otel_span_activate "$span_handle"
