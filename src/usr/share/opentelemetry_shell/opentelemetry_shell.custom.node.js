29c9787cb079cd8f3b513f5d1c993769d288f297
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-09 08:30:53 +0200

    Update opentelemetry_shell.custom.node.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
@@ -4,19 +4,20 @@ const _exec = child_process.exec;
 const _execFile = child_process.execFile;+
 
 // TODO commandline override? here or in the node call?
+// TODO make sure this can really be called in every possible way!
 
 child_process.spawn = function(command, args, options) {
   args = [ '-c', '. otel.sh\n' + command + ' "$@"', 'node'].concat(args ?? []);
-  // options = options ?? {};
-  // options.env = options.env ?? { ... options.env };
-  // options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
+  options = options ?? {};
+  options.env = options.env ?? { ... process.env };
+  options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
   return _spawn('/bin/sh', args, options);
 }
 
 child_process.exec = function(command, options, callback) {
-  // options = options ?? {};
-  // options.env = options.env ?? { ... options.env };
-  // options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
+  options = options ?? {};
+  options.env = options.env ?? { ... process.env };
+  options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command;
   return _exec('. otel.sh\n' + command, options, callback);
 }
 
