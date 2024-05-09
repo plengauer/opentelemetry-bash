d62cb211d839ea5b346bb01fc340f7342ba29256
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-09 09:08:40 +0200

    Update opentelemetry_shell.custom.node.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
@@ -3,7 +3,7 @@
 _otel_inject_node() {
   # TODO lets make it super dirty for now!
   shift # TODO lets assume first arg is simply "node"
-  _otel_call \\node --require /usr/share/opentelemetry_shell.custom.node.js "$@"
+  _otel_call \\node --require /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js "$@"
 }
 
 _otel_alias_prepend node _otel_inject_node
