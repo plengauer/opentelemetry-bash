d9f335b44046e0900ced1aeac8c0162ea03bd689
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-11 14:40:49 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -1,1 +1,34 @@
+#!/bin/false
 
+_otel_inject_docker_args() {
+  _otel_escape_arg "$1"
+  shift
+  while \[ "$#" -gt 0 ] && _otel_string_starts_with "$1" -; do
+    echo -n ' '
+    _otel_escape_arg "$1"
+    if _otel_string_contains "$1" =; then
+      shift
+      echo -n ' '
+      _otel_escape_arg "$1"
+    fi
+    shift
+  done
+  local command="$1"
+  echo -n ' '
+  _otel_escape_arg "$1"
+  shift
+  if \[ "$command" = run ]; then
+    _otel_escape_arg " --env OTEL_TRACEPARENT=$OTEL_TRACEPARENT"
+  fi
+  while \[ "$#" -gt 0 ]; do
+    echo -n ' '
+    _otel_escape_arg "$1"
+    shift
+  done
+}
+
+_otel_inject_docker() {
+  \eval _otel_call "$(_otel_inject_docker_args "$@")"
+}
+
+_otel_alias_prepend docker _otel_inject_docker
