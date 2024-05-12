872e3801721e9991b46ed2eed7d8821475cc271d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-12 08:58:33 +0200

    Inject into children of node

---
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
@@ -0,0 +1,11 @@
+#!/bin/false
+
+_otel_inject_node() {
+  local cmdline="$(_otel_dollar_star "$@")"
+  local cmdline="${cmdline#\\}"
+  local command="$1"
+  shift
+  OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE _otel_call "$command" --require /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js "$@"
+}
+
+_otel_alias_prepend node _otel_inject_node
