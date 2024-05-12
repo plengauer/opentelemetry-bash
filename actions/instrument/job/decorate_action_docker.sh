2eeb0e77d19ff5942444d836613b2b87a36756e8
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-12 11:06:23 +0200

    Update decorate_action_docker.sh

--- actions/instrument/job/decorate_action_docker.sh
+++ actions/instrument/job/decorate_action_docker.sh
@@ -2,7 +2,7 @@
 if [ "$(cat /proc/$PPID/cmdline | tr '\000-\037' ' ' | cut -d ' ' -f 1 | rev | cut -d / -f 1 | rev)" != "Runner.Worker" ]; then exec "$@"; fi
 # export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs 2> /dev/null | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
 . otelapi.sh
-eval "$(cat /usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh | grep -v '^_otel_prepend_alias ')"
+eval "$(cat /usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh | grep -v '_otel_prepend_alias ')"
 otel_init
 span_handle="$(otel_span_start INTERNAL "$GITHUB_WORKFLOW / $GITHUB_JOB / $GITHUB_ACTION")"
 otel_span_activate "$span_handle"
