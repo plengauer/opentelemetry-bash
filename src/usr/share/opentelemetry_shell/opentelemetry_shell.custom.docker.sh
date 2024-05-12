faa244749089f66047ac5ab889981ff1c2c9a1c0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-12 10:56:41 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -4,24 +4,25 @@ _otel_inject_docker_args() {
   _otel_escape_arg "$1"
   shift
   while \[ "$#" -gt 0 ] && _otel_string_starts_with "$1" -; do
-    echo -n ' '
+    \echo -n ' '
     _otel_escape_arg "$1"
     if ! _otel_string_contains "$1" =; then
       shift
-      echo -n ' '
+      \echo -n ' '
       _otel_escape_arg "$1"
     fi
     shift
   done
   local command="$1"
-  echo -n ' '
+  \echo -n ' '
   _otel_escape_arg "$1"
   shift
   if \[ "$command" = run ]; then
-    _otel_escape_arg " --env OTEL_TRACEPARENT=$OTEL_TRACEPARENT"
+    \echo -n ' '
+    _otel_escape_arg "--env OTEL_TRACEPARENT=$OTEL_TRACEPARENT"
   fi
   while \[ "$#" -gt 0 ]; do
-    echo -n ' '
+    \echo -n ' '
     _otel_escape_arg "$1"
     shift
   done
