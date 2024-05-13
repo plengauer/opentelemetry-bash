6cb546497e135edf26f1113af05baa26a3dfa1da
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-13 08:16:16 +0200

    Use bash instead of sh to decorate node actions

--- actions/instrument/job/decorate_action_docker.sh
+++ actions/instrument/job/decorate_action_docker.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+# need to use bash over sh, because it propagates invalid env vars correctly (env vars with dashes in keys)
 if [ "$(cat /proc/$PPID/cmdline | tr '\000-\037' ' ' | cut -d ' ' -f 1 | rev | cut -d / -f 1 | rev)" != "Runner.Worker" ]; then exec "$@"; fi
 # export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs 2> /dev/null | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
 . otelapi.sh
