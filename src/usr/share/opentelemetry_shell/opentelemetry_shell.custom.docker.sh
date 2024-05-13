59dd7775afd8f9477319d1f76892dec7595e5adf
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-13 15:43:30 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -31,8 +31,8 @@ _otel_inject_docker_args() {
   # extract image
   local image="$1"
   if \[ "$command" = run ]; then
-    for kvp in $(\printenv | \grep '^OTEL_' | cut -d = -f 1); do \echo -n ' '; _otel_escape_args --env "$kvp"; done
-    for file in $(\dpkg -L opentelemetry-shell | grep opentelemetry_shell); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
+    for kvp in $(\printenv | \grep '^OTEL_' | \cut -d = -f 1); do \echo -n ' '; _otel_escape_args --env "$kvp"; done
+    for file in $(\dpkg -L opentelemetry-shell | \grep opentelemetry_shell); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target="/opt/opentelemetry_shell/pipe"
     \echo -n ' '; _otel_escape_args --env "OTEL_SHELL_AUTO_INJECTED=TRUE"
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
