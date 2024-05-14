fc6ba9678197bd99b7577a321635cd7a8b8ec0bd
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-14 15:19:26 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -30,7 +30,7 @@ _otel_inject_docker_args() {
   local image="$1"
   if \[ "$command" = run ] && \docker run --rm --entrypoint cat "$image" /etc/os-release | \grep -E '^NAME=' | \grep -qE 'Debian|Ubuntu|Alpine Linux'; then
     for kvp in $(\printenv | \grep '^OTEL_' | \cut -d = -f 1); do \echo -n ' '; _otel_escape_args --env "$kvp"; done
-    for file in $(\dpkg -L opentelemetry-shell | \grep opentelemetry_shell); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
+    for file in $(\dpkg -L opentelemetry-shell | \grep -vE '^usr$' | \grep -vE '^usr/share$' | \grep -vE '^opt$'); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target="/tmp/opentelemetry_shell.global.pipe"
     \echo -n ' '; _otel_escape_args --env "OTEL_REMOTE_SDK_PIPE=/tmp/opentelemetry_shell.global.pipe"
     \echo -n ' '; _otel_escape_args --mount type=bind,source="/tmp",target="/tmp" # TODO use TMPDIR?, also this is a huge security risk!
