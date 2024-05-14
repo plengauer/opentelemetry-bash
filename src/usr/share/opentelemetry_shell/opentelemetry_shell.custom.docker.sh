b734f3ccf0c791468ef289e4f39a8acf71a98b34
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-14 20:10:11 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -32,7 +32,7 @@ _otel_inject_docker_args() {
     for kvp in $(\printenv | \grep '^OTEL_' | \cut -d = -f 1); do \echo -n ' '; _otel_escape_args --env "$kvp"; done
     for file in $(\dpkg -L opentelemetry-shell | \grep -vE '^/.$' | \grep -vE '^/usr$' | \grep -vE '^/usr/bin$' | \grep -vE '^/usr/share$' | \grep -vE '^opt$'); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     \echo -n ' '; _otel_escape_args --mount type=bind,source=/tmp,target=/tmp # TODO use TMPDIR?, also this is a huge security risk!
-    \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE="$_otel_remote_sdk_pipe"; \chmod +w "$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE="$_otel_remote_sdk_pipe"; \chmod a+w "$_otel_remote_sdk_pipe"
     \echo -n ' '; _otel_escape_args --env OTEL_SHELL_AUTO_INJECTED=TRUE
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
     \echo -n ' '; _otel_escape_arg "$1"; shift
