39f89a24dc125bc7583f4b290fc667861eaedf47
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-15 11:21:07 +0200

    Update opentelemetry_shell.custom.node.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
@@ -7,7 +7,6 @@ const _execFile = child_process.execFile;
 if (process.platform != 'linux') return;
 
 child_process.spawn = function(command, args, options) {
-  console.error('DEBUG ' + command);
   if (args && !options && !Array.isArray(args)) {
     options = args;
     args = [];
@@ -15,30 +14,31 @@ child_process.spawn = function(command, args, options) {
   options = options ?? {};
   options.env = options.env ?? { ... process.env };
   options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command + ' ';
+  if (command.includes('/')) command = 'otel_observe ' + command;
   if (options.shell) {
     if (typeof options.shell == 'boolean') options.shell = '/bin/sh';
     options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE'] = options.shell + ' -c ' + command + ' ' + args.join(' ');
     options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE'] = process.pid;
     options.env['OTEL_SHELL_AUTO_INJECTED'] = 'FALSE'
-    return _spawn(options.shell, [ '-c', 'printenv\n. otel.sh\n' + command + ' "$@"', options.shell ].concat(args ?? []), { ... options, shell: false });
+    return _spawn(options.shell, [ '-c', '. otel.sh\n' + command + ' "$@"', options.shell ].concat(args ?? []), { ... options, shell: false });
   } else {
-    return _spawn('/bin/sh', [ '-c', 'printenv\n. otel.sh\n' + command + ' "$@"', 'node' ].concat(args ?? []), options);
+    return _spawn('/bin/sh', [ '-c', '. otel.sh\n' + command + ' "$@"', 'node' ].concat(args ?? []), options);
   }
 }
 
 child_process.exec = function(command, options, callback) {
-  console.error('DEBUG ' + command);
   if (options && !callback && typeof options == 'function') {
     callback = options;
     options = {};
   }
+  if (command.includes('/')) command = 'otel_observe ' + command;
   options = options ?? {};
   options.env = options.env ?? { ... process.env };
   options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command + ' ';
   options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE'] = (options.shell ?? '/bin/sh') + ' -c ' + command;
   options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE'] = process.pid;
   options.env['OTEL_SHELL_AUTO_INJECTED'] = 'FALSE'
-  return _exec('printenv\n. otel.sh\n' + command, options, callback);
+  return _exec('. otel.sh\n' + command, options, callback);
 }
 
 /*
