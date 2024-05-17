67e54932d33e78ff7fbe3a7734751a5ed3a82031
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-17 10:53:52 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -31,12 +31,12 @@ _otel_inject_docker_args() {
     for file in $(\dpkg -L opentelemetry-shell | \grep -vE '^/.$' | \grep -vE '^/usr$' | \grep -vE '^/usr/bin$' | \grep -vE '^/usr/share$' | \grep -vE '^/opt/'); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     # \echo -n ' '; _otel_escape_args --mount type=bind,source=/tmp,target=/tmp # TODO use TMPDIR?, also this is a huge security risk!
     # \echo -n ' '; _otel_escape_args --user "$(id -u)" # TODO we shouldnt override the user, we should chmod the pipe, but for some reason it doesnt work \chmod 666 "$_otel_remote_sdk_pipe"
-    \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target=/var/opentelemetry_shell/"$_otel_remote_sdk_pipe"
-    \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE=/opt/opentelemetry_shell/"$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target=/var/opentelemetry_shell"$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE=/var/opentelemetry_shell"$_otel_remote_sdk_pipe"
     \echo -n ' '; _otel_escape_args --env OTEL_SHELL_AUTO_INJECTED=TRUE    
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
     \echo -n ' '; _otel_escape_arg "$1"; shift
-    \echo -n ' '; _otel_escape_args -xc ". otel.sh
+    \echo -n ' '; _otel_escape_args -c ". otel.sh
 $(\docker inspect "$image" | \jq -r '.[0].Config.Entrypoint[]' | _otel_line_join) "'"$@"' sh
     if \[ "$#" = 0 ]; then \echo -n ' '; \docker inspect "$image" | \jq -r '.[0].Config.Cmd[]?' | _otel_escape_stdin; fi
   else
