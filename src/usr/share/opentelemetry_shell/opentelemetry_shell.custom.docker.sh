cc5521ed417b2a77b92a6f7166117afc765a848c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-17 10:29:07 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -32,10 +32,10 @@ _otel_inject_docker_args() {
     for kvp in $(\printenv | \grep '^OTEL_' | \cut -d = -f 1); do \echo -n ' '; _otel_escape_args --env "$kvp"; done
     for file in $(\dpkg -L opentelemetry-shell | \grep -vE '^/.$' | \grep -vE '^/usr$' | \grep -vE '^/usr/bin$' | \grep -vE '^/usr/share$' | \grep -vE '^/opt/'); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     # \echo -n ' '; _otel_escape_args --mount type=bind,source=/tmp,target=/tmp # TODO use TMPDIR?, also this is a huge security risk!
-    \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target="$_otel_remote_sdk_pipe"
-    \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE="$_otel_remote_sdk_pipe"
-    \echo -n ' '; _otel_escape_args --env OTEL_SHELL_AUTO_INJECTED=TRUE
-    \echo -n ' '; _otel_escape_args --user "$(id -u)" # TODO we shouldnt override the user, we should chmod the pipe, but for some reason it doesnt work \chmod 666 "$_otel_remote_sdk_pipe"
+    # \echo -n ' '; _otel_escape_args --user "$(id -u)" # TODO we shouldnt override the user, we should chmod the pipe, but for some reason it doesnt work \chmod 666 "$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target=/opt/opentelemetry_shell/"$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE=/opt/opentelemetry_shell/"$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --env OTEL_SHELL_AUTO_INJECTED=TRUE    
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
     \echo -n ' '; _otel_escape_arg "$1"; shift
     \echo -n ' '; _otel_escape_args -xc ". otel.sh
