5b0c2a3498e829b02121706e4da52796df172e54
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-17 12:24:44 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -36,8 +36,10 @@ _otel_inject_docker_args() {
     \echo -n ' '; _otel_escape_args --env OTEL_SHELL_AUTO_INJECTED=TRUE    
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
     \echo -n ' '; _otel_escape_arg "$1"; shift
+    local entrypoint="$(\docker inspect "$image" | \jq -r '.[0].Config.Entrypoint[]?' | _otel_line_join)"
+    if [ "-z" "$entrypoint" ]; then local entrypoint="/bin/sh -c"; fi
     \echo -n ' '; _otel_escape_args -c ". otel.sh
-$(\docker inspect "$image" | \jq -r '.[0].Config.Entrypoint[]' | _otel_line_join) "'"$@"' sh
+$entrypoint "'"$@"' sh
     if \[ "$#" = 0 ]; then \echo -n ' '; \docker inspect "$image" | \jq -r '.[0].Config.Cmd[]?' | _otel_escape_stdin; fi
   else
     \echo -n ' '; _otel_escape_arg "$1"; shift
