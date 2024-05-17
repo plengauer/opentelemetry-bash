7087626e6ab681347ecb92fb8bd5044f707d7741
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-17 10:33:49 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -1,7 +1,5 @@
 #!/bin/false
 
-# TODO make it experimental configurable
-
 _otel_inject_docker_args() {
   # docker command
   _otel_escape_arg "$1"; shift
@@ -28,12 +26,12 @@ _otel_inject_docker_args() {
   done
   # extract image
   local image="$1"
-  if \[ "$command" = run ] && \docker run --rm --entrypoint cat "$image" /etc/os-release | \grep -E '^NAME=' | \grep -qE 'Debian|Ubuntu|Alpine Linux'; then
+  if \[ "$OTEL_SHELL_EXPERIMENTAL_INJECT_CONTAINERS" = TRUE ] && \[ "$command" = run ] && \docker run --rm --entrypoint cat "$image" /etc/os-release | \grep -E '^NAME=' | \grep -qE 'Debian|Ubuntu|Alpine Linux'; then
     for kvp in $(\printenv | \grep '^OTEL_' | \cut -d = -f 1); do \echo -n ' '; _otel_escape_args --env "$kvp"; done
     for file in $(\dpkg -L opentelemetry-shell | \grep -vE '^/.$' | \grep -vE '^/usr$' | \grep -vE '^/usr/bin$' | \grep -vE '^/usr/share$' | \grep -vE '^/opt/'); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     # \echo -n ' '; _otel_escape_args --mount type=bind,source=/tmp,target=/tmp # TODO use TMPDIR?, also this is a huge security risk!
     # \echo -n ' '; _otel_escape_args --user "$(id -u)" # TODO we shouldnt override the user, we should chmod the pipe, but for some reason it doesnt work \chmod 666 "$_otel_remote_sdk_pipe"
-    \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target=/opt/opentelemetry_shell/"$_otel_remote_sdk_pipe"
+    \echo -n ' '; _otel_escape_args --mount type=bind,source="$_otel_remote_sdk_pipe",target=/var/opentelemetry_shell/"$_otel_remote_sdk_pipe"
     \echo -n ' '; _otel_escape_args --env OTEL_REMOTE_SDK_PIPE=/opt/opentelemetry_shell/"$_otel_remote_sdk_pipe"
     \echo -n ' '; _otel_escape_args --env OTEL_SHELL_AUTO_INJECTED=TRUE    
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
