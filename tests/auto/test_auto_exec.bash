f56def5e29c1c311d5ad9127357d762cd036e274
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-18 13:22:16 +0200

    Update test_auto_exec.bash

--- tests/auto/test_auto_exec.bash
+++ tests/auto/test_auto_exec.bash
@@ -14,7 +14,7 @@ assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
 span="$(resolve_span '.name == "echo hello world 1"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
 
-OPEN_FD=TRUE bash auto/exec.shell hello world 2
+OPEN_FD=TRUE bash -x auto/exec.shell hello world 2
 assert_equals 0 $?
 span="$(resolve_span '.name == "bash auto/exec.shell hello world 2"')"
 assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
