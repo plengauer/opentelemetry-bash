bf790217cc8386f73c2c29c0d9abaca011c43f05
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-19 12:41:37 +0200

    Update test_auto_exec.bash

--- tests/auto/test_auto_exec.bash
+++ tests/auto/test_auto_exec.bash
@@ -2,28 +2,20 @@
 
 bash auto/exec.shell hello world 0
 assert_equals 0 $?
-span="$(resolve_span '.name == "bash auto/exec.shell hello world 0"')"
-assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
 span="$(resolve_span '.name == "echo hello world 0"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
 
 SOURCE=TRUE bash auto/exec.shell hello world 1
 assert_equals 0 $?
-span="$(resolve_span '.name == "bash auto/exec.shell hello world 1"')"
-assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
 span="$(resolve_span '.name == "echo hello world 1"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
 
 OPEN_FD=TRUE bash auto/exec.shell hello world 2
 assert_equals 0 $?
-span="$(resolve_span '.name == "bash auto/exec.shell hello world 2"')"
-assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
 span="$(resolve_span '.name == "echo hello world 2"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
 
 SOURCE=TRUE OPEN_FD=TRUE bash auto/exec.shell hello world 3
 assert_equals 0 $?
-span="$(resolve_span '.name == "bash auto/exec.shell hello world 3"')"
-assert_equals "SpanKind.SERVER" $(\echo "$span" | jq -r '.kind')
 span="$(resolve_span '.name == "echo hello world 3"')"
 assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
