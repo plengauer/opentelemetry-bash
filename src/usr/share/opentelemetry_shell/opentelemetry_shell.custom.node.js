eddfe5ff3b0178e8704ef8736f67db0688e57021
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-20 12:12:21 +0200

    Update opentelemetry_shell.custom.node.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
@@ -1,12 +1,23 @@
 const process = require('node:process');
 const child_process = require('node:child_process');
 const _spawn = child_process.spawn;
+const _spawnSync = child_process.spawnSync;
 const _exec = child_process.exec;
+const _execSync = child_process.execSync;
 const _execFile = child_process.execFile;
+const _execFileSync = child_process.execFileSync;
 
 if (process.platform != 'linux') return;
 
 child_process.spawn = function(command, args, options) {
+  return otel_spawn(command, args, options, _spawn);
+}
+
+child_process.spawnSync = function(command, args, options) {
+  return otel_spawn(command, args, options, _spawnSync);
+}
+
+function otel_spawn(command, args, options, original) {
   if (args && !options && !Array.isArray(args)) {
     options = args;
     args = [];
@@ -15,32 +26,52 @@ child_process.spawn = function(command, args, options) {
   options = options ?? {};
   options.env = options.env ?? { ... process.env };
   options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command + ' ';
-  if (command.includes('/')) command = 'otel_observe ' + command;
+  if (command.includes('/')) {
+    if (_execSync("which " + command) == command, options.shell && typeof options.shell == 'string' ? { shell: options.shell } : {}) {
+      command = command.substring(command.lastIndexOf('/') + 1);
+    } else
+      command = 'otel_observe ' + command;
+    }
+  }
   if (options.shell) {
     if (typeof options.shell == 'boolean') options.shell = '/bin/sh';
     options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE'] = options.shell + ' -c ' + command + ' ' + args.join(' ');
     options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE'] = process.pid;
     options.env['OTEL_SHELL_AUTO_INJECTED'] = 'FALSE'
-    return _spawn(options.shell, [ '-c', '. otel.sh\n' + command + ' "$@"', options.shell ].concat(args ?? []), { ... options, shell: false });
+    return original(options.shell, [ '-c', '. otel.sh\n' + command + ' "$@"', options.shell ].concat(args ?? []), { ... options, shell: false });
   } else {
-    return _spawn('/bin/sh', [ '-c', '. otel.sh\n' + command + ' "$@"', 'node' ].concat(args ?? []), options);
+    return original('/bin/sh', [ '-c', '. otel.sh\n' + command + ' "$@"', 'node' ].concat(args ?? []), options);
   }
 }
 
 child_process.exec = function(command, options, callback) {
+  return otel_exec(command, options, callback, _exec);
+}
+
+child_process.execSync = function(command, options) {
+  return otel_exec(command, options, undefined, _execSync);
+}
+
+function otel_exec(command, options, callback, original) {
   if (options && !callback && typeof options == 'function') {
     callback = options;
     options = {};
   }
   if (options && options.stdio && Array.isArray(options.stdio) && options.stdio.length > 3) return _exec(command, options, callback);
-  if (command.trim().startsWith('/') || command.trim().startsWith('.')) command = 'otel_observe ' + command;
+  if (command.trim().startsWith('/') || command.trim().startsWith('.')) {
+    if (_execSync(options.shell ?? '/bin/sh' + " -c 'which \"$1\"' sh " + command) == command) {
+      command = command.substring(command.lastIndexOf('/') + 1);
+    } else
+      command = 'otel_observe ' + command;
+    }
+  }
   options = options ?? {};
   options.env = options.env ?? { ... process.env };
   options.env['OTEL_SHELL_AUTO_INSTRUMENTATION_HINT'] = command + ' ';
   options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE'] = (options.shell ?? '/bin/sh') + ' -c ' + command;
   options.env['OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE'] = process.pid;
   options.env['OTEL_SHELL_AUTO_INJECTED'] = 'FALSE'
-  return _exec('. otel.sh\n' + command, options, callback);
+  return original('. otel.sh\n' + command, options, callback);
 }
 
 /*
