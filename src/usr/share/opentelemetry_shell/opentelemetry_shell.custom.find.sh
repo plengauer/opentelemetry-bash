76d51d3f06f04d670f98f939f12ec2c52031a5ce
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-22 21:54:09 +0200

    Update opentelemetry_shell.custom.find.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.find.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.find.sh
@@ -17,7 +17,7 @@ _otel_inject_find_arguments() {
     \echo -n ' '
     if \[ "$in_exec" -eq 0 ] && (\[ "$arg" = "-exec" ] || \[ "$arg" = "-execdir" ]); then
       local in_exec=1
-      if \[ "$_otel_shell" = busybox ]; then local extra_flag="-x"; fi
+      if \[ "$_otel_shell" = 'busybox sh' ]; then local extra_flag="-x"; fi
       \echo -n "$arg $_otel_shell $extra_flag -c '. otel.sh
 "
     elif \[ "$in_exec" -eq 1 ] && \[ "$arg" = "{}" ]; then
