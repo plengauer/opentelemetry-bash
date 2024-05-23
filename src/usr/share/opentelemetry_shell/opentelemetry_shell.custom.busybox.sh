a2ee2124c1bbe440c7ae4b73803dd25946d2b3b8
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-23 22:36:08 +0200

    Update opentelemetry_shell.custom.busybox.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.busybox.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.busybox.sh
@@ -5,10 +5,10 @@ _otel_inject_busybox() {
     _otel_call "$@"
     return "$?"
   fi
-  local command="$1"; shift
   local cmdline="$(_otel_dollar_star "$@")"
   local cmdline="${cmdline#\\}"
-  OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE OTEL_SHELL_AUTO_INSTRUMENTATION_HINT="$cmdline" \eval _otel_call "$(_otel_escape_args "$command" sh -c '. otel.sh
+  local command="$1"; shift
+  OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE \eval _otel_call "$(_otel_escape_args "$command" sh -c '. otel.sh
 eval "$(_otel_escape_args "$@")"' sh "$@")"
 }
 
