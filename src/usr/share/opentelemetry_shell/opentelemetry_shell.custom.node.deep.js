248777a2ab6639f22e37be6bf32861f10eb0b682
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-25 01:05:28 +0200

    Update opentelemetry_shell.custom.node.deep.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.js
@@ -13,37 +13,29 @@ const opentelemetry_resources_aws = require('@opentelemetry/resource-detector-aw
 const opentelemetry_resources_gcp = require('@opentelemetry/resource-detector-gcp');
 const opentelemetry_resources_alibaba_cloud = require('@opentelemetry/resource-detector-alibaba-cloud');
 
-async function init() {
-  let sdk = create();
-  process.on('exit', () => sdk.shutdown());
-  process.on('SIGINT', () => sdk.shutdown());
-  process.on('SIGQUIT', () => sdk.shutdown());
-  await sdk.start();
-}
-
-function create() {
-  // TODO select right exporter depending on OTEL_TRACES_EXPORTER
-  return new opentelemetry_sdk.NodeSDK({
-    spanProcessor: new opentelemetry_tracing.BatchSpanProcessor(new opentelemetry_traces_otlp.OTLPTraceExporter()),
-    instrumentations: [ opentelemetry_auto_instrumentations.getNodeAutoInstrumentations() ],
-    resourceDetectors: [
-      opentelemetry_resources_alibaba_cloud.alibabaCloudEcsDetector,
-      // opentelemetry_resources_gcp.gcpDetector, // TODO makes noisy spans!
-      opentelemetry_resources_aws.awsBeanstalkDetector,
-      opentelemetry_resources_aws.awsEc2Detector,
-      opentelemetry_resources_aws.awsEcsDetector,
-      opentelemetry_resources_aws.awsEksDetector,
-      // TODO k8s detector
-      opentelemetry_resources_container.containerDetector,
-      opentelemetry_resources_git.gitSyncDetector,
-      opentelemetry_resources_github.gitHubDetector,
-      opentelemetry_resources.processDetector,
-      opentelemetry_resources.envDetector
-    ],
-  });
-}
-
-await init();
+// TODO select right exporter depending on OTEL_TRACES_EXPORTER
+let sdk new opentelemetry_sdk.NodeSDK({
+  spanProcessor: new opentelemetry_tracing.BatchSpanProcessor(new opentelemetry_traces_otlp.OTLPTraceExporter()),
+  instrumentations: [ opentelemetry_auto_instrumentations.getNodeAutoInstrumentations() ],
+  resourceDetectors: [
+    opentelemetry_resources_alibaba_cloud.alibabaCloudEcsDetector,
+    // opentelemetry_resources_gcp.gcpDetector, // TODO makes noisy spans!
+    opentelemetry_resources_aws.awsBeanstalkDetector,
+    opentelemetry_resources_aws.awsEc2Detector,
+    opentelemetry_resources_aws.awsEcsDetector,
+    opentelemetry_resources_aws.awsEksDetector,
+    // TODO k8s detector
+    opentelemetry_resources_container.containerDetector,
+    opentelemetry_resources_git.gitSyncDetector,
+    opentelemetry_resources_github.gitHubDetector,
+    opentelemetry_resources.processDetector,
+    opentelemetry_resources.envDetector
+  ],
+});
+process.on('exit', () => sdk.shutdown());
+process.on('SIGINT', () => sdk.shutdown());
+process.on('SIGQUIT', () => sdk.shutdown());
+sdk.start();
 
 let span_context = opentelemetry_api.propagation.extract(opentelemetry_api.trace.ROOT_CONTEXT, { traceparent: process.env.OTEL_TRACEPARENT });
 let context = opentelemetry_api.trace.setSpan(opentelemetry_api.trace.ROOT_CONTEXT, opentelemetry_api.trace.wrapSpanContext(span_context.span));
