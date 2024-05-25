8dc6bd9242e58fee13c5ff30ee4e4e8afcf299bd
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-25 13:44:11 +0200

    Update opentelemetry_shell.custom.node.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
@@ -6,8 +6,10 @@ _otel_inject_node() {
   local command="$1"
   shift
   if \[ "$OTEL_SHELL_EXPERIMENTAL_INJECT_DEEP" = TRUE ] && \type npm &> /dev/null; then
-    # \ln --symbolic /usr/share/opentelemetry_shell.custom.node.deep.package.json opentelemetry_shell.package.json
-    \npm install --global --package /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.package.json
+    \cp package.json package.json.otel.backup 2> /dev/null
+    \cp /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.package.json package.json
+    \npm install --global --package-lock=false
+    \cp package.json.otel.backup package.json 2> /dev/null
     local extra_flags="--require /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.js"
   fi
   OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE _otel_call "$command" --require /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js $extra_flags "$@"
