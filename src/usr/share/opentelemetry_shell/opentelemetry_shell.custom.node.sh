e2eb188a4844c5ff7bafeb9dd4b317bc3db81941
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-25 15:59:49 +0200

    Update opentelemetry_shell.custom.node.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.sh
@@ -13,16 +13,21 @@ _otel_inject_node() {
       break
     done
     if \[ -f "$script" ]; then
-set -x
-      local wd="$(pwd)"
+      local wd="$(\pwd)"
       local dir="$(\echo "$script" | \rev | \cut -d / -f 2- | \rev)"
-      cd "$dir"
+      \cd "$dir"
       \cp package.json package.json.otel.backup 2> /dev/null || \true
       \cp /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.package.json package.json
       \npm install --package-lock=false && local extra_flags="--require /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.js" || \true
       \cp package.json.otel.backup package.json 2> /dev/null || \true
-      cd "$wd"
-set +x
+      \cd "$wd"
+
+      local wd="$(\pwd)"
+      \cd /opt/opentelemetry_shell
+      \ln --symbolic /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.package.json package.json
+      \ln --symbolic /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.js opentelemetry_shell.custom.node.deep.js
+      \npm install --package-lock=false && local extra_flags="--require /opt/opentelemetry_shell/opentelemetry_shell.custom.node.deep.js" || \true
+      \cd "$wd"
     fi
   fi
   OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE _otel_call "$command" --require /usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js $extra_flags "$@"
