f4bf9a7102ced548921d10c1275cbf92b4263bdc
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-27 19:54:27 +0200

    Update decorate_action_run.sh

--- actions/instrument/job/decorate_action_run.sh
+++ actions/instrument/job/decorate_action_run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 # sh|dash|bash -e /.../*.sh
-if [ -n "$GITHUB_RUN_ID" ] && [ -f "$3" ] && [ "$(echo "$3" | rev | cut -d . -f 1 | rev)" = "sh" ] && [ "$(echo "$GITHUB_ENV" | rev | cut -d / -f 3- | rev)" = "$(echo "$3" | rev | cut -d / -f 2- | rev)" ]; then
+file="$(eval echo '"$'"$#"'"'")"
+if [ -n "$GITHUB_RUN_ID" ] && [ -f "$file" ] && [ "$(echo "$file" | rev | cut -d . -f 1 | rev)" = "sh" ] && [ "$(echo "$GITHUB_ENV" | rev | cut -d / -f 3- | rev)" = "$(echo "$file" | rev | cut -d / -f 2- | rev)" ]; then
   # export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
-  file="$3"
   script="$(cat "$file")"
   script=". otel.sh
 $script"
