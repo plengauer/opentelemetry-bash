3532a1d423d646cb826e0b247bb6c60752c1b372
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-29 17:28:53 +0200

    Update opentelemetry_shell.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.sh
@@ -143,7 +143,7 @@ _otel_list_builtin_commands() {
 _otel_filter_commands_by_hint() {
   local hint="$1"
   if \[ -n "$hint" ]; then
-if \[ "$_otel_shell" = 'busybox sh'; then
+if \[ "$_otel_shell" = 'busybox sh' ]; then
 \echo "DEBUG resolve $hint" >&2
 _otel_resolve_instrumentation_hint "$hint" >&2
 fi
