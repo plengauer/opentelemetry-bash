ac06a4411a70e7cf6be16971b85ced2e0d4e2ed0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-05-30 23:48:51 +0200

    Update opentelemetry_shell_api.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
@@ -15,7 +15,7 @@ fi
 
 # basic setup
 _otel_remote_sdk_pipe="${OTEL_REMOTE_SDK_PIPE:-$(\mktemp -u --tmpdir="$_otel_shell_pipe_dir")_opentelemetry_shell_$$.pipe}"
-_otel_shell_pipe_dir="${OTEL_SHELL_PIPE_DIR;-${TMPDIR:-/tmp}}"
+_otel_shell_pipe_dir="${OTEL_SHELL_PIPE_DIR:-${TMPDIR:-/tmp}}"
 _otel_shell="$(\readlink "/proc/$$/exe" | \rev | \cut -d / -f 1 | \rev)"
 if \[ "$_otel_shell" = busybox ]; then _otel_shell="busybox sh"; fi
 if \[ "$OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE" = 0 ] || \[ "$OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE" = "$PPID" ] || \[ "$PPID" = 0 ] || \[ "$(\tr '\000-\037' ' ' < /proc/$PPID/cmdline)" = "$(\tr '\000-\037' ' ' < /proc/$OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE/cmdline)" ]; then _otel_commandline_override="$OTEL_SHELL_COMMANDLINE_OVERRIDE"; fi
