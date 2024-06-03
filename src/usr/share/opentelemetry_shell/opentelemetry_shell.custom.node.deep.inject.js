40d1fc70b3c1509180da0ea2b1b5ea2603aeea16
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-03 22:50:08 +0200

    Update opentelemetry_shell.custom.node.deep.inject.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.inject.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.inject.js
@@ -1,13 +1,2 @@
 const opentelemetry_api = require('@opentelemetry/api');
-
-console.error('DEBUG DEBUG DEBUG traceparent: ' + process.env.OTEL_TRACEPARENT);
-let span_context = opentelemetry_api.propagation.extract(opentelemetry_api.trace.ROOT_CONTEXT, { traceparent: process.env.OTEL_TRACEPARENT });
-console.error('DEBUG DEBUG DEBUG span context: ' + span_context);
-let context = opentelemetry_api.trace.setSpan(opentelemetry_api.trace.ROOT_CONTEXT, opentelemetry_api.trace.wrapSpanContext(span_context.span));
-console.error('DEBUG DEBUG DEBUG context: ' + context);
-opentelemetry_api.trace.context.setGlobalContextManager({
-  active() {
-    console.error('DEBUG DEBUG DEBUG context accessed');
-    return context;
-  },
-});
+opentelemetry_api.trace.setSpanContext(opentelemetry_api.context.active(), opentelemetry_api.propagation.extract(opentelemetry_api.context.active(), { traceparent: process.env.OTEL_TRACEPARENT }));
