1031a21658dae5b582d30686736d659739ded3cb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-04 12:38:02 +0200

    Update shutdown.sh

--- actions/instrument/job/shutdown.sh
+++ actions/instrument/job/shutdown.sh
@@ -3,3 +3,4 @@ root_pid_file="$(mktemp -u | rev | cut -d / -f 2- | rev)/opentelemetry_shell_$GI
 root_pid="$(cat "$root_pid_file")"
 kill -USR1 "$root_pid"
 while kill -0 "$root_pid" 2> /dev/null; do sleep 1; done
+cat "$OTEL_SHELL_SDK_OUTPUT_REDIRECT" >&2
