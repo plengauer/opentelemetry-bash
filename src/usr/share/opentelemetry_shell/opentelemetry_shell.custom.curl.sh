f83a11068f560230475456d57f5fa5a1606aabcf
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-04 10:05:31 +0200

    Update opentelemetry_shell.custom.curl.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.curl.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.curl.sh
@@ -13,7 +13,7 @@ _otel_propagate_curl() {
   _otel_pipe_curl_stderr "$is_verbose" < "$stderr_pipe" >&2 &
   local stderr_pid="$!"
   local exit_code=0
-  _otel_call "$@" -H "traceparent: $OTEL_TRACEPARENT" -v --no-progress-meter 2> "$stderr_pipe" || exit_code="$?"
+  _otel_call "$@" -H "traceparent: $OTEL_TRACEPARENT" -H "tracestate: $OTEL_TRACESTATE" -v --no-progress-meter 2> "$stderr_pipe" || exit_code="$?"
   \wait "$stderr_pid"
   \rm "$stderr_pipe"
   if \[ "$job_control" = 1 ]; then \set -m; fi
