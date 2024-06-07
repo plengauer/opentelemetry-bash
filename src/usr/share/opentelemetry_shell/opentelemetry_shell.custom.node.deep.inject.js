611bfbf6ef7d3c14bb66336095b98e6257e3d42b
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-07 10:03:23 +0200

    Delete src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.inject.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.deep.inject.js
+++
@@ -1,2 +0,0 @@
-const opentelemetry_api = require('@opentelemetry/api');
-opentelemetry_api.trace.setSpanContext(opentelemetry_api.context.active(), opentelemetry_api.propagation.extract(opentelemetry_api.context.active(), { traceparent: process.env.OTEL_TRACEPARENT }));
