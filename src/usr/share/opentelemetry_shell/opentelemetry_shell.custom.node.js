c6dde6348c9d693ab7192f26b5f709fb36b7cb48
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-13 14:47:50 +0200

    Update opentelemetry_shell.custom.node.js

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.node.js
@@ -40,6 +40,7 @@ function otel_spawn(command, args, options, original) {
     options.env['OTEL_SHELL_AUTO_INJECTED'] = 'FALSE'
     return original(options.shell, [ '-c', '. otel.sh\n' + command + ' "$@"', options.shell ].concat(args ?? []), { ... options, shell: false });
   } else {
+    console.error(JSON.stringify(options.env));
     return original('/bin/sh', [ '-c', '. otel.sh\n' + command + ' "$@"', 'node' ].concat(args ?? []), options);
   }
 }
