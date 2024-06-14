54db4d0b0833f56b859c1cb9f50a9f4e8b238443
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-14 19:43:07 +0200

    Update run_tests_containerized.sh

--- tests/run_tests_containerized.sh
+++ tests/run_tests_containerized.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+set -e
 retry=1
 while [ "$retry" -lt 60 ] && ! sudo docker build --build-arg=image="$1" --build-arg=update="$2" --build-arg=shell="$3" --tag test --network=host .; do sleep "$retry"; retry=$((retry * 2)); done
 exit_code=0
