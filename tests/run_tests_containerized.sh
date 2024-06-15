0db8640792b34c4beaa80f5d548f0f7b1bd08cb0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-15 07:13:02 +0200

    Harden tests

--- tests/run_tests_containerized.sh
+++ tests/run_tests_containerized.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-sudo docker build --build-arg=image="$1" --build-arg=update="$2" --build-arg=shell="$3" --tag test --network=host .
-sudo docker run --name test --network=host test
-exit_code=$?
-sudo docker container rm test
-sudo docker image rm test
+#!/bin/bash -e
+set -e
+retry=1
+while [ "$retry" -lt 60 ] && ! sudo docker build --build-arg=image="$1" --build-arg=update="$2" --build-arg=shell="$3" --tag test --network=host .; do sleep "$retry"; retry=$((retry * 2)); done
+exit_code=0
+sudo docker run --rm --name test --network=host test || exit_code=$?
+sudo docker image rm test || true
 exit $exit_code
