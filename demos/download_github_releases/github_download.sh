bd5f50d0adb9eac7e4a0ab845519cd38adc04625
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-15 17:21:50 +0200

    Create github_download.sh

---
+++ demos/download_github_releases/github_download.sh
@@ -0,0 +1,10 @@
+repository=plengauer/opentelemetry-bash
+per_page=100
+url="https://api.github.com/repos/$repository/releases?per_page=$per_page"
+curl --no-progress-meter --fail --retry 16 --retry-all-errors --head -H "$auth_header" "$url" \
+  | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
+  | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
+  | grep '^page=' | cut -d = -f 2 \
+  | xargs seq 1 | xargs parallel -q curl --no-progress-meter --fail --retry 16 --retry-all-errors -H "$auth_header" "$url"\&page={} ::: \
+  | jq '.[] | .assets[] | .browser_download_url' -r | (grep '.deb$' || true)
+  | xargs wget
