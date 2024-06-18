adc2b54110421d976b71372adf04707193a9f8fd
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-18 14:58:05 +0200

    Update shutdown.sh

--- actions/instrument/job/shutdown.sh
+++ actions/instrument/job/shutdown.sh
@@ -1,5 +1,5 @@
 set -e
-root_pid="$(cat "$(mktemp -u | rev | cut -d / -f 2- | rev)/opentelemetry_shell_$GITHUB_RUN_ID.pid")"
+root_pid="$STATE_pid"
 kill -USR1 "$root_pid"
 while kill -0 "$root_pid" 2> /dev/null; do sleep 1; done
 cat "$STATE_log_file" >&2
