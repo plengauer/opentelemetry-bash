fd0bc5d53fb4f6e4ee04971eb924fb361db3ebd4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-20 20:11:11 +0200

    Configure renovate to export (#427)
    Co-authored-by: Moritz Witti <46486962+moflwi@users.noreply.github.com>

---
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
@@ -0,0 +1,20 @@
+#!/bin/false
+
+# renovate => OTEL_...=... renovate
+
+_otel_configure_renovate() {
+  # renovate looks at some very specific env vars to enable tracing
+  if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
+  if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
+    if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
+      if _otel_string_ends_with "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" /v1/traces; then
+        export OTEL_EXPORTER_OTLP_ENDPOINT="${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT%/v1/traces}"
+      fi
+    else
+      export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
+    fi
+  fi
+  _otel_call "$@"
+}
+
+_otel_alias_prepend renovate _otel_configure_renovate
