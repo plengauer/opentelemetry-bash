cc8691349f960f988f60e5c46462e2736537d64a
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-21 00:09:07 +0200

    Update opentelemetry_shell_api.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
@@ -414,7 +414,6 @@ _otel_call_and_record_pipes() {
     \tee "$stdin_bytes" "$stdin_lines" 2> /dev/null | {
       local inner_exit_code=0
       $call_command "$@" || local inner_exit_code="$?"
-      # local stdin_pid="$(\ps -o 'pid,command' | \grep -F "tee $stdin_bytes $stdin_lines" | \grep -vF grep | \cut -d , -f1 | \tr -d ' ')"
       local stdin_pid="$(\ps -o 'pid,command' | \grep -F "tee $stdin_bytes $stdin_lines" | \grep -vF grep | \awk '{ print $1 }')"
       if \[ -n "$stdin_pid" ]; then \kill -2 "$stdin_pid" 2> /dev/null || true; fi
       return "$inner_exit_code"
