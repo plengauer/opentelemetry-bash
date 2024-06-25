8894a7f97fd379a6b5e2cd93ba070d07cbb28b38
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-25 22:36:35 +0200

    Update opentelemetry_shell.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.sh
@@ -26,9 +26,9 @@ case "$-" in
   *)   _otel_is_interactive=FALSE;;
 esac
 
-if \[ "$_otel_is_interactive" = "TRUE" ] || \[ "$(\readlink -f "$(\which "$0")" | \rev | \cut -d / -f 1 | \rev)" = "$(\readlink -f "/proc/$$/exe" | \rev | \cut -d / -f 1 | \rev)" ]; then
+if \[ "$_otel_is_interactive" = "TRUE" ]; then
   _otel_shell_auto_instrumentation_hint=""
-elif \[ -f "$0" ]; then
+elif \[ -f "$0" ] && \[ "$(\readlink -f "$(\which "$0")" | \rev | \cut -d / -f 1 | \rev)" != "$(\readlink -f "/proc/$$/exe" | \rev | \cut -d / -f 1 | \rev)" ]; then
   _otel_shell_auto_instrumentation_hint="$0"
 else
   _otel_shell_auto_instrumentation_hint="$(_otel_resolve_command_self)"
