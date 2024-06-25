730055562607712957f0424e1e7e7d89a9e07261
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-25 22:13:11 +0200

    Update opentelemetry_shell.custom.docker.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.docker.sh
@@ -79,7 +79,7 @@ _otel_inject_docker_args() {
     \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
     \echo -n ' '; _otel_escape_arg "$1"; shift
     \echo -n ' '; _otel_escape_args -c '. otel.sh
-alias >&2
+\alias | \sort >&2
 eval "$(_otel_escape_args "$@")"' sh
     \echo -n ' '; if \[ -n "$entrypoint_override" ]; then \echo "$entrypoint_override" | _otel_line_split; else "$executable" inspect "$image" | \jq -r '.[0].Config.Entrypoint[]?'; fi | _otel_escape_stdin
     if \[ "$#" = 0 ]; then \echo -n ' '; "$executable" inspect "$image" | \jq -r '.[0].Config.Cmd[]?' | _otel_escape_stdin; fi
