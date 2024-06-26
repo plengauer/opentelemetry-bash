09d608af88293fc98590f41944561783bf8b01c0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-26 16:47:04 +0200

    Update opentelemetry_shell.custom.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
@@ -1,20 +1,47 @@
 #!/bin/false
 
-# renovate => OTEL_...=... renovate
+# write | netcat 127.0.0.1 8080 | read
+# netcat -l -p 8080 | read
+# netcat -l -p 8080 -e respond
 
-_otel_configure_renovate() {
-  # renovate looks at some very specific env vars to enable tracing
-  if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
-  if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
-    if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
-      if _otel_string_ends_with "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" /v1/traces; then
-        export OTEL_EXPORTER_OTLP_ENDPOINT="${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT%/v1/traces}"
-      fi
-    else
-      export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
-    fi
-  fi
-  _otel_call "$@"
+_otel_propagate_netcat_read() {
+  \cat # TODO
 }
 
-_otel_alias_prepend renovate _otel_configure_renovate
+_otel_propagate_netcat_write() {
+  \cat # TODO  
+}
+
+_otel_propagate_netcat() {
+  if _otel_args_contains -l "$@" || _otel_args_contains --listen "$@"; then
+    # TODO
+  else
+    local exit_code_file="$(\mktemp)"
+    \echo 0 > "$exit_code_file"
+    local span_handle="$(otel_span_start CLIENT send/receive)"
+    _otel_propagate_netcat_parse "$span_handle" "$@"
+    otel_span_activate "$span_handle"
+    _otel_propagate_netcat_write "$span_handle" | { _otel_call "$@" || \echo "$?" > "$exit_code_file"; } | _otel_propagate_netcat_read "$span_handle"
+    otel_span_deactivate "$span_handle"
+    otel_span_end "$span_handle"
+    return "$(\cat "$exit_code_file")"
+  fi
+}
+
+_otel_propagate_netcat_parse() {
+  local span_handle="$1"
+  # TODO use -u and --udp to set type to udp not tcp
+  # TODO set bunch of default arguments
+}
+
+_otel_args_contains() {
+  local needle="$1"; shift
+  for _otel_netcat_arg in "$@"; do
+    if \[ "$_otel_netcat_arg" = "$needle" ]; then return 0; fi
+  done
+  return 1
+}
+
+_otel_alias_prepend nc _otel_propagate_netcat
+_otel_alias_prepend ncat _otel_propagate_netcat
+_otel_alias_prepend netcat _otel_propagate_netcat
