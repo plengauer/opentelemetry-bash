6f384cbe7cecf55981f6b036293b97ab6c93f128
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-26 20:32:07 +0200

    Update opentelemetry_shell.custom.renovate.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
@@ -1,9 +1,6 @@
 #!/bin/false
 
-# renovate => OTEL_...=... renovate
-
-_otel_configure_renovate() {
-  # renovate looks at some very specific env vars to enable tracing
+if \[ "$(_otel_command_self)" = '/bin/bash /usr/local/bin/renovate' ]; then # renovate looks at some very specific env vars to enable tracing
   if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
   if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
     if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
@@ -14,12 +11,4 @@ _otel_configure_renovate() {
       export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
     fi
   fi
-  if \[ "$1" = /bin/bash ] && \[ /bin/bash = "$(\which bash)" ]; then
-    shift
-    bash "$@"
-  else
-    _otel_call "$@"
-  fi
-}
-
-# _otel_alias_prepend renovate _otel_configure_renovate
+fi
