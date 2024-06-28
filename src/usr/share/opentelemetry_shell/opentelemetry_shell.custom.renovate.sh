6f51ae9c363da1c47abd8ae91ff937a7768468fe
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-28 12:16:29 +0200

    Fix injection into shebanged command

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
@@ -1,9 +1,6 @@
 #!/bin/false
 
-# renovate => OTEL_...=... renovate
-
-_otel_configure_renovate() {
-  # renovate looks at some very specific env vars to enable tracing
+if \[ "$(_otel_command_self)" = '/bin/bash /usr/local/bin/renovate' ]; then # renovate looks at some very specific env vars to enable tracing
   if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
   if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
     if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
@@ -14,7 +11,4 @@ _otel_configure_renovate() {
       export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
     fi
   fi
-  _otel_call "$@"
-}
-
-_otel_alias_prepend renovate _otel_configure_renovate
+fi
