2b95ddfddeb0af66a640b00297fa87810f72c398
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-29 12:31:53 +0200

    Create test_auto_netcat_injection.sh

---
+++ tests/auto/test_auto_netcat_injection.sh
@@ -0,0 +1,12 @@
+if ! [ -x "$(which ncat)" ]; then exit 0; fi
+set -e
+. ./assert.sh
+
+otelinjectnetcat ncat -l -c cat "$port" &
+assert_equals "hello world" "$(echo -n hello world | netcat -N 127.0.0.1 "$port")"
+span="$(resolve_span '.kind == "SpanKind.SERVER"')"
+assert_equals "send/receive" $(\echo "$span" | jq -r '.name')
+span_id=$(\echo "$span" | jq -r '.context.span_id')
+span="$(resolve_span '.name == "cat"')"
+assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.name')
+assert_equals "$span_id" $(\echo "$span" | jq -r '.parent_id')
