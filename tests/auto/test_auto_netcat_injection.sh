2767f42e9d12599ad3d9d2a5d5d401ddaaeb41f4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-06-30 13:32:54 +0200

    Update test_auto_netcat_injection.sh

--- tests/auto/test_auto_netcat_injection.sh
+++ tests/auto/test_auto_netcat_injection.sh
@@ -4,7 +4,7 @@ set -e
 
 port=12345
 
-otel4netcat ncat -l -c cat "$port" &
+otel4netcat ncat -l -p "$port" -c cat &
 assert_equals "hello world" "$(echo -n hello world | netcat -w 30 127.0.0.1 "$port")"
 span="$(resolve_span '.kind == "SpanKind.SERVER"')"
 assert_equals "send/receive" $(\echo "$span" | jq -r '.name')
