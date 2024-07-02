d220df6091578a52b4f7909d73e605e5e8151509
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-02 18:12:14 +0200

    Fix renovate configuration

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
@@ -1,6 +1,6 @@
 #!/bin/false
 
-if \[ "$(_otel_command_self)" = '/bin/bash /usr/local/bin/renovate' ]; then # renovate looks at some very specific env vars to enable tracing
+if _otel_string_contains "$(_otel_resolve_command_self)" /usr/local/bin/renovate; then # renovate looks at some very specific env vars to enable tracing
   if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
   if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
     if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
