2d4a30de8ff07611233e3153bece704327c347ae
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-02 12:55:00 +0200

    Update opentelemetry_shell.custom.renovate.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.renovate.sh
@@ -1,8 +1,6 @@
 #!/bin/false
 
 if _otel_string_contains "$(_otel_resolve_command_self)" /usr/local/bin/renovate; then # renovate looks at some very specific env vars to enable tracing
-  \printenv | grep OTEL_ >&2
-  set -x
   if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
   if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
     if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
@@ -13,5 +11,4 @@ if _otel_string_contains "$(_otel_resolve_command_self)" /usr/local/bin/renovate
       export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
     fi
   fi
-  set +x
 fi
