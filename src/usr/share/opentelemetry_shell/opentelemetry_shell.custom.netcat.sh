b6d44a205552a31f54062de99afe4cf368b82e38
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-05 15:30:54 +0200

    Update opentelemetry_shell.custom.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
@@ -305,6 +305,7 @@ _otel_binary_read() {
     local byte="$(\dd bs=1 count=1 2> /dev/null | \hexdump -v -e '/1 "%02x"')"
     if \[ "$byte" = '' ]; then local eos=1; break; fi
     if \[ "$byte" = 0a ]; then break; fi
+    local line="$line$byte"
   done
   \eval "$var='$line'"
   return "$eos"
