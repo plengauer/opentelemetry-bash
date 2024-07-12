196f0ce5ea76116bca4602ebbf1c323eece20b00
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-12 15:12:56 +0200

    Update opentelemetry_shell.custom.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
@@ -53,6 +53,7 @@ _otel_inject_netcat_listen_and_respond_args() {
       # TODO the following injection doesnt maintain the exit code, does it matter though? is it important for netcat?
       if \[ "$OTEL_SHELL_CONFIG_NETCAT_ASSUME_REQUEST_RESPONSE" = TRUE ]; then
         _otel_escape_args -c "OTEL_SHELL_AUTO_INJECTED=FALSE
+set -x
 span_handle_file=\"\$(mktemp)\"
 span_handle_file_1=\"\$(mktemp -u)\"
 span_handle_file_2=\"\$(mktemp -u)\"
@@ -68,6 +69,7 @@ otel_span_end \"\$span_handle\"
 \rm \"\$span_handle_file\" \"\$span_handle_file_1\" \"\$span_handle_file_2\" 2> /dev null"        
       else
         _otel_escape_args -c "OTEL_SHELL_AUTO_INJECTED=FALSE
+set -x
 span_handle_file=\"\$(mktemp)\"
 . otel.sh
 span_handle=\"\$(otel_span_start CONSUMER send/receive)\"
@@ -85,7 +87,6 @@ otel_span_end \"\$span_handle\"
 }
 
 _otel_netcat_parse_request() {
-  set -x
   local is_server_side="$1"; shift
   local span_handle_file="$1"; shift
   if ! _otel_binary_read line; then
