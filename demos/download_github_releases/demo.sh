b0fac3ff6d385e4f64de2c3637cf4f974ceb3289
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-12 13:42:20 +0200

    Update demo.sh

--- demos/download_github_releases/demo.sh
+++ demos/download_github_releases/demo.sh
@@ -2,7 +2,8 @@
 repository=plengauer/opentelemetry-bash
 per_page=100
 url="https://api.github.com/repos/$repository/releases?per_page=$per_page"
-curl --no-progress-meter --fail --retry 16 --retry-all-errors --head "$url" \
+# curl --no-progress-meter --fail --retry 16 --retry-all-errors --head "$url" \
+printf 'HEAD /'"$(echo "$url" | cut -d / -f 4-)"' HTTP/1.1\r\nUser-Agent: ncat\r\n\r\n' | ncat --ssl "$(echo "$url" | cut -d / -f 3)" 443 \
   | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
   | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
   | grep '^page=' | cut -d = -f 2 \
