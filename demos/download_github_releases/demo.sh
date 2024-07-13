8f4a398f145bd3d33ee65d26de29718605efc0b7
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-13 14:52:23 +0200

    Update demo.sh

--- demos/download_github_releases/demo.sh
+++ demos/download_github_releases/demo.sh
@@ -1,9 +1,10 @@
 . otel.sh
 repository=plengauer/opentelemetry-bash
 per_page=100
-url="https://api.github.com/repos/$repository/releases?per_page=$per_page"
-# curl --no-progress-meter --fail --retry 16 --retry-all-errors --head "$url" \
-printf 'HEAD /'"$(echo "$url" | cut -d / -f 4-)"' HTTP/1.1\r\nUser-Agent: ncat\r\nHost: '"$(echo "$url" | cut -d / -f 3)"'\r\n\r\n' | ncat --ssl -i 3 --no-shutdown "$(echo "$url" | cut -d / -f 3)" 443 \
+host=api.github.com
+path="/repos/$repository/releases?per_page=$per_page"
+url=https://"$host""$path"
+printf "HEAD /$host HTTP/1.1\r\nUser-Agent: ncat\r\nHost: $host\r\n\r\n" | ncat --ssl -i 3 --no-shutdown "$host" 443 \
   | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
   | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
   | grep '^page=' | cut -d = -f 2 \
