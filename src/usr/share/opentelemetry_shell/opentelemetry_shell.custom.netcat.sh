05651e7fe3918e6a4e2fa718c5f58c6ff89d4a2f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-14 22:10:15 +0200

    Update opentelemetry_shell.custom.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
@@ -86,6 +86,7 @@ otel_span_end \"\$span_handle\"
 }
 
 _otel_netcat_parse_request() {
+if \[ "$OTEL_SHELL_DEBUG" = TRUE ]; then set -x; fi
   local is_server_side="$1"; shift
   local span_handle_file="$1"; shift
   if ! _otel_binary_read line; then
