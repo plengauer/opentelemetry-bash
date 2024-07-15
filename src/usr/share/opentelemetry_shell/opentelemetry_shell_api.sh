9e09d601a889cd63350a567ada379dc281c1e96b
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-15 17:11:36 +0200

    Fix accidentally monitored internal command

--- src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
@@ -416,7 +416,7 @@ _otel_call_and_record_pipes() {
       local inner_exit_code=0
       $call_command "$@" || local inner_exit_code="$?"
       local stdin_pid="$(\ps -o 'pid,command' | \grep -F "tee $stdin_bytes $stdin_lines" | \grep -vF grep | \awk '{ print $1 }')"
-      if \[ -n "$stdin_pid" ]; then \kill -2 "$stdin_pid" 2> /dev/null || true; fi
+      if \[ -n "$stdin_pid" ]; then \kill -2 "$stdin_pid" 2> /dev/null || \true; fi
       \echo -n "$inner_exit_code" > "$exit_code_file" # return "$inner_exit_code"
     } 1> "$stdout" 2> "$stderr" # || local exit_code="$?"
     local exit_code="$(\cat "$exit_code_file")"
