7e1dfdeb3ab8920d70fbe7f1fab538b4ff6cbdf5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-15 10:38:58 +0200

    Update opentelemetry_shell.inject.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.netcat.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
-set -x # TODO remove
 if [ "$OTEL_SHELL_AUTO_INJECTED" = TRUE ]; then
   exec "$@"
 else
   export OTEL_SHELL_AUTO_INJECTED=TRUE
   . otel.sh
   \unalias -a
+  set -x # TODO remove
   _otel_inject_netcat "$@"
 fi
