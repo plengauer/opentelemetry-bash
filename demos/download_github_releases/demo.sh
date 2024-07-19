b1036cfae2b79a5e9716a2fac855b2fad6345132
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-19 16:39:11 +0200

    Update demo.sh

--- demos/download_github_releases/demo.sh
+++ demos/download_github_releases/demo.sh
@@ -4,10 +4,10 @@ per_page=100
 host=api.github.com
 path="/repos/$repository/releases?per_page=$per_page"
 url=https://"$host""$path"
-printf "HEAD /$host HTTP/1.1\r\nConnection: close\r\nUser-Agent: ncat\r\nHost: $host\r\n\r\n" | ncat --ssl -i 3 --no-shutdown "$host" 443 | tr '[:upper:]' '[:lower:]' \
+printf "HEAD /$path HTTP/1.1\r\nConnection: close\r\nUser-Agent: ncat\r\nHost: $host\r\n\r\n" | ncat --ssl -i 3 --no-shutdown "$host" 443 | tr '[:upper:]' '[:lower:]' \
   | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
   | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
   | grep '^page=' | cut -d = -f 2 \
   | xargs seq 1 | xargs parallel -q curl --no-progress-meter --fail --retry 16 --retry-all-errors "$url"\&page={} ::: \
-  | jq '.[] | .assets[] | .browser_download_url' -r | (grep '.deb$' || true) | (head --lines=3 && cat > /dev/null) \
+  | jq '.[] | .assets[] | .browser_download_url' -r | (grep '.deb$' || true) | grep '_1.' | head --lines=3 \
   | xargs wget
