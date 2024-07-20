a21e9f3f178606bce36e93698b40fdfbdf6a9eb4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-20 09:31:44 +0200

    Update opentelemetry_shell.custom.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
@@ -55,7 +55,9 @@ _otel_inject_netcat_listen_and_respond_args() {
       if \[ "$OTEL_SHELL_CONFIG_NETCAT_ASSUME_REQUEST_RESPONSE" = TRUE ]; then
         _otel_escape_args -c '
 OTEL_SHELL_AUTO_INJECTED=TRUE
-span_handle_file_0="$(\mktemp -u)_opentelemetry_shell_$$.netcat.request.span_handle"
+exit_code_file="$(mktemp)"
+echo 0 > "$exit_code_file"
+span_handle_file_0="$(mktemp -u)_opentelemetry_shell_$$.netcat.request.span_handle"
 span_handle_file_1="$(mktemp -u)_opentelemetry_shell_$$.netcat.command.span_handle"
 span_handle_file_2="$(mktemp -u)_opentelemetry_shell_$$.netcat.response.span_handle"
 mkfifo "$span_handle_file_0" "$span_handle_file_1" "$span_handle_file_2"
@@ -64,10 +66,11 @@ mkfifo "$span_handle_file_0" "$span_handle_file_1" "$span_handle_file_2"
 span_handle="$(otel_span_start CONSUMER send/receive)"
 otel_span_activate "$span_handle"
 _otel_netcat_parse_args 1 "$span_handle" '"$netcat_command"' > /dev/null
-_otel_netcat_parse_request 1 "$span_handle_file_0" '"$netcat_command"' | { otel_span_activate "$(\cat "$span_handle_file_1")"; '"$command"'; } | _otel_netcat_parse_response 1 "$span_handle_file_2"
+_otel_netcat_parse_request 1 "$span_handle_file_0" '"$netcat_command"' | { otel_span_activate "$(\cat "$span_handle_file_1")"; '"$command"' || \echo "$!" > "$exit_code_file"; } | _otel_netcat_parse_response 1 "$span_handle_file_2"
 otel_span_deactivate "$span_handle"
 otel_span_end "$span_handle"
 \rm "$span_handle_file_0" "$span_handle_file_1" "$span_handle_file_2" 2> /dev/null
+exit "$(\cat "$exit_code_file")"
 '
       else
         _otel_escape_args -c '
