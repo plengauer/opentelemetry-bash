aaaf5ade35a6451aaf5c57b1558ee596488ebb20
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-21 10:16:09 +0200

    Added demos

---
+++ demos/README.md
@@ -0,0 +1,1 @@
+See the subdirectories for invidual demo scripts including their output and traces they generate.
---
+++ demos/download_github_releases/demo.sh
@@ -0,0 +1,13 @@
+. otel.sh
+repository=plengauer/opentelemetry-bash
+per_page=100
+host=api.github.com
+path="/repos/$repository/releases?per_page=$per_page"
+url=https://"$host""$path"
+printf "HEAD $path HTTP/1.1\r\nConnection: close\r\nUser-Agent: ncat\r\nHost: $host\r\n\r\n" | ncat --ssl -i 3 --no-shutdown "$host" 443 | tr '[:upper:]' '[:lower:]' \
+  | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
+  | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
+  | grep '^page=' | cut -d = -f 2 \
+  | xargs seq 1 | xargs parallel -q curl --no-progress-meter --fail --retry 16 --retry-all-errors "$url"\&page={} ::: \
+  | jq '.[] | .assets[] | .browser_download_url' -r | grep '.deb$' | grep '_1.' | head --lines=3 \
+  | xargs wget -q
