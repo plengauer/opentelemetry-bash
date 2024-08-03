6b9b4a34a28579e33d83044ade946960a6f82115
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-08-03 11:03:23 +0200

    Update opentelemetry_shell.custom.shells.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.shells.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.shells.sh
@@ -46,7 +46,7 @@ _otel_inject_shell_args_with_copy() {
   \echo "OTEL_SHELL_AUTO_INSTRUMENTATION_HINT='$temporary_script'" >> "$temporary_script"
   \echo ". otel.sh" >> "$temporary_script"
   \echo "\set -- $(_otel_escape_args "$@")" >> "$temporary_script"
-  (if \[ "$is_script" -eq 1 ]; then \cat "$command"; else \echo "$command"; fi) >> "$temporary_script"
+  (if \[ "$is_script" -eq 1 ]; then \cat "$command"; else \echo "$command"; fi) >> "$temporary_script" # TODO edit script!
 }
 
 _otel_inject_shell_with_copy() {
