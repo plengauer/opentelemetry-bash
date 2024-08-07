cc0c8577d229e53acf30a2fa4766406450ecab16
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2024-08-07 10:50:07 +0200

    blubb

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.shells.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.shells.sh
@@ -47,16 +47,18 @@ _otel_inject_shell_args_with_copy() {
   \echo ". otel.sh" >> "$temporary_script"
   \echo "\set -- $(_otel_escape_args "$@")" >> "$temporary_script"
   # if \[ "$is_script" -eq 1 ]; then \cat "$command"; else \echo "$command"; fi >> "$temporary_script"
-  if \[ "$is_script" -eq 1 ]; then \cat "$command"; else \echo "$command"; fi | if \[ "$OTEL_SHELL_EXPERIMENTAL_INSTRUMENT_ABSOLUTE_PATHS" = TRUE ]; then \awk '
-  {
-    gsub(/^[ \t]+/, "", $0);
-    if ($1 ~ /^\//) {
+  if \[ "$is_script" -eq 1 ]; then \cat "$command"; else \echo "$command"; fi | if \[ "$OTEL_SHELL_EXPERIMENTAL_INSTRUMENT_ABSOLUTE_PATHS" = TRUE ]; then
+    \awk '{
+      gsub(/^[ \t]+/, "", $0);
+      if ($1 ~ /^\//) {
         print "_otel_inject " $0;
-    } else {
+      } else {
         print $0;
-    }
-  }
-'; else \cat; fi >> "$temporary_script"
+      }
+    }'
+  else
+    \cat
+  fi >> "$temporary_script"
 }
 
 _otel_inject_shell_with_copy() {
