ec5337f54fd52537e69c2b0f2b166cbcb24c38a5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-08-15 13:41:14 +0200

    Update opentelemetry_shell_api.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell_api.sh
@@ -305,7 +305,8 @@ otel_metric_add() {
 }
 
 otel_observe() {
-  local IFS=' '
+  local IFS=' 
+'
   # validate and clean arguments
   local dollar_star="$(_otel_dollar_star "$@")"
   local command="$dollar_star"
@@ -382,7 +383,8 @@ else
 fi
 
 _otel_call_and_record_logs() {
-  local IFS=' '
+  local IFS=' 
+'
   case "$-" in
     *m*) local job_control=1; \set +m;;
     *) local job_control=0;;
@@ -402,7 +404,8 @@ _otel_call_and_record_logs() {
 }
 
 _otel_call_and_record_pipes() {
-  local IFS=' '
+  local IFS=' 
+'
   # some notes about this function
   # (*) we have to wait for the background processes because otherwise the span_id may not be valid anymore
   # (*) waiting for the processes only works when its not a subshell so we can access the last process id
