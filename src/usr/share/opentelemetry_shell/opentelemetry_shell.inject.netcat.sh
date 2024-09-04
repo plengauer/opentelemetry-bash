288e4a64869dec49915061eed684d7944b75a5f0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-04 22:14:58 +0200

    Update opentelemetry_shell.inject.netcat.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.netcat.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.netcat.sh
@@ -3,6 +3,6 @@ if [ "$OTEL_SHELL_AUTO_INJECTED" = TRUE ]; then
   exec "$@"
 else
   . otelapi.sh
-  . /usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh
+  \eval "$(\grep -vh '_otel_alias_prepend ' /usr/share/opentelemetry_shell/opentelemetry_shell.custom.netcat.sh)"
   _otel_inject_netcat "$@"
 fi
