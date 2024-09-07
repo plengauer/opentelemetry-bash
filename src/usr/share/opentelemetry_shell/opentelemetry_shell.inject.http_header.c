3931fcd11591da27594d1afa199bf5f45839ad2c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-07 13:01:49 +0200

    Update opentelemetry_shell.inject.http_header.c

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.http_header.c
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.http_header.c
@@ -1,12 +1,60 @@
 #define _GNU_SOURCE
-#include <dlfcn.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <dlfcn.h>
+#include <sys/types.h> // TODO do we need this?
+#include <sys/socket.h> // TODO do we need this?
+
+int otel_span_start(FILE *sdk, const char *type, const char *name) {
+  size_t buffer_size = 1024 * 4;
+  char *buffer = (char*) calloc(buffer_size, sizeof(char));
+  if (!buffer) return -1;
+  
+  char *sdk_response = tmpnam(); // this is unsafe, i know
+  if (mkfifo(sdk_response, 0666) != 0) { free(buffer); return -1; }
+  
+  memset(buffer, 0, buffer_size);
+  sprintf(buffer, "SPAN_START %s %s %s CLIENT HTTP", sdk_response, getenv("TRACEPARENT"), getenv("TRACESTATE"));
+  fwrite(buffer, sizeof(char), strlen(buffer), sdk);
+  free(buffer);
+  
+  memset(buffer, 0, buffer_size);
+  FILE *response_file = fopen(sdk_response, "r");
+  fread(buffer, sizeof(char), strlen(buffer), sdk_response);
+  fclose(response_file);
+  span_handle = atoi(buffer);
+  
+  remove(sdk_response);
+  free(buffer);
+  return span_handle;
+}
+
+char * otel_traceparent(FILE *sdk, int span_handle) {
+  size_t buffer_size = 1024 * 4;
+  char *buffer = (char*) calloc(buffer_size, sizeof(char));
+  if (!buffer) return -1;
+  
+  char *sdk_response = tmpnam(); // this is unsafe, i know
+  if (mkfifo(sdk_response, 0666) != 0) { free(buffer); return -1; }
+
+  memset(buffer, 0, buffer_size);
+  sprintf(buffer, "SPAN_TRACEPARENT %s %d", sdk_response, span_handle);
+  fwrite(buffer, sizeof(char), strlen(buffer), sdk);
+  free(buffer);
+
+  memset(buffer, 0, buffer_size);
+  FILE *response_file = fopen(sdk_response, "r");
+  fread(buffer, sizeof(char), strlen(buffer), sdk_response);
+  fclose(response_file);
+  span_handle = atoi(buffer);
+
+  remove(sdk_response);
+  return buffer;
+}
 
 int inject(char *buffer, size_t length) {
-  char* line_end = strchr(buffer, '\n');
+  char *line_end = strchr(buffer, '\n');
   if (!line_end) {
     return 1;
   }
@@ -22,12 +70,25 @@ int inject(char *buffer, size_t length) {
   while (!!line_end && line_end - buffer > 1) {
     *line_end = '\0';
     if (strstr(buffer, "traceparent: ") == buffer) {
-      // open pipe and
-      // start span CLIENT HTTP
-      // store the span handle in a predefined spot
-      // get the trace parent header
-      // replace the traceparent
-      fprintf(stderr, "%s %s\n", "DEBUG inject into", buffer);
+      FILE *sdk = fopen(getenv("OTEL_SHELL_INJECT_HTTP_SDK_PIPE"), "a+");
+      if (!sdk) return 4;
+      int span_handle = otel_span_start(sdk, "CLIENT", "HTTP");
+      if (span_handle < 0) return 5;
+      {
+        char span_handle_string[16];
+        memset(span_handle_string, 0, 16);
+        sprintf(span_handle_string, "%d", span_handle);
+        FILE *storage = fopen(getenv("OTEL_SHELL_INJECT_HTTP_HANDLE_FILE"), "w");
+        if (!storage) return 6;
+        fwrite(span_handle_string, sizeof(char), strlen(span_handle_string), storage); // TODO this could fail, what then?
+        fclose(storage);
+      }
+      char *traceparent = otel_traceparent(span_handle);
+      if (!traceparent) return 7;
+      if (strlen("traceparent: ") + strlen(traceparent) + 1 != strlen(buffer)) { free(traceparent); return 8; }
+      sprintf(buffer, "traceparent: %s\r", traceparent);
+      free(traceparent);
+      fclose(sdk); // this will leak on some early aborts, process terminate will clean it up
       *line_end = '\n';
       return 0;
     }
@@ -35,6 +96,7 @@ int inject(char *buffer, size_t length) {
     buffer = line_end + 1;
     line_end = strchr(buffer, '\n');
   }
+  
   return 3;
 }
 
