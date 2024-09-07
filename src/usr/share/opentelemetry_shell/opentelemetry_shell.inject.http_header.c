501e6199bf4e1a8d8fe3b32c043f51595d8ff1be
plengauer <p.lengauer@gmail.com>
2024-09-07 09:14:37 +0000

    added file

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.http_header.c
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.http_header.c
@@ -5,27 +5,56 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-ssize_t (*original_send)(int sockfd, const void *buf, size_t len, int flags);
+int inject(char *buffer, size_t length) {
+  char* line_end = strchr(buffer, '\n');
+  if (!line_end) {
+    return 1;
+  }
+  *line_end = '\0';
+  if (!strstr(buffer, " HTTP/")) {
+    *line_end = '\n';
+    return 2;
+  }
+  *line_end = '\n';
 
-ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
-/*
-    const char *header = "Custom-Header: MyValue\r\n";
-    if (strncmp(buf, "GET ", 4) == 0 || strncmp(buf, "POST ", 5) == 0) {
-        // Find the end of the headers
-        const char *end = strstr(buf, "\r\n\r\n");
-        if (end) {
-            char new_buf[4096];
-            int header_len = (end - (char *)buf) + 4;
-            snprintf(new_buf, sizeof(new_buf), "%.*s%s%.*s", header_len, (char *)buf, header, (int) len - header_len, end + 4);
-            return original_send(sockfd, new_buf, strlen(new_buf), flags);
-        }
+  buffer = line_end + 1;
+  line_end = strchr(buffer, '\n');
+  while (!!line_end && line_end - buffer > 1) {
+    *line_end = '\0';
+    if (strstr(buffer, "traceparent: ") == buffer) {
+      // open pipe and
+      // start span CLIENT HTTP
+      // store the span handle in a predefined spot
+      // get the trace parent header
+      // replace the traceparent
+      fprintf(stderr, "%s %s\n", "DEBUG inject into", buffer);
+      *line_end = '\n';
+      return 0;
     }
-*/
-    fprintf(stderr, "%s", "DEBUG\n");
+    *line_end = '\n';
+    buffer = line_end + 1;
+    line_end = strchr(buffer, '\n');
+  }
+  return 3;
+}
+
+
+ssize_t (*original_send)(int sockfd, const void *buf, size_t len, int flags);
+ssize_t send(int sockfd, const void *buf, size_t len, int flags) {
+    int result = inject((char *) buf, len);
+    fprintf(stderr, "DEBUG %d\n", result);
     return original_send(sockfd, buf, len, flags);
+}
+
+int (*original_SSL_write)(void *ssl, const void *buf, int num);
+int SSL_write(void *ssl, const void *buf, int num) {
+    int result = inject((char *) buf, (size_t) num);
+    fprintf(stderr, "DEBUG %d\n", result);
+    return original_SSL_write(ssl, buf, num);
 }
 
 __attribute__ ((constructor))
 void init() {
     original_send = dlsym(RTLD_NEXT, "send");
+    original_SSL_write = dlsym(RTLD_NEXT, "SSL_write");
 }
