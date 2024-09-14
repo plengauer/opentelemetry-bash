3871099cb30cea8408d7c5327a30a6ebdfa72b8a
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-14 14:05:56 +0200

    Update demo.sh

--- demos/context_propagation_http_curl/demo.sh
+++ demos/context_propagation_http_curl/demo.sh
@@ -1,4 +1,4 @@
 . otel.sh
-printf 'HTTP/1.1 418 I'\''m a teapot\r\ncontent-length: 0\r\n\r\n' | netcat -l 12345 &
+\otel4netcat_http ncat -l -c 'printf "HTTP/1.1 418 I'\''m a teapot\r\n\r\n"' 12345 &
 sleep 5
 curl http://127.0.0.1:12345
