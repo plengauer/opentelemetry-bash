eb9a33067f193b4a9abadbff865eea36a12b6c1c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-18 21:01:21 +0200

    Fix span kind for demos

--- demos/context_propagation_http_ncat/demo.sh
+++
@@ -1,4 +0,0 @@
-. otel.sh
-ncat -l -c 'printf "HTTP/1.1 418 I'\''m a teapot\r\n\r\n"' 12345 &
-sleep 5
-printf 'GET / HTTP/1.1\r\n\r\n' | ncat --no-shutdown 127.0.0.1 12345
---
+++ demos/context_propagation_http_netcat/demo.sh
@@ -0,0 +1,4 @@
+. otel.sh
+\otel4netcat_http ncat -l -c 'printf "HTTP/1.1 418 I'\''m a teapot\r\n\r\n"' 12345 & # inject with special command
+sleep 5
+printf 'GET / HTTP/1.1\r\n\r\n' | ncat --no-shutdown 127.0.0.1 12345
