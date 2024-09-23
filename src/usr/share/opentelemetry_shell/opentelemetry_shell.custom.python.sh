88103214063dd33faa777f8d2fbe756c99c3c3ef
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-23 14:49:26 +0200

    Update opentelemetry_shell.custom.python.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
@@ -7,7 +7,7 @@ _otel_inject_python_args() {
 }
 
 _otel_inject_python() {
-  if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ]; && _otel_string_starts_with "$("$1" -V | \cut -d ' ' f 2)" "3." then
+  if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ] && _otel_string_starts_with "$("$1" -V | \cut -d ' ' f 2)" "3."; then
     local cmdline="$(_otel_dollar_star "$@")"
     local cmdline="${cmdline#\\}"
     OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/python3.*/site-packages/ \eval _otel_call "$(_otel_inject_python_args "$@")"
