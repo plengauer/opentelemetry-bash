111d097f79aee77e2c9004b29475a441b8ba6085
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-26 15:29:19 +0200

    Update demo.sh

--- demos/injection_deep_python/demo.sh
+++ demos/injection_deep_python/demo.sh
@@ -1,7 +1,8 @@
+export OTEL_SHELL_CONFIG_INJECT_DEEP=TRUE
 . otel.sh
 python3 -m venv venv
 . venv/bin/activate
 pip install requests
-OTEL_SHELL_CONFIG_INJECT_DEEP=TRUE python script.py
+python script.py
 deactivate
 rm -rf venv
