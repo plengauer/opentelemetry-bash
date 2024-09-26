8378be04fe72b46afcf4ff340d6ae96c1f9bd9f0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-09-26 15:23:14 +0200

    Update opentelemetry_shell.custom.python.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
@@ -1,16 +1,13 @@
 #!/bin/false
 
-_otel_inject_python_args() {
-  _otel_escape_args "$1" /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument
-  \echo -n ' '
-  while \[ "$#" -gt 0 ]; do \echo -n ' '; _otel_escape_arg "$1"; shift; done
-}
-
 _otel_inject_python() {
   if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ] && _otel_string_starts_with "$("$1" -V | \cut -d ' ' f 2)" "3."; then
     local cmdline="$(_otel_dollar_star "$@")"
     local cmdline="${cmdline#\\}"
-    OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/python3.*/site-packages/ \eval _otel_call "$(_otel_inject_python_args "$@")"
+    if \[ "$1" != opentelemetry-instrument ] && ! _otel_string_ends_with "$1" /opentelemetry-instrument; then
+      set -- /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument "$@"
+    fi
+    OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/python3.*/site-packages/ _otel_call "$@"
   else
     _otel_call "$@"
   fi
@@ -18,9 +15,4 @@ _otel_inject_python() {
 
 _otel_alias_prepend python _otel_inject_python
 _otel_alias_prepend python3 _otel_inject_python
-
-_otel_inject_python_opentelemetry_instrument() {
-  _otel_call "$@" # TODO
-}
-
-_otel_alias_prepend opentelemetry-instrument _otel_inject_python_opentelemetry_instrument
+_otel_alias_prepend opentelemetry-instrument _otel_inject_python
