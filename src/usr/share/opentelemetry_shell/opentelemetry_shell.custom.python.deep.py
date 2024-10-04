10d715a47bc8774adadb3b0b01406a49d35ff809
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-04 22:33:15 +0200

    Update opentelemetry_shell.custom.python.deep.py

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.deep.py
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.deep.py
@@ -18,25 +18,25 @@ def inject_arguments(*args):
 # TODO get current span and set traceparent and tracestate as env var
 # TODO set additional env var like auto injection, ...
 
-def observed_subprocess_run(original_subprocess_run, *args, **kwargs):
-    if len(args) > 0:
-        args = inject_arguments(args)
-    return original_subprocess_run(*args, **kwargs)
-
-def observed_subprocess_call(original_subprocess_call, *args, **kwargs):
-    if len(args) > 0:
-        args = inject_arguments(args)
-    return original_subprocess_call(*args, **kwargs)
-
-def observed_subprocess_Popen___init__(original_subprocess_call, *args, **kwargs):
-    if len(args) > 0:
-        args = inject_arguments(args)
-    return observed_subprocess_Popen___init__(*args, **kwargs)
-
-def instrument(observed_function, original_function):
-    # functools.update_wrapper(observed_function, original_function) # TODO why do we need this?
-    return functools.partial(observed_function, original_function)
-
-subprocess.run = instrument(observed_subprocess_run, subprocess.run)
-subprocess.call = instrument(observed_subprocess_call, subprocess.call)
-subprocess.Popen.__init__ = instrument(observed_subprocess_Popen___init__, subprocess.Popen.__init__)
+# def observed_subprocess_run(original_subprocess_run, *args, **kwargs):
+#     if len(args) > 0:
+#         args = inject_arguments(args)
+#     return original_subprocess_run(*args, **kwargs)
+# 
+# def observed_subprocess_call(original_subprocess_call, *args, **kwargs):
+#     if len(args) > 0:
+#         args = inject_arguments(args)
+#     return original_subprocess_call(*args, **kwargs)
+# 
+# def observed_subprocess_Popen___init__(original_subprocess_call, *args, **kwargs):
+#     if len(args) > 0:
+#         args = inject_arguments(args)
+#     return observed_subprocess_Popen___init__(*args, **kwargs)
+# 
+# def instrument(observed_function, original_function):
+#    # functools.update_wrapper(observed_function, original_function) # TODO why do we need this?
+#    return functools.partial(observed_function, original_function)
+# 
+# subprocess.run = instrument(observed_subprocess_run, subprocess.run)
+# subprocess.call = instrument(observed_subprocess_call, subprocess.call)
+# subprocess.Popen.__init__ = instrument(observed_subprocess_Popen___init__, subprocess.Popen.__init__)
