4b2c5a516a90cc83d3a0338dcee2de8e2252bba1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-04 14:11:55 +0200

    Update opentelemetry_shell.custom.python.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
@@ -4,7 +4,6 @@ _otel_inject_python() {
 \echo "DEBUG DEBUG $*" >&2
 if _otel_python_is_venv_active; then \echo "DEBUG DEBUG venv is active" >&2; fi
 if _otel_python_is_venv_path_injectable; then \echo "DEBUG DEBUG venv is injectable" >&2; fi
-\echo "DEBUG DEBUG $*" >&2
   if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ] && _otel_string_starts_with "$(\eval "$1 -V" | \cut -d ' ' -f 2)" "3." && (! _otel_python_is_venv_active || _otel_python_is_venv_path_injectable) && ! _otel_string_ends_with "$2" /pip; then # TODO support pip
     local cmdline="$(_otel_dollar_star "$@")"
     local cmdline="${cmdline#\\}"
@@ -13,6 +12,7 @@ if _otel_python_is_venv_path_injectable; then \echo "DEBUG DEBUG venv is injecta
 \echo "DEBUG DEBUG $cmdline => $*" >&2
     OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"$PYTHONPATH" OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1 _otel_call "$@"
   else
+\echo "DEBUG DEBUG $cmdline => $cmdline" >&2
     _otel_call "$@"
   fi
 }
