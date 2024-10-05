a5822964f5789d32d1004f88aaea72abddc13cb7
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-05 10:00:59 +0200

    Update opentelemetry_shell.custom.python.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
@@ -1,22 +1,33 @@
 #!/bin/false
 
 _otel_inject_python() {
-  if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ] && _otel_string_starts_with "$(\eval "$1 -V" | \cut -d ' ' -f 2)" "3." && (! _otel_python_is_venv_active || _otel_python_is_venv_path_injectable); then
-    local cmdline="$(_otel_dollar_star "$@")"
-    local cmdline="${cmdline#\\}"
-    local command="$1"; shift
-    set -- "$command" /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument "${command#\\}" "$@"
-    OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"$PYTHONPATH" OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1 _otel_call "$@"
+  if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ] && _otel_string_starts_with "$(\eval "$1 -V" | \cut -d ' ' -f 2)" "3." && ! _otel_string_ends_with "$2" /pip && ! _otel_string_ends_with "$2" /pip3; then
+    if _otel_python_is_customize_injectable; then
+      local cmdline="$(_otel_dollar_star "$@")"
+      local cmdline="${cmdline#\\}"
+      local command="$1"; shift
+      set -- "$command" /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument "${command#\\}" "$@"
+      OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"$PYTHONPATH" OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1 _otel_call "$@"
+    elif _otel_python_is_venv_active; then
+      # TODO parse -c or stdin or file
+      _otel_call "$@"
+    else
+      _otel_call "$@"
+    fi
   else
     _otel_call "$@"
   fi
+}
+
+_otel_python_is_customize_injectable() {
+ ! _otel_python_is_venv_active || _otel_python_is_venv_customize_injectable
 }
 
 _otel_python_is_venv_active() {
   \[ -n "${VIRTUAL_ENV:-}" ]
 }
 
-_otel_python_is_venv_path_injectable() {
+_otel_python_is_venv_customize_injectable() {
   \[ -d "${VIRTUAL_ENV:-}" ] && \cat "$VIRTUAL_ENV"/pyvenv.cfg | \grep -q -- '^include-system-site-packages = true$'
 }
 
