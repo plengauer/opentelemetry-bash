142aea60478ab69bee221648d21ed3b8dc7f45af
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-05 10:34:23 +0200

    Update opentelemetry_shell.custom.python.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.sh
@@ -9,8 +9,15 @@ _otel_inject_python() {
       set -- "$command" /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument "${command#\\}" "$@"
       OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"$PYTHONPATH" OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1 _otel_call "$@"
     elif _otel_python_is_venv_active; then
-      # TODO parse -c or stdin or file
-      _otel_call "$@"
+      \eval "set -- $(_otel_python_inject_args "$@")"
+      local command="$1"; shift
+      set -- "$command" /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument "${command#\\}" "$@"
+      if \[ "$_otel_python_code_source" = stdin ]; then
+        unset _otel_python_code_source
+        { \cat /usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.deep.py; \cat; } | OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"$PYTHONPATH" OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1 _otel_call "$@"
+      else
+        OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"$PYTHONPATH" OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1 _otel_call "$@"
+      fi
     else
       _otel_call "$@"
     fi
@@ -29,6 +36,32 @@ _otel_python_is_venv_active() {
 
 _otel_python_is_venv_customize_injectable() {
   \[ -d "${VIRTUAL_ENV:-}" ] && \cat "$VIRTUAL_ENV"/pyvenv.cfg | \grep -q -- '^include-system-site-packages = true$'
+}
+
+_otel_python_inject_args() {
+  _otel_escape_arg "$1"; shift
+  while \[ "$#" -gt 0 ]; do
+    \echo -n ' '
+    local arg="$1"; shift
+    if \[ -n "${injected:-}" ]; then
+      _otel_escape_arg "$arg"
+    elif \[ "$arg" = -c ]; then
+      _otel_escape_arg "$arg"
+      \echo -n ' '
+      local arg="$1"; shift
+      _otel_escape_arg "$(\cat /usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.deep.py)
+$arg"
+      local injected=cmdline
+    elif \[ -f "$arg" ]; then
+      _otel_escape_args -c "$(\cat /usr/share/opentelemetry_shell/opentelemetry_shell.custom.python.deep.py)
+with open('$arg', 'r') as file:
+  exec(file.read())"
+      local injected=file
+    fi
+  done
+  if \[ -z "$injected" ]; then
+    _otel_python_code_source=stdin
+  fi
 }
 
 _otel_inject_opentelemetry_instrument() {
