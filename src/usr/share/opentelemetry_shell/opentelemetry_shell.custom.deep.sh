3d357003c7d8db06743b0d720fd47b0500b5d0d0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-07 22:46:56 +0200

    Update opentelemetry_shell.custom.deep.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.deep.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.custom.deep.sh
@@ -5,3 +5,12 @@ _otel_inject_deep() {
 }
 
 _otel_alias_prepend perl _otel_inject_deep
+
+_otel_alias_prepend csh _otel_inject_deep
+_otel_alias_prepend ksh _otel_inject_deep
+_otel_alias_prepend zsh _otel_inject_deep
+_otel_alias_prepend pdksh _otel_inject_deep
+_otel_alias_prepend posh _otel_inject_deep
+_otel_alias_prepend yash _otel_inject_deep
+_otel_alias_prepend bosh _otel_inject_deep
+_otel_alias_prepend fish _otel_inject_deep
