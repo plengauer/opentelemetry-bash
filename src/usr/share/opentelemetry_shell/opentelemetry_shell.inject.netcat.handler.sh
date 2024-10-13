e9b1a9ddf6c93c2b3eff226cedd843b58900b755
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-13 19:54:52 +0200

    Update opentelemetry_shell.inject.netcat.handler.sh

--- src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.netcat.handler.sh
+++ src/usr/share/opentelemetry_shell/opentelemetry_shell.inject.netcat.handler.sh
@@ -19,7 +19,7 @@ if \[ "$OTEL_SHELL_CONFIG_NETCAT_ASSUME_REQUEST_RESPONSE" = TRUE ]; then
   span_handle_file_2="$(\mktemp -u)_opentelemetry_shell_$$.netcat.response.span_handle"
   \mkfifo "$span_handle_file_0" "$span_handle_file_1" "$span_handle_file_2"
   (\cat "$span_handle_file_0" | \tee "$span_handle_file_1" | \tee "$span_handle_file_2" > /dev/null &)
-  _otel_netcat_parse_request 1 "$span_handle_file_0" $netcat_command_string | { unset OTEL_SHELL_CONFIG_NETCAT_READ_TIMEOUT; otel_span_activate"$(\cat "$span_handle_file_1")"; \eval "$handler_command_string" || \echo "$?" > "$exit_code_file"; } | _otel_netcat_parse_response 1 "$span_handle_file_2"
+  _otel_netcat_parse_request 1 "$span_handle_file_0" $netcat_command_string | { unset OTEL_SHELL_CONFIG_NETCAT_READ_TIMEOUT; otel_span_activate "$(\cat "$span_handle_file_1")"; \eval "$handler_command_string" || \echo "$?" > "$exit_code_file"; } | _otel_netcat_parse_response 1 "$span_handle_file_2"
   exit_code="$(\cat "$exit_code_file")"
   \rm "$span_handle_file_0" "$span_handle_file_1" "$span_handle_file_2" "$exit_code_file" 2> /dev/null
 else
