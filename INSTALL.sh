21e6a180619693b10b0c408aae72738f3175f5d3
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-10-25 16:24:42 +0200

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 set -e
-#TODO check if debian or RPM and install accordingly
-package=$(mktemp -u)_opentelemetry_shell.deb
-wget -O $package $(curl --no-progress-meter https://api.github.com/repos/plengauer/opentelemetry-bash/releases/latest | jq -r '.assets[].browser_download_url') || \
-wget -O $package https://github.com/plengauer/opentelemetry-bash/releases/latest/download/opentelemetry-shell_$(curl -v --no-progress-meter https://github.com/plengauer/opentelemetry-bash/releases/latest/download/opentelemetry-shell.deb 2>&1 | grep location | awk -F\  '{ print $3 }' | awk -F/ '{ print $8 }' | awk -Fv '{ print $2 }' | xargs).deb
-if [ "$(whoami)" = "root" ]; then
-  apt-get install -y $package
+
+if type apt-get 1> /dev/null 2> /dev/null; then
+  extension=deb
+elif type rpm 1> /dev/null 2> /dev/null; then
+  extension=rpm
 else
-  sudo -E apt-get install -y $package
+  echo "Unsupported operating system (no apt-get and no rpm available)" >&2
+  exit 1
 fi
-rm $package
+
+package="$(mktemp -u)"."$extension"
+curl --no-progress-meter https://api.github.com/repos/plengauer/opentelemetry-bash/releases/latest | jq -r '.assets[].browser_download_url' | grep '.'"$extension"'$' | xargs wget -O "$package" \
+ || curl -v --no-progress-meter https://github.com/plengauer/opentelemetry-bash/releases/latest/download/opentelemetry-shell."$extension" 2>&1 | grep location | awk -F\  '{ print $3 }' | awk -F/ '{ print $8 }' | awk -Fv '{ print $2 }' | xargs -I '{}' wget -O "$package" https://github.com/plengauer/opentelemetry-bash/releases/latest/download/opentelemetry-shell_'{}'."$extension"
+
+case "$extension" in
+  deb)
+    if [ "$(whoami)" = "root" ]; then
+      apt-get install -y "$package"
+    else
+      sudo -E apt-get install -y "$package"
+    fi
+    ;;
+  rpm)
+    if [ "$(whoami)" = "root" ]; then
+      rpm --install "$package"
+    else
+      sudo -E rpm --install "$package"
+    fi
+    ;;
+  *) echo Here be dragons >&2; exit 1;;
+esac
+
+rm "$package"
