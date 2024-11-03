9472e5dc6f730e930c7d5b0a6cab464198de95cc
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-11-03 07:42:20 +0000

    Fix shell injection with exotic argument ordering

--- tests/auto/test_auto_injection_c.sh
+++ tests/auto/test_auto_injection_c.sh
@@ -1,9 +1,9 @@
 . ./assert.sh
 . /usr/bin/opentelemetry_shell.sh
 
-sh -c 'echo hello world'
+sh -c -- 'echo hello world'
 assert_equals 0 $?
-span="$(resolve_span '.resource.attributes."process.command_line" == "sh -c echo hello world"')"
+span="$(resolve_span '.resource.attributes."process.command_line" == "sh -c -- echo hello world"')"
 assert_equals "echo hello world" "$(\echo "$span" | jq -r '.name')"
 assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
 
