2d01832f04be577bff1e418cd54fd3c06f2ef6e5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-11-03 07:42:47 +0000

    Fix missing spans in renovate action

--- src/usr/share/opentelemetry_shell/agent.instrumentation.renovate.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.renovate.sh
@@ -3,6 +3,7 @@
 if _otel_string_contains "$(_otel_resolve_command_self)" /usr/local/bin/renovate || _otel_string_contains "$(_otel_resolve_command_self)" /usr/local/sbin/renovate; then # renovate looks at some very specific env vars to enable tracing
   export OTEL_SHELL_CONFIG_INJECT_DEEP=TRUE
   export OTEL_SHELL_CONFIG_INSTRUMENT_ABSOLUTE_PATHS=TRUE
+  export OTEL_BSP_MAX_EXPORT_BATCH_SIZE=1
   if _otel_string_contains "$OTEL_TRACES_EXPORTER" console; then export RENOVATE_TRACING_CONSOLE_EXPORTER=true; fi
   if \[ -z "$OTEL_EXPORTER_OTLP_ENDPOINT" ] && ( _otel_string_contains "$OTEL_TRACES_EXPORTER" otlp || \[ -z "$OTEL_TRACES_EXPORTER" ] ); then
     if \[ -n "$OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" ]; then
