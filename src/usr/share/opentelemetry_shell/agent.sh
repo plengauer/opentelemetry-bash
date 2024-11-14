029de944025bb5443fa5cebba5a760f490a1e6a2
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-11-14 09:38:28 +0100

    Update agent.sh

--- src/usr/share/opentelemetry_shell/agent.sh
+++ src/usr/share/opentelemetry_shell/agent.sh
@@ -208,7 +208,7 @@ _otel_resolve_shebang() {
   if \[ -z "$path" ] || ! \[ -x "$path" ]; then return 1; fi
   read -r first_line < "$path"
   if ! _otel_string_starts_with "$first_line" "#!"; then return 2; fi
-  local shebang="${first_line#\#!}"
+  local shebang="${first_line#\#\!}"
   local shebang="${shebang#"${shebang%%[![:space:]]*}"}"
   \echo "$shebang"
 }
