c09f4005b0997188d1a72ec535ccc623a9755324
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-11-20 10:23:19 +0100

    Fix deep injections (accidentally off)

--- src/usr/share/opentelemetry_shell/agent.instrumentation.deep.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.deep.sh
@@ -2,7 +2,7 @@
 
 _otel_inject_deep() {
   if \type strace 1> /dev/null 2> /dev/null; then
-    _otel_call_and_record_subprocesses "$(otel_span_current)" _otel_call "$@"
+    OTEL_SHELL_CONFIG_OBSERVE_SUBPROCESSES=TRUE _otel_call_and_record_subprocesses "$(otel_span_current)" _otel_call "$@"
   else
     _otel_call "$@"
   fi
