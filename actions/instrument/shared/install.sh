f78ebc4cca6e9c851381ea1dff700f94b10606ec
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-06 17:09:55 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -5,7 +5,7 @@ npm install '@actions/cache'
 export GITHUB_ACTION_REPOSITORY="${GITHUB_ACTION_REPOSITORY:-"$GITHUB_REPOSITORY"}"
 action_tag_name="$(echo "$GITHUB_ACTION_REF" | cut -sd @ -f 2-)"
 if [ -z "$action_tag_name" ]; then action_tag_name="v$(cat ../../../VERSION)"; fi
-if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && dpkg -l | grep -q opentelemetry-shell; then
+if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && dpkg -l | grep -q opentelemetry-shell && false; then
   :
 elif [ -n "$action_tag_name" ]; then
   debian_file="$(mktemp -u).deb"
