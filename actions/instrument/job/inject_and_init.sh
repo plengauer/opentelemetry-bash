9f5e4b65e087abce3e7552eb6d934a7c110b8622
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-06 15:17:00 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -1,40 +1,11 @@
 #!/bin/bash
 set -e
+
 my_dir="$(echo "$0" | rev | cut -d / -f 2- | rev)"
-
-github() {
-  url="$GITHUB_API_URL"/"$1"?per_page=100
-  curl --no-progress-meter --fail --retry 16 --retry-all-errors --head "$url" \
-    | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
-    | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
-    | grep '^page=' | cut -d = -f 2 \
-    | xargs seq 1 | while IFS= read -r page; do
-      curl --no-progress-meter --fail --retry 16 --retry-all-errors "$url"\&page="$page"
-    done
-}
-export -f github
-
-github_workflow() {
-  github repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/"$1"
-}
-export -f github_workflow
+. "$my_dir"/../shared/github.sh
+bash -e "$my_dir"/../shared/install.sh
 
 echo "$GITHUB_ACTION" > /tmp/opentelemetry_shell_action_name
-
-if [ -z "$GITHUB_ACTION_REPOSITORY" ]; then export GITHUB_ACTION_REPOSITORY="$GITHUB_REPOSITORY"; fi
-action_tag_name="$(echo "$GITHUB_ACTION_REF" | cut -sd @ -f 2-)"
-if [ -z "$action_tag_name" ]; then action_tag_name="v$(cat "$my_dir"/../../../VERSION)"; fi
-if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && dpkg -l | grep -q opentelemetry-shell; then
-  :
-elif [ -n "$action_tag_name" ]; then
-  debian_file="$(mktemp -u).deb"
-  github repos/"$GITHUB_ACTION_REPOSITORY"/releases | { if [ "$action_tag_name" = main ]; then jq '.[0]'; else jq '.[] | select(.tag_name=="'"$action_tag_name"'")'; fi } | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O "$debian_file"
-  sudo -E apt-get install -y "$debian_file"
-  rm "$debian_file"
-else
-  wget -O - https://raw.githubusercontent.com/"$GITHUB_ACTION_REPOSITORY"/main/INSTALL.sh | sh
-fi
-npm install '@actions/artifact'
 
 new_path_dir="/tmp/otel/bin"
 mkdir -p "$new_path_dir"
