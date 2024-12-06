ef24a59afac90e50c9278297d9c8314f95bb0644
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-06 20:37:19 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -8,13 +8,13 @@ elif [ -n "$action_tag_name" ]; then
   packages_cache_directory=/tmp/opentelemetry-shell-debians
   mkdir -p "$packages_cache_directory"
   npm install '@actions/cache'
-  cache_key="${GITHUB_ACTION_REPOSITORY}-${action_tag_name}-$(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
+  cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
   node -e "require('@actions/cache').restoreCache(['$packages_cache_directory'], '$cache_key');"
   debian_file="$packages_cache_directory"/opentelemetry-shell.deb
   if ! [ -f "$debian_file" ]; then
     github repos/"$GITHUB_ACTION_REPOSITORY"/releases | ( [ "$action_tag_name" = main ] && jq '.[0]' || jq '.[] | select(.tag_name=="'"$action_tag_name"'")' ) | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O "$debian_file"
     # TODO download dependencies
-    node -e "await require('@actions/cache').saveCache(['$packages_cache_directory'], '$cache_key');"
+    node -e "require('@actions/cache').saveCache(['$packages_cache_directory'], '$cache_key');"
   fi
   sudo -E apt-get install -y "$packages_cache_directory"/*.deb
 else
