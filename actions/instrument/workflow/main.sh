0a688388db9d1976a1c1da903064c4fe5d122234
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-06 15:16:30 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -1,35 +1,10 @@
+#/bin/bash
 set -e
-if [ "$GITHUB_JOB" != observe ]; then echo "Job name must be 'observe'!" >&2; exit 1; fi 
+if [ "$GITHUB_JOB" != observe ]; then echo "Job name must be 'observe'!" >&2; exit 1; fi
 
-github() {
-  url="$GITHUB_API_URL"/"$1"?per_page=100
-  curl --no-progress-meter --fail --retry 16 --retry-all-errors --head "$url" \
-    | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
-    | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
-    | grep '^page=' | cut -d = -f 2 \
-    | xargs seq 1 | while IFS= read -r page; do
-      command curl --no-progress-meter --fail --retry 12 --retry-all-errors "$url"\&page="$page"
-    done
-}
-
-github_workflow() {
-  github repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/"$1"
-}
-
-export OTEL_SHELL_CONFIG_INSTALL_DEEP=FALSE
-export GITHUB_ACTION_REPOSITORY="${GITHUB_ACTION_REPOSITORY:-"$GITHUB_REPOSITORY"}"
-action_tag_name="$(echo "$GITHUB_ACTION_REF" | cut -sd @ -f 2-)"
-if [ -z "$action_tag_name" ]; then action_tag_name="v$(cat "$(echo "$0" | rev | cut -d / -f 2- | rev)"/../../../VERSION)"; fi
-if [ -n "$action_tag_name" ]; then
-  debian_file="$(mktemp -u).deb"
-  github repos/"$GITHUB_ACTION_REPOSITORY"/releases | { if [ "$action_tag_name" = main ]; then jq '.[0]'; else jq '.[] | select(.tag_name=="'"$action_tag_name"'")'; fi } | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O "$debian_file"
-  sudo apt-get install -y "$debian_file"
-  rm "$debian_file"
-else
-  wget -O - https://raw.githubusercontent.com/"$GITHUB_ACTION_REPOSITORY"/main/INSTALL.sh | sh
-fi
-unset OTEL_SHELL_CONFIG_INSTALL_DEEP
-npm install '@actions/artifact'
+my_dir="$(echo "$0" | rev | cut -d / -f 2- | rev)"
+. "$my_dir"/../shared/github.sh
+OTEL_SHELL_CONFIG_INSTALL_DEEP=FALSE bash -e "$my_dir"/../shared/install.sh
 
 export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-"$(echo "$GITHUB_REPOSITORY" | cut -d / -f 2-) CI"}"
 
