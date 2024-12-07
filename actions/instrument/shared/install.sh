7af831e588800c70e31a404ccc8b1b413c1b43a5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-07 16:50:02 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -6,6 +6,10 @@ if [ -n "$action_tag_name" ]; then
   if [ "$INPUT_CACHE" = "true" ]; then
     npm install '@actions/cache' # TODO where to get this from? just install and deal with it
     cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
+    ls -la $(sudo pip cache dir)
+    ls -la $(pip cache dir)
+    ls -la $(sudo npm config get cache)
+    ls -la $(npm config get cache)
     sudo node -e "require('@actions/cache').restoreCache(['/tmp/opentelemetry-shell.deb', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
   fi
   debian_file=/tmp/opentelemetry-shell.deb
