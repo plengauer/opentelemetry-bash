1cbcc65f3ee6eac5f590bbc39a315d54179a66c1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-07 23:37:25 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -6,16 +6,21 @@ if [ -n "$action_tag_name" ]; then
   if [ "$INPUT_CACHE" = "true" ]; then
     npm install '@actions/cache' # TODO where to get this from? just install and deal with it
     cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
-    sudo -E -H node -e "require('@actions/cache').restoreCache(['/tmp/opentelemetry-shell.deb', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
+    sudo -E -H node -e "require('@actions/cache').restoreCache(['/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
   fi
-  debian_file=/tmp/opentelemetry-shell.deb
+  debian_file=/var/cache/apt/archives/opentelemetry-shell_$(cat ../../../VERSION)_all.deb
   if ! [ -f "$debian_file" ]; then
-    [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && [ -f package.deb ] && mv package.deb "$debian_file" || github repos/"$GITHUB_ACTION_REPOSITORY"/releases | ( [ "$action_tag_name" = main ] && jq '.[0]' || jq '.[] | select(.tag_name=="'"$action_tag_name"'")' ) | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O "$debian_file"
-    sudo apt-get install -y --download-only "$debian_file"
-    [ -z "${cache_key:-}" ] || sudo -E -H node -e "require('@actions/cache').saveCache(['/tmp/opentelemetry-shell.deb', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
+    if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && [ -f package.deb ]; then
+      sudo mv package.deb "$debian_file"
+    else
+      github repos/"$GITHUB_ACTION_REPOSITORY"/releases | ( [ "$action_tag_name" = main ] && jq '.[0]' || jq '.[] | select(.tag_name=="'"$action_tag_name"'")' ) | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O - | sudo tee "$debian_file" > /dev/null
+    fi
   fi
-  sudo -E -H apt-get install -y /tmp/opentelemetry-shell.deb
-  rm "$debian_file"
+  sudo -E -H apt-get -o Binary::apt::APT::Keep-Downloaded-Packages=true install -y /tmp/opentelemetry-shell.deb
+  if [ -n "${cache_key:-}" ]; then
+    sudo -E -H node -e "require('@actions/cache').saveCache(['/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
+  fi
+  sudo rm "$debian_file"
 else
   wget -O - https://raw.githubusercontent.com/"$GITHUB_ACTION_REPOSITORY"/main/INSTALL.sh | sh
 fi
