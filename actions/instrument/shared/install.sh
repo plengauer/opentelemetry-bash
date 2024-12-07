97b66fe47ac48477b32fc2062356bea769251460
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-07 16:28:29 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -3,7 +3,6 @@ export GITHUB_ACTION_REPOSITORY="${GITHUB_ACTION_REPOSITORY:-"$GITHUB_REPOSITORY
 action_tag_name="$(echo "$GITHUB_ACTION_REF" | cut -sd @ -f 2-)"
 if [ -z "$action_tag_name" ]; then action_tag_name="v$(cat ../../../VERSION)"; fi
 if [ -n "$action_tag_name" ]; then
-  mkdir -p "$packages_cache_directory"
   if [ "$INPUT_CACHE" = "true" ]; then
     npm install '@actions/cache' # TODO where to get this from? just install and deal with it
     cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
@@ -11,13 +10,13 @@ if [ -n "$action_tag_name" ]; then
     pip cache dir
     sudo npm config get cache
     npm config get cache
-    sudo node -e "require('@actions/cache').restoreCache(['$packages_cache_directory', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
+    sudo node -e "require('@actions/cache').restoreCache(['/tmp/opentelemetry-shell.deb', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
   fi
   debian_file=/tmp/opentelemetry-shell.deb
   if ! [ -f "$debian_file" ]; then
     [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && [ -f package.deb ] && mv package.deb "$debian_file" || github repos/"$GITHUB_ACTION_REPOSITORY"/releases | ( [ "$action_tag_name" = main ] && jq '.[0]' || jq '.[] | select(.tag_name=="'"$action_tag_name"'")' ) | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O "$debian_file"
     sudo apt-get install -y --download-only "$debian_file"
-    [ -z "${cache_key:-}" ] || sudo node -e "require('@actions/cache').saveCache(['$packages_cache_directory', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
+    [ -z "${cache_key:-}" ] || sudo node -e "require('@actions/cache').saveCache(['/tmp/opentelemetry-shell.deb', '/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)'], '$cache_key');"
   fi
   sudo -E apt-get install -y "$packages_cache_directory"/opentelemetry-shell.deb
   rm "$debian_file"
