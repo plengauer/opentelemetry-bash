501f8e2000f233954fd56de0279b6dd7ccf2e626
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-07 13:07:57 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -7,14 +7,16 @@ if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && dpkg -l | grep -q o
 elif [ -n "$action_tag_name" ]; then
   packages_cache_directory=/tmp/opentelemetry-shell-debians
   mkdir -p "$packages_cache_directory"
-  npm install '@actions/cache' # TODO where to get this from? just install and deal with it
-  cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
-  [ "$INPUT_CACHE" != "true" ] || node -e "require('@actions/cache').restoreCache(['$packages_cache_directory', '/var/cache/apt/archives/*.deb', '/home/runner/.cache/pip', '/home/runner/.npm'], '$cache_key');"
+  if [ "$INPUT_CACHE" = "true" ]; then
+    npm install '@actions/cache' # TODO where to get this from? just install and deal with it
+    cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
+    node -e "require('@actions/cache').restoreCache(['$packages_cache_directory', '/var/cache/apt/archives/*.deb', '/home/runner/.cache/pip', '/home/runner/.npm'], '$cache_key');"
+  fi
   debian_file="$packages_cache_directory"/opentelemetry-shell.deb
   if ! [ -f "$debian_file" ]; then
     github repos/"$GITHUB_ACTION_REPOSITORY"/releases | ( [ "$action_tag_name" = main ] && jq '.[0]' || jq '.[] | select(.tag_name=="'"$action_tag_name"'")' ) | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O "$debian_file"
     sudo apt-get install -y --download-only "$debian_file"
-    [ "$INPUT_CACHE" != "true" ] || node -e "require('@actions/cache').saveCache(['$packages_cache_directory', '/var/cache/apt/archives/*.deb', '/home/runner/.cache/pip', '/home/runner/.npm'], '$cache_key');"
+    [ -z "${cache_key:-}" ] || node -e "require('@actions/cache').saveCache(['$packages_cache_directory', '/var/cache/apt/archives/*.deb', '/home/runner/.cache/pip', '/home/runner/.npm'], '$cache_key');"
   fi
   sudo -E apt-get install -y "$packages_cache_directory"/opentelemetry-shell.deb
   rm "$packages_cache_directory"/opentelemetry-shell.deb
