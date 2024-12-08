b66fb2b6b00846bef6ee1a001256c8f19615929d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-08 17:15:03 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -4,9 +4,9 @@ action_tag_name="$(echo "$GITHUB_ACTION_REF" | cut -sd @ -f 2-)"
 if [ -z "$action_tag_name" ]; then action_tag_name="v$(cat ../../../VERSION)"; fi
 if [ -n "$action_tag_name" ]; then
   if [ "$INPUT_CACHE" = "true" ]; then
-    npm install '@actions/cache' # TODO where to get this from? just install and deal with it
+    npm install '@actions/cache' # TODO where to get this from? just install and deal with it, or do we wanna install it into the repository to not have to fetch it?
     cache_key="${GITHUB_ACTION_REPOSITORY} ${action_tag_name} $(cat /etc/os-release | grep 'VERSION=' | cut -d = -f 2- | tr -d \")"
-    sudo -E -H node -e "require('@actions/cache').restoreCache(['/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)', '/opt/opentelemetry_shell/sdk/venv', '/opt/opentelemetry_shell/venv', '/opt/opentelemetry_shell/node_modules'], '$cache_key');"
+    sudo -E -H node -e "require('@actions/cache').restoreCache(['/var/cache/apt/archives/*.deb', '/opt/opentelemetry_shell/sdk/venv', '/opt/opentelemetry_shell/venv', '/opt/opentelemetry_shell/node_modules'], '$cache_key');"
   fi
   debian_file=/var/cache/apt/archives/opentelemetry-shell_$(cat ../../../VERSION)_all.deb
   if ! [ -f "$debian_file" ]; then
@@ -15,13 +15,14 @@ if [ -n "$action_tag_name" ]; then
     else
       github repos/"$GITHUB_ACTION_REPOSITORY"/releases | ( [ "$action_tag_name" = main ] && jq '.[0]' || jq '.[] | select(.tag_name=="'"$action_tag_name"'")' ) | jq -r '.assets[].browser_download_url' | grep '.deb$' | xargs wget -O - | sudo tee "$debian_file" > /dev/null
     fi
+    write_back_cache=TRUE
   fi
   OTEL_SHELL_CONFIG_INSTALL_ASSUME=TRUE sudo -E -H apt-get -o Binary::apt::APT::Keep-Downloaded-Packages=true install -y "$debian_file"
-  if [ -n "${cache_key:-}" ]; then
-    sudo -E -H node -e "require('@actions/cache').saveCache(['/var/cache/apt/archives/*.deb', '$(sudo pip cache dir)', '$(sudo npm config get cache)', '/opt/opentelemetry_shell/sdk/venv', '/opt/opentelemetry_shell/venv', '/opt/opentelemetry_shell/node_modules'], '$cache_key');"
+  if [ "$write_back_cache" = TRUE ]; then
+    sudo -E -H node -e "require('@actions/cache').saveCache(['/var/cache/apt/archives/*.deb', '/opt/opentelemetry_shell/sdk/venv', '/opt/opentelemetry_shell/venv', '/opt/opentelemetry_shell/node_modules'], '$cache_key');"
   fi
   sudo rm "$debian_file"
 else
   wget -O - https://raw.githubusercontent.com/"$GITHUB_ACTION_REPOSITORY"/main/INSTALL.sh | sh
 fi
-npm install '@actions/artifact' # TODO include in cache
+npm install '@actions/artifact' # do we want to include this also in the cache?
