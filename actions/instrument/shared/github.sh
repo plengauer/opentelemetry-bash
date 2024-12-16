c168e7d6e2874c4f6dc2a742cb5f53eba0ea7353
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-16 16:56:36 +0100

    Setup caches in GitHub actions to increase setup performance

---
+++ actions/instrument/shared/github.sh
@@ -0,0 +1,18 @@
+#!/bin/false
+
+github() {
+  url="$GITHUB_API_URL"/"$1"?per_page=100
+  curl --no-progress-meter --fail --retry 16 --retry-all-errors --head "$url" \
+    | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
+    | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
+    | grep '^page=' | cut -d = -f 2 \
+    | xargs seq 1 | while IFS= read -r page; do
+      curl --no-progress-meter --fail --retry 16 --retry-all-errors "$url"\&page="$page"
+    done
+}
+export -f github
+
+github_workflow() {
+  github repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/"$1"
+}
+export -f github_workflow
