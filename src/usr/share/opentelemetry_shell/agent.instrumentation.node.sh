c6b94719c714dd2b8ca6e45e49eb8ed6bf9325b9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-23 10:39:26 +0100

    Update agent.instrumentation.node.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.node.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.node.sh
@@ -27,24 +27,24 @@ _otel_inject_node_args() {
     elif _otel_string_starts_with "$1" -; then
       _otel_escape_arg "$1"; shift
     else
-      if \[ "$OTEL_SHELL_CONFIG_INJECT_DEEP" = TRUE ] && \[ -d "$(\readlink -f /usr/share/opentelemetry_shell/node_modules)" ]; then
-        if \[ "$next_is_code" = TRUE ]; then
+      if \[ "${OTEL_SHELL_CONFIG_INJECT_DEEP:-}" = TRUE ] && \[ -d "$(\readlink -f /usr/share/opentelemetry_shell/node_modules)" ]; then
+        if \[ "${next_is_code:-FALSE}" = TRUE ]; then
           _otel_escape_arg "$1"; \echo -n ' '
           local base_dir="$(\echo "$1" | \rev | \cut -d / -f 2- | \rev)"
         else
           local base_dir="$(\pwd)"
         fi
         local dir="$base_dir"
-        while [ -n "$dir" ] && ! \[ -d "$dir"/node_modules ] && ! \[ -f "$dir"/package.json ] && ! \[ -f "$dir"/package-lock.json ]; do
+        while [ -n "${dir:-}" ] && ! \[ -d "$dir"/node_modules ] && ! \[ -f "$dir"/package.json ] && ! \[ -f "$dir"/package-lock.json ]; do
           local dir="$(\echo "$dir" | \rev | \cut -sd / -f 2- | \rev)"
         done
-        if \[ -z "$dir" ]; then local dir="$base_dir"; fi
+        if \[ -z "${dir:-}" ]; then local dir="$base_dir"; fi
         if _otel_is_node_injected "$dir"; then
           _otel_escape_args --require /usr/share/opentelemetry_shell/agent.instrumentation.node.deep.link.js
-        elif \[ -z "$OTEL_TRACES_EXPORTER" ] || \[ "$OTEL_TRACES_EXPORTER" = console ] || \[ "$OTEL_TRACES_EXPORTER" = otlp ]; then
+        elif \[ -z "${OTEL_TRACES_EXPORTER:-}" ] || \[ "${OTEL_TRACES_EXPORTER:-}" = console ] || \[ "${OTEL_TRACES_EXPORTER:-}" = otlp ]; then
           _otel_escape_args --require /usr/share/opentelemetry_shell/agent.instrumentation.node.deep.instrument.js
         fi
-        if ! \[ "$next_is_code" = TRUE ]; then \echo -n ' '; _otel_escape_arg "$1"; fi
+        if ! \[ "${next_is_code:-FALSE}" = TRUE ]; then \echo -n ' '; _otel_escape_arg "$1"; fi
       else
         _otel_escape_arg "$1"
       fi
