f0472675f0134f78b9d43f69f79d9c250df822e8
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-12-30 13:51:23 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -22,7 +22,7 @@ for dir in unit sdk auto integration; do
     ( while true; do cat $OTEL_SHELL_SDK_OUTPUT_REDIRECT >> $OTEL_EXPORT_LOCATION; done ) &
     echo "running $file"
     # export OTEL_SHELL_EXPERIMENTAL_OBSERVE_PIPES=TRUE
-    options='-f -o pipefail $file'
+    options='-f -o pipefail'
     if [ "$SHELL" = bash ]; then
       options="$options -p"
     fi
