62e9af039adf7414eca3f51b4716889485a95442
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-04 14:58:07 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -32,6 +32,8 @@ fi
 env_dir="$(mktemp -d)"
 node download_artifact.js opentelemetry "$env_dir" || true
 if [ -f "$env_dir"/.env ]; then
+  mv "$env_dir"/.env "$env_dir"/.env.encrypted
+  cat "$env_dir"/.env.encrypted | base64 --decode | openssl enc -d -aes-256-cbc -pbkdf2 -pass pass:"$GITHUB_TOKEN" > "$env_dir"/.env
   while read -r line; do
     export "$line"
   done < "$env_dir"/.env
