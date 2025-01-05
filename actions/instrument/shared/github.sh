0ef54b7fec2ccfaf521ab4dbd6f50373c9652dab
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-05 11:06:47 +0100

    Update github.sh

--- actions/instrument/shared/github.sh
+++ actions/instrument/shared/github.sh
@@ -3,12 +3,12 @@
 github() {
   printenv >&2
   url="$GITHUB_API_URL"/"$1"?per_page=100
-  curl --no-progress-meter --fail --retry 16 --retry-all-errors --header "Authorization: Bearer $INPUT__GITHUB_TOKEN" --head "$url" \
+  curl --no-progress-meter --fail --retry 16 --retry-all-errors --header "Authorization: Bearer $INPUT_GITHUB_TOKEN" --head "$url" \
     | grep '^link: ' | cut -d ' '  -f 2- | tr -d ' <>' | tr ',' '\n' \
     | grep 'rel="last"' | cut -d ';' -f1 | cut -d '?' -f 2- | tr '&' '\n' \
     | grep '^page=' | cut -d = -f 2 \
     | xargs seq 1 | while IFS= read -r page; do
-      curl --no-progress-meter --fail --retry 16 --retry-all-errors --header "Authorization: Bearer $INPUT__GITHUB_TOKEN" "$url"\&page="$page"
+      curl --no-progress-meter --fail --retry 16 --retry-all-errors --header "Authorization: Bearer $INPUT_GITHUB_TOKEN" "$url"\&page="$page"
     done
 }
 export -f github
