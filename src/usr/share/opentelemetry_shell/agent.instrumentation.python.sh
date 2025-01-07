ab8b7a8941cf34d796b2a5e9b7369589ab02a968
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-07 11:57:12 +0100

    Update agent.instrumentation.python.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.python.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.python.sh
@@ -1,9 +1,9 @@
 #!/bin/false
 
 _otel_inject_python() {
+  local cmdline="$(_otel_dollar_star "$@")"
+  local cmdline="${cmdline#\\}"
   if \[ "${OTEL_SHELL_CONFIG_INJECT_DEEP:-FALSE}" = TRUE ] && \[ -d "/opt/opentelemetry_shell/venv" ] && _otel_string_starts_with "$(\eval "$1 -V" | \cut -d ' ' -f 2)" "3." && ! _otel_string_ends_with "${2:-}" /pip && ! _otel_string_ends_with "${2:-}" /pip3; then
-    local cmdline="$(_otel_dollar_star "$@")"
-    local cmdline="${cmdline#\\}"
     if _otel_python_is_customize_injectable; then
       local command="$1"; shift
       set -- "$command" /opt/opentelemetry_shell/venv/bin/opentelemetry-instrument "${command#\\}" "$@"
@@ -20,7 +20,7 @@ _otel_inject_python() {
       fi
     fi
   else
-    _otel_call "$@"
+    OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE PYTHONPATH=/opt/opentelemetry_shell/venv/lib/"$(\ls /opt/opentelemetry_shell/venv/lib/)"/site-packages/:"${PYTHONPATH:-}" _otel_call "$@"
   fi
 }
 
