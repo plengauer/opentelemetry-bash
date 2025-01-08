fa899bbc8a162e18a6266582b12bb901eb1cf7d3
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-08 22:24:42 +0100

    Update agent.instrumentation.python.deep.py

--- src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
@@ -34,31 +34,33 @@ except ModuleNotFoundError:
 import functools
 
 def inject_file(file):
-  return '/bin/sh'
+    return '/bin/sh'
 
 def inject_arguments(file, args):
-  try:
-    file = file.decode()
-  except (UnicodeDecodeError, AttributeError):
-    pass
-  if type(args) is tuple:
-      args = list(args)
-  if '/' in file and (not os.path.exists(file) or not os.path.isfile(file) or not os.access(file, os.X_OK)):
-    raise FileNotFoundError(file) # python will just trial and error all possible paths if the 'p' variants of exec are used
-  return [ args[0], '-c', '. otel.sh\n_otel_inject "' + str(file) + '" "$@"', 'python' ] + args[1:]
+    try:
+        file = file.decode()
+    except (UnicodeDecodeError, AttributeError):
+      pass
+    if type(args) is tuple:
+        args = list(args)
+    if '/' in file and (not os.path.exists(file) or not os.path.isfile(file) or not os.access(file, os.X_OK)):
+        raise FileNotFoundError(file) # python will just trial and error all possible paths if the 'p' variants of exec are used
+    return [ args[0], '-c', '. otel.sh\n_otel_inject "' + str(file) + '" "$@"', 'python' ] + args[1:]
 
 def observed_os_execv(original_os_execve, file, args):
-  return original_os_execve(inject_file(file), inject_arguments(file, args), inject_env(None))
+    return original_os_execve(inject_file(file), inject_arguments(file, args), inject_env(None))
 
 def observed_os_execve(original_os_execve, file, args, env):
-  return original_os_execve(inject_file(file), inject_arguments(file, args), inject_env(env))
+    return original_os_execve(inject_file(file), inject_arguments(file, args), inject_env(env))
 
 def observed_subprocess_Popen___init__(original_subprocess_Popen___init__, self, *args, **kwargs):
-  kwargs['env'] = inject_env(kwargs.get('env', None))
-  return original_subprocess_Popen___init__(self, *([ inject_file(args[0]) ] + inject_arguments(args[0], args[1:])), **kwargs);
+    kwargs['env'] = inject_env(kwargs.get('env', None))
+    args = tolist(args)
+    args = ([ inject_file(args[0]) ] + inject_arguments(args[0], args[1:]))
+    return original_subprocess_Popen___init__(self, *args, **kwargs);
 
 def instrument(observed_function, original_function):
-   return functools.partial(observed_function, original_function)
+    return functools.partial(observed_function, original_function)
 
 os.execv = instrument(observed_os_execv, os.execve)
 os.execve = instrument(observed_os_execve, os.execve)
