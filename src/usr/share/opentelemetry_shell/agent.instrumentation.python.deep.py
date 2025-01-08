833c18ba7c775708a19a82e7a55f780bebbc1ff0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-08 11:43:49 +0100

    Update agent.instrumentation.python.deep.py

--- src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
@@ -42,6 +42,8 @@ def inject_arguments(file, args):
     pass
   if type(args) is tuple:
       args = list(args)
+  if '/' in file and (not os.path.exists(file) or not os.path.isfile(file) or not os.access(file, os.X_OK)):
+    raise FileNotFoundError(file) # python will just trial and error all possible paths if the 'p' variants of exec are used
   return [ args[0], '-x', '-c', '. otel.sh\n_otel_inject "' + str(file) + '" "$@"', 'python' ] + args[1:]
 
 def observed_os_execv(original_os_execve, file, args):
