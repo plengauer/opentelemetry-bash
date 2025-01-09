bbd8f297045913ba1ebfbd8721cafe6302eca861
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-09 12:47:01 +0100

    Update agent.instrumentation.python.deep.py

--- src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
@@ -64,6 +64,7 @@ os.execve = instrument(observed_os_execve, os.execve)
 original_subprocess_Popen___init__ = subprocess.Popen.__init__
 def observed_subprocess_Popen___init__(self, *args, **kwargs):
     print('subprocess.Popen(' + str(type(self)) + ', ' + str(type(args)) + ', ' + str(type(kwargs)) + ')', file=sys.stderr)
+    print('subprocess.Popen(' + str(args) + ', ' + str(kwargs) + ')', file=sys.stderr)
     print('subprocess.Popen([' + ','.join(list(args)) + '], ' + str(kwargs) + ')', file=sys.stderr)
     args = list(args)
     # TODO handle shell
