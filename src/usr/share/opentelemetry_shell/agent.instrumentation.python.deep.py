0a63df1fc0f4aaf213a67e183151e07f550a6aaf
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-09 19:38:28 +0100

    Update agent.instrumentation.python.deep.py

--- src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
@@ -46,7 +46,7 @@ def inject_arguments(file, args, is_file=True):
             file = './' + file
         if not os.path.exists(file) or not os.path.isfile(file) or not os.access(file, os.X_OK):
             raise FileNotFoundError(file) # python will just trial and error all possible paths if the 'p' variants of exec are used
-        file = '_otel_inject "' + file + "'"
+        file = "_otel_inject '" + file + "'"
     return [ '-c', '. otel.sh\n' + file + ' "$@"', 'python' ] + args
 
 original_os_execve = os.execve
