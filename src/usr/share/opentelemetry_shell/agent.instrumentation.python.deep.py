0e43b1d81665e73c8779b3df836204b3ca9bac4b
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-12 19:42:32 +0100

    Update agent.instrumentation.python.deep.py

--- src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.python.deep.py
@@ -78,6 +78,8 @@ def observed_subprocess_Popen___init__(self, *args, **kwargs):
     args = list(args)
     if len(args) > 0 and type(args[0]) is list:
         args = args[0]
+    if len(args) > 0 and type(args[0]) is tuple:
+        args = list(args[0])
     print('subprocess.Popen([' + ','.join(args) + '],' + str(kwargs) + ')', file=sys.stderr)
     args = ([ inject_file(kwargs.get('executable', args[0])) ] + inject_arguments(kwargs.get('executable', args[0]), args[1:], not kwargs.get('shell', False)))
     if kwargs.get('executable'):
