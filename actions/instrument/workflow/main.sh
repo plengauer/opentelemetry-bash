91039b560811d163676ab2c5495de14fdb6e987a
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-26 15:20:57 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -7,22 +7,23 @@ OTEL_SHELL_CONFIG_INSTALL_DEEP=FALSE bash -e ../shared/install.sh
 
 export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-"$(echo "$GITHUB_REPOSITORY" | cut -d / -f 2-) CI"}" # TODO where to get this from?
 
+
+workflow_json="$(mktemp)"
+jq < "$GITHUB_EVENT_PATH" > "$workflow_json" .workflow_run
+if [ "$INPUT_WORKFLOW_RUN_ID" != "$(jq < "$workflow_json" .id)" ] || [ "$INPUT_WORKFLOW_RUN_ATTEMPT" != "$(jq < "$workflow_json" .run_attempt)" ]; then gh_workflow_run "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" > "$workflow_json"; fi
+
 # TODO for now debug outout
 export OTEL_TRACES_EXPORTER=console
 export OTEL_SHELL_SDK_OUTPUT_REDIRECT=/dev/stderr
-
 . otelapi.sh
-# TODO configure sdk to not use resource detectors
-# TODO configure api to not record process level resource attributes
+export OTEL_DISABLE_RESOURCE_DETECTION=TRUE # todo re-add resource attributes based on workflow
+_otel_resource_attributes_process() { ; }
 # TODO what to use as span service? could we also get it from a foreign artifact?
 trace_id=123 # TODO get it from a foreign artifact from the workflow
 span_id=123 # TODO get it from a foreign artifact from the workflow
 # TODO configure sdk to use the above trace_id and span_id if available for the first root span 
 otel_init
 
-workflow_json="$(mktemp)"
-jq < "$GITHUB_EVENT_PATH" > "$workflow_json" .workflow_run
-if [ "$INPUT_WORKFLOW_RUN_ID" != "$(jq < "$workflow_json" .id)" ] || [ "$INPUT_WORKFLOW_RUN_ATTEMPT" != "$(jq < "$workflow_json" .run_attempt)" ]; then gh_workflow_run "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" > "$workflow_json"; fi
 workflow_span_handle="$(otel_span_start CONSUMER "$(jq < "$workflow_json" -r .name)")" # TODO fix start time
 otel_span_activate "$workflow_span_handle"
 gh_jobs "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" | jq -r '.jobs[] | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
@@ -43,4 +44,5 @@ done
 otel_span_deactivate "$workflow_span_handle"
 # TODO mark span as failed if conclusion is failure
 otel_span_end "$workflow_span_handle" # TODO fix end time
+
 otel_shutdown
