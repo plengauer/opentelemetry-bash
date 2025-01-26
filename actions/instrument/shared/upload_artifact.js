9f70fe311b5b0af84a3afc54374128b03fc26bd6
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-26 11:04:44 +0100

    Delete actions/instrument/shared/upload_artifact.js

--- actions/instrument/shared/upload_artifact.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const { DefaultArtifactClient } = require('@actions/artifact');
-const artifactName = process.argv[2];
-const fullPath = process.argv[3];
-new DefaultArtifactClient().uploadArtifact(artifactName, [ fullPath ], path.dirname(fullPath));
