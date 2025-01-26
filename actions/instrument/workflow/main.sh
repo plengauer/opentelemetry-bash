0fff719257bd74d2b3205de3f1ef6cd2deacd368
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-26 14:48:31 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -8,8 +8,8 @@ OTEL_SHELL_CONFIG_INSTALL_DEEP=FALSE bash -e ../shared/install.sh
 export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-"$(echo "$GITHUB_REPOSITORY" | cut -d / -f 2-) CI"}" # TODO where to get this from?
 
 # TODO for now debug outout
-# export OTEL_TRACES_EXPORTER=console
-# export OTEL_SHELL_SDK_OUTPUT_REDIRECT=/dev/stderr
+export OTEL_TRACES_EXPORTER=console
+export OTEL_SHELL_SDK_OUTPUT_REDIRECT=/dev/stderr
 
 . otelapi.sh
 # TODO configure sdk to not use resource detectors
@@ -25,14 +25,12 @@ jq < "$GITHUB_EVENT_PATH" > "$workflow_json" .workflow_run
 if [ "$INPUT_WORKFLOW_RUN_ID" != "$(jq < "$workflow_json" .id)" ] || [ "$INPUT_WORKFLOW_RUN_ATTEMPT" != "$(jq < "$workflow_json" .run_attempt)" ]; then gh_workflow_run "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" > "$workflow_json"; fi
 workflow_span_handle="$(otel_span_start CONSUMER "$(jq < "$workflow_json" -r .name)")" # TODO fix start time
 otel_span_activate "$workflow_span_handle"
-gh_jobs "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" | jq -r '.jobs[] | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | tee /dev/stderr | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
-  echo "$job_name" >&2
+gh_jobs "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" | jq -r '.jobs[] | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
   # TODO check if job has been created by checking the artifacts, if so continue
   # TODO of not continue, and check which steps are missing?
   job_span_handle="$(otel_span_start CONSUMER "$job_name)")" # TODO fix start time
   otel_span_activate "$job_span_handle"
   gh_job "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" "$job_id" | jq -r '.steps[] | [.conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r step_conclusion step_started_at step_completed_at step_name; do
-    echo "$step_name" >&2
     step_span_handle="$(otel_span_start INTERNAL "$step_name)")" # TODO fix start time
     # TODO fetch logs?
     if [ "$step_conclusion" = failure ]; then otel_span_error "$job_span_handle"; fi
