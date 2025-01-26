708c139a7cbd2e246b023d78274cbce5dcf1c06c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-26 11:04:38 +0100

    Update github.sh

--- actions/instrument/shared/github.sh
+++ actions/instrument/shared/github.sh
@@ -13,3 +13,41 @@ gh_curl_paginated() {
       | xargs seq 1 || true
   } | while read -r page; do echo "$@"'&page='"$page"; done | xargs parallel gh_curl :::
 }
+
+gh_jobs() {
+  gh_curl_paginated /actions/runs/"$1"/attempt/"$2"/jobs'?page=100'
+}
+
+gh_job() {
+  gh_curl /actions/runs/"$1"/attempt/"$2"/jobs/"$3"
+}
+
+gh_download_artifact() {
+  node -e '
+    const { DefaultArtifactClient } = require("@actions/artifact");
+    const artifactName = process.argv[2];
+    const outputPath = process.argv[3];
+    const client = new DefaultArtifactClient()
+    client.listArtifacts()
+      .then(response => response.artifacts.find(artifact => artifact.name == artifactName)?.id)
+      .then(id => id ? client.downloadArtifact(id, { path: outputPath }) : null);
+  ' "$@"
+}
+
+gh_upload_artifact() {
+  node -e '
+    const path = require('path');
+    const { DefaultArtifactClient } = require('@actions/artifact');
+    const artifactName = process.argv[2];
+    const fullPath = process.argv[3];
+    new DefaultArtifactClient().uploadArtifact(artifactName, [ fullPath ], path.dirname(fullPath));
+  ' "$@"
+}
+
+gh_delete_artifact() {
+  node -e '
+    const { DefaultArtifactClient } = require('@actions/artifact');
+    const artifactName = process.argv[2];
+    new DefaultArtifactClient().deleteArtifact(artifactName);
+  ' "$@"
+}
