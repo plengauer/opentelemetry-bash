265cf36fc20dd708898f7a57b47fbe1d952529b8
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-27 16:08:20 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -26,11 +26,11 @@ otel_init
 
 workflow_span_handle="$(otel_span_start @"$(jq < "$workflow_json" -r .started_at)" CONSUMER "$(jq < "$workflow_json" -r .name)")"
 otel_span_activate "$workflow_span_handle"
-jq < "$jobs_json" -r '. | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | tee /dev/stderr | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
-  if [ -n "${OTEL_ID_GENERATOR_OVERRIDE_TRACEPARENT:-}" ]; then continue; fi
+jq < "$jobs_json" -r '. | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
+  if gh_artifact_download "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" opentelemetry_job_"$job_id" opentelemetry_job_"$job_id"; then continue; fi
   job_span_handle="$(otel_span_start @"$job_started_at" CONSUMER "$job_name")"
   otel_span_activate "$job_span_handle"
-  jq < "$jobs_json" -r '. | select(.id == '"$job_id"') | .steps[] | [.conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | tee /dev/stderr | while read -r step_conclusion step_started_at step_completed_at step_name; do
+  jq < "$jobs_json" -r '. | select(.id == '"$job_id"') | .steps[] | [.conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r step_conclusion step_started_at step_completed_at step_name; do
     step_span_handle="$(otel_span_start @"$step_started_at" INTERNAL "$step_name")"
     # TODO fetch logs?
     if [ "$step_conclusion" = failure ]; then otel_span_error "$job_span_handle"; fi
