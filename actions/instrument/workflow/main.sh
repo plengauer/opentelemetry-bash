7c1e88fdd8fca258cc58eb61ad9855aa8342e9c6
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-01-29 17:33:56 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -15,8 +15,11 @@ if [ "$(jq < "$workflow_json" -r .status)" != completed ]; then exit 1; fi
 jobs_json="$(mktemp)"
 gh_jobs "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" | jq .jobs[] > "$jobs_json"
 
+artifacts_json="$(mktemp)"
+gh_artifacts "$INPUT_WORKFLOW_RUN_ID" | jq -r .artifacts[] > "$artifacts_json"
+
 . otelapi.sh
-export OTEL_DISABLE_RESOURCE_DETECTION=TRUE # todo re-add resource attributes based on workflow
+export OTEL_DISABLE_RESOURCE_DETECTION=TRUE # TODO re-add resource attributes based on workflow
 _otel_resource_attributes_process() {
   :
 }
@@ -27,7 +30,8 @@ otel_init
 
 workflow_span_handle="$(otel_span_start @"$(jq < "$workflow_json" -r .run_started_at)" CONSUMER "$(jq < "$workflow_json" -r .name)")"
 otel_span_activate "$workflow_span_handle"
-jq < "$jobs_json" -r '. | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | grep -v "$(gh_artifacts "$INPUT_WORKFLOW_RUN_ID" | jq -r .artifacts[].name | grep '^opentelemetry_job_' | cut -d _ -f 3)" | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
+jq < "$jobs_json" -r '. | [.id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r job_id job_conclusion job_started_at job_completed_at job_name; do
+  if jq < "$artifacts_json" -r .name | grep -q '^opentelemetry_job_'"$job_id"'$'; then continue; fi
   job_span_handle="$(otel_span_start @"$job_started_at" CONSUMER "$job_name")"
   otel_span_activate "$job_span_handle"
   jq < "$jobs_json" -r '. | select(.id == '"$job_id"') | .steps[] | [.conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r step_conclusion step_started_at step_completed_at step_name; do
