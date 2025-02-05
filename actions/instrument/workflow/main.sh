7d3f6737fafb6ff14ac001ba06e6bb72843a0116
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-05 16:26:40 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -25,6 +25,7 @@ export OTEL_DISABLE_RESOURCE_DETECTION=TRUE # TODO re-add resource attributes ba
 _otel_resource_attributes_process() {
   :
 }
+echo DEBUG DEBUG DEBUG opentelemetry_workflow_run_"$INPUT_WORKFLOW_RUN_ATTEMPT" >&2
 gh_artifact_download "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" opentelemetry_workflow_run_"$INPUT_WORKFLOW_RUN_ATTEMPT" opentelemetry_workflow_run || true
 if [ -r opentelemetry_workflow_run/traceparent ]; then export OTEL_ID_GENERATOR_OVERRIDE_TRACEPARENT="$(cat opentelemetry_workflow_run/traceparent)"; fi
 rm -rf opentelemetry_workflow_run
