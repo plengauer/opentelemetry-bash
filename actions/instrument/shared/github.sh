4a073ee0f632de76d62b1235e3439c4c768e9042
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-06 16:06:52 +0100

    Update github.sh

--- actions/instrument/shared/github.sh
+++ actions/instrument/shared/github.sh
@@ -47,21 +47,7 @@ export -f gh_artifacts
 
 gh_artifact_download() {
   local artifact_filename="$(mktemp)"
-  # --header="Authorization: Bearer $INPUT_GITHUB_TOKEN"
   gh_curl /actions/runs/"$1"/artifacts'?per_page=1&'name="$3" | tee /dev/stderr | jq '.artifacts[0].id' | grep -v '^null$' | xargs -t -r -I '{}' bash -xc 'gh_curl "$@"' bash /actions/artifacts/'{}'/zip --head | tr -d '\r' | grep '^location:' | cut -d ' ' -f 2- | xargs -t -r wget -O "$artifact_filename" && [ -r "$artifact_filename" ] && unzip "$artifact_filename" -d "$4"
-  # node -e '
-  #   const { DefaultArtifactClient } = require("@actions/artifact");
-  #   const client = new DefaultArtifactClient()
-  #   const findBy = {
-  #     token: "'"$INPUT_GITHUB_TOKEN"'",
-  #     repositoryOwner: "'"${GITHUB_REPOSITORY%%/*}"'",
-  #     repositoryName: "'"${GITHUB_REPOSITORY#*/}"'",
-  #     workflowRunId: '"$1"',
-  #   }
-  #   client.listArtifacts({ findBy })
-  #     .then(response => response.artifacts.find(artifact => artifact.name == "'"$3"'")?.id)
-  #     .then(id => id ? client.downloadArtifact(id, { path: "'"$4"'", findBy }) : null);
-  # '
 }
 export -f gh_artifact_download
 
