a320896575a904d7af2f1a2bd389ef4570282907
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-08 13:07:21 +0100

    Fix wget demo

--- demos/context_propagation_http_wget/demo.sh
+++ demos/context_propagation_http_wget/demo.sh
@@ -1,4 +1,4 @@
 otel4netcat_http ncat -l -c 'printf "HTTP/1.1 418 I'\''m a teapot\r\n\r\n"' 12345 & # fake http server
 sleep 5
 . otel.sh
-wget http://127.0.0.1:12345
+wget http://127.0.0.1:12345 || true
