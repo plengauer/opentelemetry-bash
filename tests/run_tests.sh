a404c0b0345d146602e53dd57e9c22f568406285
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-10 18:59:13 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,24 +9,23 @@ if [ "$SHELL" = busybox ]; then
   export SHELL="busybox sh";
 fi
 
-(while sleep 60; do pstree -a -c -p -l; done) &
 for dir in unit sdk auto integration; do
   for file in $(find $dir -iname 'test_*.shell') $(find $dir -iname 'test_*.'"$SHELL"); do
     rm /tmp/opentelemetry_shell_*_instrumentation_cache_*.aliases 2> /dev/null || true
     export OTEL_EXPORT_LOCATION=$(mktemp -u).sdk.out
     export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u -p "$(mktemp -d)").pipe
     export OTEL_TRACES_EXPORTER=console
-    # export OTEL_METRICS_EXPORTER=console
     export OTEL_LOGS_EXPORTER=console
     mkfifo --mode=666 $OTEL_SHELL_SDK_OUTPUT_REDIRECT
-    ( while true; do cat $OTEL_SHELL_SDK_OUTPUT_REDIRECT >> $OTEL_EXPORT_LOCATION; done ) &
+    while true; do cat $OTEL_SHELL_SDK_OUTPUT_REDIRECT >> $OTEL_EXPORT_LOCATION; done &
+    pipe_pid="$!"
     echo "running $file"
-    # export OTEL_SHELL_EXPERIMENTAL_OBSERVE_PIPES=TRUE
     options='-u -f'
     if [ "$SHELL" = bash ]; then
       options="$options -p -o pipefail"
     fi
     timeout $((60 * 60 * 3)) $SHELL $options $file && echo "SUCCEEDED" || (echo "FAILED" && cat $OTEL_EXPORT_LOCATION && exit 1)
+    kill -9 "$pipe_pid"
   done
 done
 echo "ALL TESTS SUCCESSFUL"
