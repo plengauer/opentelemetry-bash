ae00ccd2c545b177f5e2adbe2055c5afe7acb544
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-10 19:11:24 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -12,21 +12,21 @@ fi
 for dir in unit sdk auto integration; do
   for file in $(find $dir -iname 'test_*.shell') $(find $dir -iname 'test_*.'"$SHELL"); do
     rm /tmp/opentelemetry_shell_*_instrumentation_cache_*.aliases 2> /dev/null || true
-    export OTEL_EXPORT_LOCATION=$(mktemp -u).sdk.out
-    export OTEL_SHELL_SDK_OUTPUT_REDIRECT=$(mktemp -u -p "$(mktemp -d)").pipe
+    export OTEL_EXPORT_LOCATION="$(mktemp -u)".sdk.out
+    export OTEL_SHELL_SDK_OUTPUT_REDIRECT="$(mktemp -u -p "$(mktemp -d)")".pipe
     export OTEL_TRACES_EXPORTER=console
     export OTEL_LOGS_EXPORTER=console
-    mkfifo --mode=666 $OTEL_SHELL_SDK_OUTPUT_REDIRECT
-    while true; do cat $OTEL_SHELL_SDK_OUTPUT_REDIRECT >> $OTEL_EXPORT_LOCATION; done &
+    mkfifo --mode=666 "$OTEL_SHELL_SDK_OUTPUT_REDIRECT"
+    while true; do cat "$OTEL_SHELL_SDK_OUTPUT_REDIRECT" >> "$OTEL_EXPORT_LOCATION"; done &
     pipe_pid="$!"
     echo "running $file"
     options='-u -f'
     if [ "$SHELL" = bash ]; then
       options="$options -p -o pipefail"
     fi
-    stdout="$(mktemp)"
-    stderr="$(mktemp)"
-    timeout $((60 * 60 * 3)) $SHELL $options "$file" 1> "$stdout" 1> "$stderr" && echo "$file SUCCEEDED" || (echo "$file FAILED" && echo "stdout:" && cat "$stdout" && echo "stderr:" && cat "$stderr" && echo "otlp:" && cat $OTEL_EXPORT_LOCATION && exit 1)
+    stdout="$(mktemp -u).out"
+    stderr="$(mktemp -u).err"
+    timeout $((60 * 60 * 3)) $SHELL $options "$file" 1> "$stdout" 1> "$stderr" && echo "$file SUCCEEDED" || (echo "$file FAILED" && echo "stdout:" && cat "$stdout" && echo "stderr:" && cat "$stderr" && echo "otlp:" && cat "$OTEL_EXPORT_LOCATION" && exit 1)
     kill -9 "$pipe_pid"
   done
 done
