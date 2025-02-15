ff870c9b129ae7608e035c7e087391450b4e6357
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-15 18:00:33 +0100

    Update decorate_action_run.sh

--- actions/instrument/job/decorate_action_run.sh
+++ actions/instrument/job/decorate_action_run.sh
@@ -7,7 +7,8 @@ otel_init
 span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
 otel_span_attribute_typed $span_handle string github.actions.type=step
 otel_span_attribute_typed $span_handle string github.actions.step.name="${GITHUB_STEP:-$GITHUB_ACTION}"
-otel_span_attribute_typed "$step_span_handle" string github.actions.step.phase=main
+otel_span_attribute_typed $span_handle string github.actions.step.type=shell
+otel_span_attribute_typed $span_handle string github.actions.step.phase=main
 otel_span_activate "$span_handle"
 otel_observe _otel_inject_shell_with_copy "$@"
 exit_code="$?"
