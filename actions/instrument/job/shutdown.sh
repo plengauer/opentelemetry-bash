c6582c37b5197c6d491b74b4680096122ec7c28d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-16 10:36:02 +0100

    Update shutdown.sh

--- actions/instrument/job/shutdown.sh
+++ actions/instrument/job/shutdown.sh
@@ -6,4 +6,4 @@ fi
 root_pid="$STATE_pid"
 kill -USR1 "$root_pid"
 while kill -0 "$root_pid" 2> /dev/null; do sleep 1; done
-cat "$STATE_log_file" >&2
+cat "$STATE_log_file" > "$OTEL_SHELL_SDK_OUTPUT_REDIRECT_DEFERRED"
