db8f28c3bb7cdcc1b9d500c7353b7759941df23f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-16 11:35:43 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -7,13 +7,15 @@ bash -e ../shared/install.sh
 
 echo "$GITHUB_ACTION" > /tmp/opentelemetry_shell_action_name
 
+# setup injections
+## setup injection for shell actions
 new_path_dir="/tmp/otel/bin"
 mkdir -p "$new_path_dir"
 gcc -o "$new_path_dir"/sh forward.c -DEXECUTABLE="$(which sh)" -DARG1="$(pwd)"/decorate_action_run.sh -DARG2="$(which sh)"
 gcc -o "$new_path_dir"/dash forward.c -DEXECUTABLE="$(which dash)" -DARG1="$(pwd)"/decorate_action_run.sh -DARG2="$(which dash)"
 gcc -o "$new_path_dir"/bash forward.c -DEXECUTABLE="$(which bash)" -DARG1="$(pwd)"/decorate_action_run.sh -DARG2="$(which bash)"
 echo "$new_path_dir" >> "$GITHUB_PATH"
-
+## setup injections into node actions
 for node_path in /home/runner/runners/*/externals/node*/bin/node; do
   dir_path_new="$(echo "$node_path" | rev | cut -d / -f 2- | rev).original"
   mkdir "$dir_path_new"
@@ -21,12 +23,13 @@ for node_path in /home/runner/runners/*/externals/node*/bin/node; do
   mv "$node_path" "$node_path_new"
   gcc -o "$node_path" forward.c -DEXECUTABLE=/bin/bash -DARG1="$(pwd)"/decorate_action_node.sh -DARG2="$node_path_new"
 done
-
-# cant use the same path trick as for the shells, because path is resolved at the very start, so paths must not change
+## setup injections into docker actions
+### cant use the same path trick as for the shells, because path is resolved at the very start, so paths must not change
 docker_path="$(which docker)"
 sudo mv "$docker_path" "$(pwd)"
 sudo gcc -o "$docker_path" forward.c -DEXECUTABLE=/bin/bash -DARG1="$(pwd)"/decorate_action_docker.sh -DARG2="$(pwd)"/docker
 
+# guess job id for proper linking
 OTEL_SHELL_GITHUB_JOB="$GITHUB_JOB"
 job_arguments="$(printf '%s' "$INPUT___JOB_MATRIX" | jq -r '. | [.. | scalars] | @tsv' | sed 's/\t/, /g')"
 if [ -n "$job_arguments" ]; then OTEL_SHELL_GITHUB_JOB="$OTEL_SHELL_GITHUB_JOB ($job_arguments)"; fi
@@ -35,49 +38,77 @@ GITHUB_JOB_ID="$(gh_jobs "$GITHUB_RUN_ID" "$GITHUB_RUN_ATTEMPT" | jq --unbuffere
 if [ "$(printf '%s' "$GITHUB_JOB_ID" | wc -l)" -le 1 ]; then export GITHUB_JOB_ID; fi
 echo "Guessing GitHub job id to be $GITHUB_JOB_ID" >&2
 
-if type docker && [ "${OTEL_LOGS_EXPORTER:-otlp}" = otlp ] && [ "${OTEL_METRICS_EXPORTER:-otlp}" = otlp ] && [ "${OTEL_TRACES_EXPORTER:-otlp}" = otlp ] && ([ "$INPUT_COLLECTOR" = yes ] || ([ "$INPUT_COLLECTOR" = auto ] && ([ -n "${OTEL_EXPORTER_OTLP_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_LOGS_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_METRICS_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_TRACES_HEADERS:-}" ]))); then
+# configure collector if required
+if [ "$INPUT_COLLECTOR" = yes ] || ([ "$INPUT_COLLECTOR" = auto ] && ([ -n "${OTEL_EXPORTER_OTLP_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_LOGS_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_METRICS_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_TRACES_HEADERS:-}" ])); then
+  if ! type docker; then echo "::error ::Cannot use collector because docker is unavailable." && false; fi
+  section_exporter_logs="$(mktemp)"; section_exporter_metrics="$(mktemp)"; section_exporter_traces="$(mktemp)"
+  section_pipeline_logs="$(mktemp)"; section_pipeline_metrics="$(mktemp)"; section_pipeline_traces="$(mktemp)"
+  if [ "${OTEL_LOGS_EXPORTER:-otlp}" = otlp ]; then
+    if [ "${OTEL_EXPORTER_OTLP_PROTOCOL:-http/protobuf}" ]; then local collector_exporter=otlphttp; else local collector_exporter=otlp; fi
+    cat > "$section_exporter_logs" <<EOF
+  $collector_exporter/logs:
+    endpoint: "${OTEL_EXPORTER_OTLP_LOGS_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}"
+    headers:
+$(echo "$OTEL_EXPORTER_OTLP_HEADERS","$OTEL_EXPORTER_OTLP_LOGS_HEADERS" | tr ',' '\n' | grep -v '^$' | sed 's/=/: /g' | sed 's/^/      /g')
+EOF
+    cat > "$section_pipeline_logs" <<EOF
+    logs:
+      receivers: [otlp]
+      exporters: [$collector_exporter/logs]
+      processors: [batch]
+EOF
+    unset OTEL_EXPORTER_OTLP_LOGS_HEADERS
+  fi
+  if [ "${OTEL_METRICS_EXPORTER:-otlp}" = otlp ]; then
+    if [ "${OTEL_EXPORTER_OTLP_PROTOCOL:-http/protobuf}" ]; then local collector_exporter=otlphttp; else local collector_exporter=otlp; fi
+    cat > "$section_exporter_metrics" <<EOF
+  $collector_exporter/metrics:
+    endpoint: "${OTEL_EXPORTER_OTLP_METRICS_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}"
+    headers:
+$(echo "$OTEL_EXPORTER_OTLP_HEADERS","$OTEL_EXPORTER_OTLP_METRICS_HEADERS" | tr ',' '\n' | grep -v '^$' | sed 's/=/: /g' | sed 's/^/      /g')
+EOF
+    cat > "$section_pipeline_metrics" <<EOF
+    logs:
+      receivers: [otlp]
+      exporters: [$collector_exporter/metrics]
+      processors: [batch]
+EOF
+    unset OTEL_EXPORTER_OTLP_METRICS_HEADERS
+  fi
+  if [ "${OTEL_TRACES_EXPORTER:-otlp}" = otlp ]; then
+    if [ "${OTEL_EXPORTER_OTLP_PROTOCOL:-http/protobuf}" ]; then local collector_exporter=otlphttp; else local collector_exporter=otlp; fi
+    cat > "$section_exporter_traces" <<EOF
+  $collector_exporter/logs:
+    endpoint: "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}"
+    headers:
+$(echo "$OTEL_EXPORTER_OTLP_HEADERS","$OTEL_EXPORTER_OTLP_TRACES_HEADERS" | tr ',' '\n' | grep -v '^$' | sed 's/=/: /g' | sed 's/^/      /g')
+EOF
+    cat > "$section_pipeline_traces" <<EOF
+    logs:
+      receivers: [otlp]
+      exporters: [$collector_exporter/logs]
+      processors: [batch]
+EOF
+    unset OTEL_EXPORTER_OTLP_TRACES_HEADERS
+  fi
+  unset OTEL_EXPORTER_OTLP_HEADERS
   cat > collector.yaml <<EOF
 receivers:
   otlp:
     protocols:
       http:
         endpoint: localhost:4318
-
 exporters:
-  ${OTEL_LOGS_EXPORTER:-otlphttp}/logs:
-    endpoint: "${OTEL_EXPORTER_OTLP_LOGS_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}"
-    headers:
-      $(echo "$OTEL_EXPORTER_OTLP_HEADERS","$OTEL_EXPORTER_OTLP_LOGS_HEADERS" | tr ',' '\n' | grep -v '^$' | sed 's/=/: /g')
-  
-  ${OTEL_METRICS_EXPORTER:-otlphttp}/metrics:
-    endpoint: "${OTEL_EXPORTER_OTLP_METRICS_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}"
-    headers:
-      $(echo "$OTEL_EXPORTER_OTLP_HEADERS","$OTEL_EXPORTER_OTLP_METRICS_HEADERS" | tr ',' '\n' | grep -v '^$' | sed 's/=/: /g')
-  
-  ${OTEL_TRACES_EXPORTER:-otlphttp}/traces:
-    endpoint: "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}"
-    headers:
-      $(echo "$OTEL_EXPORTER_OTLP_HEADERS","$OTEL_EXPORTER_OTLP_TRACES_HEADERS" | tr ',' '\n' | grep -v '^$' | sed 's/=/: /g')
-
+$(cat $section_exporter_logs)
+$(cat $section_exporter_metrics)
+$(cat $section_exporter_traces)
 processors:
   batch:
-
 service:
   pipelines:
-    logs:
-      receivers: [otlp]
-      exporters: [${OTEL_LOGS_EXPORTER:-otlphttp}/logs]
-      processors: [batch]
-
-    metrics:
-      receivers: [otlp]
-      exporters: [${OTEL_METRICS_EXPORTER:-otlphttp}/metrics]
-      processors: [batch]
-
-    traces:
-      receivers: [otlp]
-      exporters: [${OTEL_TRACES_EXPORTER:-otlphttp}/traces]
-      processors: [batch]
+$(cat $section_pipeline_logs)
+$(cat $section_pipeline_metrics)
+$(cat $section_pipeline_traces)
 EOF
   export OTEL_TRACES_EXPORTER=otlp
   export OTEL_EXPORTER_OTLP_ENDPOINT=https://localhost:4318
@@ -86,8 +117,12 @@ EOF
   unset OTEL_LOGS_EXPORTER OTEL_METRICS_EXPORTER OTEL_TRACES_EXPORTER
   export OTEL_SHELL_COLLECTOR_IMAGE="$(cat Dockerfile | grep '^FROM ' | cut -d ' ' -f 2-)"
   sudo docker pull "$collector_image" &
+#  if [ -n "$INPUT_DEBUG" ]; then
+    cat collector.yaml
+#  fi
 fi
 
+# resolve parent (does not exist yet - see workflow action) and make sure all jobs are of the same trace and have the same deferred parent 
 opentelemetry_root_dir="$(mktemp -d)"
 while ! gh_artifact_download "$GITHUB_RUN_ID" "$GITHUB_RUN_ATTEMPT" opentelemetry_workflow_run_"$GITHUB_RUN_ATTEMPT" "$opentelemetry_root_dir" || ! [ -r "$opentelemetry_root_dir"/traceparent ]; do
   . otelapi.sh
@@ -102,6 +137,7 @@ rm -rf "$opentelemetry_root_dir"
 
 export OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-"$(echo "$GITHUB_REPOSITORY" | cut -d / -f 2-) CI"}"
 
+# end job span
 root4job_end() {
   if [ -f /tmp/opentelemetry_shell.github.error ]; then
     otel_span_attribute_typed "$span_handle" string github.actions.job.conclusion=failure
@@ -116,6 +152,7 @@ root4job_end() {
 }
 export -f root4job_end
 
+# start job span
 root4job() {
   [ -z "${OTEL_SHELL_COLLECTOR_CONTAINER:-}" ] || export OTEL_SHELL_COLLECTOR_CONTAINER="$(sudo docker start --restart unless-stopped --network=host --mount type=bind,source="$(pwd)"/collector.yaml,target=/etc/otelcol/config.yaml "$OTEL_SHELL_COLLECTOR_IMAGE")"
   rm /tmp/opentelemetry_shell.github.error 2> /dev/null
@@ -148,6 +185,7 @@ root4job() {
 }
 export -f root4job
 
+# redirect output and create job span
 tmp_dir="$(mktemp -d)"
 chmod 777 "$tmp_dir"
 export otel_shell_sdk_output_redirect="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}" >> "$GITHUB_STATE"
@@ -160,14 +198,17 @@ traceparent_file="$(mktemp -u)"
 nohup bash -c 'root4job "$@"' bash "$traceparent_file" &> /dev/null &
 echo "pid=$!" >> "$GITHUB_STATE"
 
+# wait for traceparent to be available
 while ! [ -f "$traceparent_file" ]; do sleep 1; done
 export TRACEPARENT="$(cat "$traceparent_file")"
 rm "$traceparent_file"
 
+# some default configurations
 export OTEL_SHELL_CONFIG_MUTE_BUILTINS="${OTEL_SHELL_CONFIG_MUTE_BUILTINS:-TRUE}"
 export OTEL_SHELL_CONFIG_INJECT_DEEP="${OTEL_SHELL_CONFIG_INJECT_DEEP:-TRUE}"
 export OTEL_SHELL_CONFIG_OBSERVE_SUBPROCESSES="${OTEL_SHELL_CONFIG_OBSERVE_SUBPROCESSES:-TRUE}"
 export OTEL_SHELL_CONFIG_OBSERVE_SIGNALS="${OTEL_SHELL_CONFIG_OBSERVE_SIGNALS:-TRUE}"
 export OTEL_SHELL_CONFIG_OBSERVE_PIPES="${OTEL_SHELL_CONFIG_OBSERVE_PIPES:-TRUE}"
 
+# propagate all config to all other actions
 printenv | grep -E '^OTEL_|^TRACEPARENT=|^TRACESTATE=' >> "$GITHUB_ENV"
