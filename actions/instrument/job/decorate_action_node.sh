863dae02fe0797b55edc8f2eab0f9d4fbbbbbb34
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-16 14:52:37 +0100

    Update decorate_action_node.sh

--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -8,6 +8,8 @@ otel_init
 span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
 otel_span_attribute_typed $span_handle string github.actions.type=step
 otel_span_attribute_typed $span_handle string github.actions.step.name="${GITHUB_STEP:-$GITHUB_ACTION}"
+printenv | grep '^INPUT_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.input."$key"="$(eval 'printf '\''%s'\'' $'"$key")"; done
+printenv | grep '^STATE_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.state."$key"="$(eval 'printf '\''%s'\'' $'"$key")"; done
 if [ -n "${GITHUB_ACTION_PATH:-}" ] && [ -d "$GITHUB_ACTION_PATH" ]; then
   otel_span_attribute_typed $span_handle string github.actions.action.type=composite/javascript
 else
