73965bc27e20d629430b76443b654aac1d2aab33
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-19 09:27:01 +0100

    Update decorate_action_docker.sh

--- actions/instrument/job/decorate_action_docker.sh
+++ actions/instrument/job/decorate_action_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$(cat /tmp/opentelemetry_shell_action_name 2> /dev/null)" = "$GITHUB_ACTION" ]; then exec "$@"; fi
 _OTEL_GITHUB_STEP_AGENT_INSTRUMENTATION_FILE=/usr/share/opentelemetry_shell/agent.instrumentation.docker.sh
+_OTEL_GITHUB_STEP_AGENT_INJECTION_FUNCTION=_otel_inject_docker
 _OTEL_GITHUB_STEP_ACTION_TYPE=docker
 case "$2" in
   build) _OTEL_GITHUB_STEP_ACTION_PHASE=pre;;
