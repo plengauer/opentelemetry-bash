f1a90241963645973e423ca379384cab8fe26342
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-19 09:14:52 +0100

    Update decorate_action.sh

--- actions/instrument/job/decorate_action.sh
+++ actions/instrument/job/decorate_action.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 # need to use bash over sh, because it propagates invalid env vars correctly (env vars with dashes in keys)
 if [ -z "$GITHUB_RUN_ID" ] || [ "$(cat /proc/$PPID/cmdline | tr '\000-\037' ' ' | cut -d ' ' -f 1 | rev | cut -d / -f 1 | rev)" != "Runner.Worker" ]; then exec "$@"; fi
-# TODO override some functions to not create some resource attributes
 . otelapi.sh
+_otel_resource_attributes_process() {
+  _otel_resource_attribute string github.repository.id="$GITHUB_REPOSITORY_ID"
+  _otel_resource_attribute string github.repository.name="$GITHUB_REPOSITORY"
+  _otel_resource_attribute string github.repository.owner.id="$GITHUB_REPOSITORY_OWNER_ID"
+  _otel_resource_attribute string github.repository.owner.name="$GITHUB_REPOSITORY_OWNER"
+  _otel_resource_attribute string github.actions.workflow.name="$GITHUB_WORKFLOW"
+  _otel_resource_attribute string github.actions.workflow.ref="$GITHUB_WORKFLOW_REF"
+  _otel_resource_attribute string github.actions.workflow.sha="$GITHUB_WORKFLOW_SHA"
+}
 eval "$(cat "$_OTEL_GITHUB_STEP_AGENT_INSTRUMENTATION_FILE" | grep -v '_otel_alias_prepend ')"
 otel_init
 span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
@@ -16,7 +24,6 @@ otel_span_attribute_typed $span_handle string github.actions.action.name="$GITHU
 otel_span_attribute_typed $span_handle string github.actions.action.ref="$GITHUB_ACTION_REF"
 [ -z "${_OTEL_GITHUB_STEP_ACTION_PHASE:-}" ] || otel_span_attribute_typed $span_handle string github.actions.action.phase="$_OTEL_GITHUB_STEP_ACTION_PHASE"
 otel_span_activate "$span_handle"
-# TODO override commandline to hide injection
 otel_observe _otel_inject_node "$@"
 exit_code="$?"
 if [ "$exit_code" != 0 ]; then
