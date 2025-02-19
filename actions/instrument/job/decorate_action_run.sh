bca929d3bf084c0d6f52e2a25324c08424730294
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-19 08:24:51 +0100

    Update decorate_action_run.sh

--- actions/instrument/job/decorate_action_run.sh
+++ actions/instrument/job/decorate_action_run.sh
@@ -3,4 +3,4 @@ if [ "$(cat /tmp/opentelemetry_shell_action_name 2> /dev/null)" = "$GITHUB_ACTIO
 _OTEL_GITHUB_STEP_AGENT_INSTRUMENTATION_FILE=/usr/share/opentelemetry_shell/agent.instrumentation.shells.sh
 _OTEL_GITHUB_STEP_ACTION_TYPE=shell
 _OTEL_GITHUB_STEP_ACTION_PHASE=main
-. decorate_action.sh # TODO where to take path
+. "${0%/*}"/decorate_action.sh
