8c20b968b9e01f3da0b266de58f682ff375f22ac
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-19 12:04:16 +0100

    Extract common logic for decorating GitHub actions

---
+++ actions/instrument/job/decorate_action.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+if [ -z "$GITHUB_RUN_ID" ] || [ "$(cat /proc/$PPID/cmdline | tr '\000-\037' ' ' | cut -d ' ' -f 1 | rev | cut -d / -f 1 | rev)" != "Runner.Worker" ]; then exec "$@"; fi
+. otelapi.sh
+_otel_resource_attributes_process() {
+  _otel_resource_attribute string github.repository.id="$GITHUB_REPOSITORY_ID"
+  _otel_resource_attribute string github.repository.name="$GITHUB_REPOSITORY"
+  _otel_resource_attribute string github.repository.owner.id="$GITHUB_REPOSITORY_OWNER_ID"
+  _otel_resource_attribute string github.repository.owner.name="$GITHUB_REPOSITORY_OWNER"
+  _otel_resource_attribute string github.actions.workflow.name="$GITHUB_WORKFLOW"
+  _otel_resource_attribute string github.actions.workflow.ref="$GITHUB_WORKFLOW_REF"
+  _otel_resource_attribute string github.actions.workflow.sha="$GITHUB_WORKFLOW_SHA"
+}
+eval "$(cat "$_OTEL_GITHUB_STEP_AGENT_INSTRUMENTATION_FILE" | grep -v '_otel_alias_prepend ')"
+otel_init
+span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
+otel_span_attribute_typed $span_handle string github.actions.type=step
+otel_span_attribute_typed $span_handle string github.actions.step.name="${GITHUB_STEP:-$GITHUB_ACTION}"
+printenv | grep '^INPUT_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.input."${key#INPUT_}"="$(eval 'printf '\''%s'\'' $'"$key")"; done
+printenv | grep '^STATE_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.state."${key#STATE_}"="$(eval 'printf '\''%s'\'' $'"$key")"; done
+[ -z "${GITHUB_ACTION_PATH:-}" ] || ! [ -d "$GITHUB_ACTION_PATH" ] || _OTEL_GITHUB_STEP_ACTION_TYPE=composite/"$_OTEL_GITHUB_STEP_ACTION_TYPE"
+otel_span_attribute_typed $span_handle string github.actions.action.type="$_OTEL_GITHUB_STEP_ACTION_TYPE"
+otel_span_attribute_typed $span_handle string github.actions.action.name="$GITHUB_ACTION_REPOSITORY"
+otel_span_attribute_typed $span_handle string github.actions.action.ref="$GITHUB_ACTION_REF"
+[ -z "${_OTEL_GITHUB_STEP_ACTION_PHASE:-}" ] || otel_span_attribute_typed $span_handle string github.actions.action.phase="$_OTEL_GITHUB_STEP_ACTION_PHASE"
+otel_span_activate "$span_handle"
+otel_observe "$_OTEL_GITHUB_STEP_AGENT_INJECTION_FUNCTION" "$@"
+exit_code="$?"
+if [ "$exit_code" != 0 ]; then
+  otel_span_attribute_typed $span_handle string github.actions.step.conclusion=failure
+  otel_span_error "$span_handle"
+  touch /tmp/opentelemetry_shell.github.error
+else
+  otel_span_attribute_typed $span_handle string github.actions.step.conclusion=success
+fi
+otel_span_deactivate "$span_handle"
+otel_span_end "$span_handle"
+otel_shutdown
+exit "$exit_code"
--- actions/instrument/job/decorate_action_node.sh
+++ actions/instrument/job/decorate_action_node.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
-# need to use bash over sh, because it propagates invalid env vars correctly (env vars with dashes in keys)
-if [ "$(cat /tmp/opentelemetry_shell_action_name 2> /dev/null)" = "$GITHUB_ACTION" ] || [ -z "$GITHUB_RUN_ID" ] || [ "$(cat /proc/$PPID/cmdline | tr '\000-\037' ' ' | cut -d ' ' -f 1 | rev | cut -d / -f 1 | rev)" != "Runner.Worker" ]; then exec "$@"; fi
-# export GITHUB_STEP="$(curl --no-progress-meter --fail --retry 12 --retry-all-errors "$GITHUB_API_URL"/repos/"$GITHUB_REPOSITORY"/actions/runs/"$GITHUB_RUN_ID"/jobs 2> /dev/null | jq -r ".jobs[] | select(.name == \"$GITHUB_JOB\") | select(.run_attempt == $GITHUB_RUN_ATTEMPT) | .steps[] | select(.status == \"in_progress\") | .name")"
-. otelapi.sh
-eval "$(cat /usr/share/opentelemetry_shell/agent.instrumentation.node.sh | grep -v '_otel_alias_prepend ')"
-otel_init
-span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
-otel_span_attribute_typed $span_handle string github.actions.type=step
-otel_span_attribute_typed $span_handle string github.actions.step.name="${GITHUB_STEP:-$GITHUB_ACTION}"
-printenv | grep '^INPUT_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.input."${key#INPUT_}"="$(eval 'printf '\''%s'\'' $'"$key")"; done
-printenv | grep '^STATE_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.state."${key#STATE_}"="$(eval 'printf '\''%s'\'' $'"$key")"; done
-if [ -n "${GITHUB_ACTION_PATH:-}" ] && [ -d "$GITHUB_ACTION_PATH" ]; then
-  otel_span_attribute_typed $span_handle string github.actions.action.type=composite/javascript
-else
-  otel_span_attribute_typed $span_handle string github.actions.action.type=javascript
-fi
-otel_span_attribute_typed $span_handle string github.actions.action.name="$GITHUB_ACTION_REPOSITORY"
-otel_span_attribute_typed $span_handle string github.actions.action.ref="$GITHUB_ACTION_REF"
-# otel_span_attribute_typed $span_handle string github.actions.action.phase=main # TODO
-otel_span_activate "$span_handle"
-otel_observe _otel_inject_node "$@"
-exit_code="$?"
-if [ "$exit_code" != 0 ]; then
-  otel_span_attribute_typed $span_handle string github.actions.step.conclusion=failure
-  otel_span_error "$span_handle"; touch /tmp/opentelemetry_shell.github.error
-else
-  otel_span_attribute_typed $span_handle string github.actions.step.conclusion=success
-fi
-otel_span_deactivate "$span_handle"
-otel_span_end "$span_handle"
-otel_shutdown
-exit "$exit_code"
+if [ "$(cat /tmp/opentelemetry_shell_action_name 2> /dev/null)" = "$GITHUB_ACTION" ]; then exec "$@"; fi
+_OTEL_GITHUB_STEP_AGENT_INSTRUMENTATION_FILE=/usr/share/opentelemetry_shell/agent.instrumentation.node.sh
+_OTEL_GITHUB_STEP_AGENT_INJECTION_FUNCTION=_otel_inject_node
+_OTEL_GITHUB_STEP_ACTION_TYPE=javascript
+# _OTEL_GITHUB_STEP_ACTION_PHASE=main # TODO
+. "${0%/*}"/decorate_action.sh
