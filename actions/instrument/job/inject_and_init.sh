7fc807bc56a1563d03ec459bac6ef8501394fdae
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-22 00:12:00 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -165,8 +165,8 @@ echo "Guessing GitHub job id to be $GITHUB_JOB_ID" >&2
 
 # observe
 observe_rate_limit() {
-  used_gauge_handle="$(otel_counter_create observable gauge github.api.rate_limit.used 1 "The amount of rate limited requests used")"
-  remaining_gauge_handle="$(otel_counter_create observable gauge github.api.rate_limit.remaining 1 "The amount of rate limited requests remaining")"
+  used_gauge_handle="$(otel_counter_create standard gauge github.api.rate_limit.used 1 "The amount of rate limited requests used")"
+  remaining_gauge_handle="$(otel_counter_create standard gauge github.api.rate_limit.remaining 1 "The amount of rate limited requests remaining")"
   while true; do gh_rate_limit; sleep 5; done | jq --unbuffered -r '.resources | to_entries[] | [.key, .value.used, .value.remaining] | @tsv' | sed 's/\t/ /g' | while read -r resource used remaining; do
     observation_handle="$(otel_observation_create "$used")"
     otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
@@ -196,19 +196,7 @@ root4job() {
   traceparent_file="$1"
   . otelapi.sh
   otel_init
-
-  used_gauge_handle="$(otel_counter_create observable gauge github.api.rate_limit.used 1 "The amount of rate limited requests used")"
-  remaining_gauge_handle="$(otel_counter_create observable gauge github.api.rate_limit.remaining 1 "The amount of rate limited requests remaining")"
-  gh_rate_limit | jq --unbuffered -r '.resources | to_entries[] | [.key, .value.used, .value.remaining] | @tsv' | sed 's/\t/ /g' | while read -r resource used remaining; do
-    observation_handle="$(otel_observation_create "$used")"
-    otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
-    otel_counter_observe "$used_gauge_handle" "$observation_handle"
-    observation_handle="$(otel_observation_create "$remaining")"
-    otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
-    otel_counter_observe "$used_gauge_handle" "$observation_handle"
-  done
-
-  # observe_rate_limit &
+  observe_rate_limit &
   observe_rate_limit_pid="$!"
   span_handle="$(otel_span_start CONSUMER "${OTEL_SHELL_GITHUB_JOB:-$GITHUB_JOB}")"
   otel_span_attribute_typed $span_handle string github.actions.type=job
