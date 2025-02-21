b91d88f99ee9739af49895195a950beb3d7f23b8
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-21 10:50:44 +0100

    Update config_validation.sh

--- actions/instrument/shared/config_validation.sh
+++ actions/instrument/shared/config_validation.sh
@@ -1,24 +1,33 @@
 #/bin/false
-if [ "${OTEL_LOGS_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_LOGS_EXPORTER:-otlp}" != console ]; then
+if [ "${OTEL_LOGS_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_LOGS_EXPORTER:-otlp}" != console ] && [ "${OTEL_LOGS_EXPORTER:-otlp}" != none ]; then
   echo "::error ::OpenTelemetry for GitHub actions only supports otlp exporters ($OTEL_LOGS_EXPORTER). For other exporters, pipe the data through a collector outside of GitHub to translate the data to a different protocol." && false
 fi
-if [ "${OTEL_METRICS_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_METRICS_EXPORTER:-otlp}" != console ]; then
+if [ "${OTEL_METRICS_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_METRICS_EXPORTER:-otlp}" != console ] && [ "${OTEL_METRICS_EXPORTER:-otlp}" != none ]; then
   echo "::error ::OpenTelemetry for GitHub actions only supports otlp exporters ($OTEL_METRICS_EXPORTER). For other exporters, pipe the data through a collector outside of GitHub to translate the data to a different protocol." && false
 fi
-if [ "${OTEL_TRACES_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_TRACES_EXPORTER:-otlp}" != console ]; then
+if [ "${OTEL_TRACES_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_TRACES_EXPORTER:-otlp}" != console ] && [ "${OTEL_TRACES_EXPORTER:-otlp}" != none ]; then
   echo "::error ::OpenTelemetry for GitHub actions only supports otlp exporters ($OTEL_TRACES_EXPORTER). For other exporters, pipe the data through a collector outside of GitHub to translate the data to a different protocol." && false
 fi
-if [ -z "${OTEL_EXPORTER_OTLP_LOGS_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}" ] && [ -z "${OTEL_LOGS_EXPORTER:-}" ]; then
+if [ "${OTEL_LOGS_EXPORTER:-otlp}" = otlp ] && [ -z "${OTEL_EXPORTER_OTLP_LOGS_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ]; then
+  echo "::error ::OpenTelemetry for GitHub actions is configured to export logs, but no endpoint is configured." && false
+fi
+if [ "${OTEL_METRICS_EXPORTER:-otlp}" = otlp ] && [ -z "${OTEL_EXPORTER_OTLP_METRICS_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ]; then
+  echo "::error ::OpenTelemetry for GitHub actions is configured to export metrics, but no endpoint is configured." && false
+fi
+if [ "${OTEL_TRACES_EXPORTER:-otlp}" = otlp ] && [ -z "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ]; then
+  echo "::error ::OpenTelemetry for GitHub actions is configured to export traces, but no endpoint is configured." && false
+fi
+if [ -z "${OTEL_EXPORTER_OTLP_LOGS_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ] && [ -z "${OTEL_LOGS_EXPORTER:-}" ]; then
   export OTEL_LOGS_EXPORTER=console
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}"
   echo "::notice ::OpenTelemetry for GitHub actions has no logs export configured. Consult the documentation for instructions."
 fi
-if [ -z "${OTEL_EXPORTER_OTLP_METRICS_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}" ] && [ -z "${OTEL_METRICS_EXPORTER:-}" ]; then
+if [ -z "${OTEL_EXPORTER_OTLP_METRICS_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ] && [ -z "${OTEL_METRICS_EXPORTER:-}" ]; then
   export OTEL_METRICS_EXPORTER=console
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}"
   echo "::notice ::OpenTelemetry for GitHub actions has no metrics export configured. Consult the documentation for instructions."
 fi
-if [ -z "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-$OTEL_EXPORTER_OTLP_ENDPOINT}" ] && [ -z "${OTEL_TRACES_EXPORTER:-}" ]; then
+if [ -z "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ] && [ -z "${OTEL_TRACES_EXPORTER:-}" ]; then
   export OTEL_TRACES_EXPORTER=console
   export OTEL_SHELL_SDK_OUTPUT_REDIRECT="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}"
   echo "::notice ::OpenTelemetry for GitHub actions has no traces export configured. Consult the documentation for instructions."
