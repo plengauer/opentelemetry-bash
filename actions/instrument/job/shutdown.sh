13e143f6f3ec89f097f4e9dcac69bd8f6cd893c3
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-23 00:18:42 +0100

    Update shutdown.sh

--- actions/instrument/job/shutdown.sh
+++ actions/instrument/job/shutdown.sh
@@ -7,4 +7,3 @@ root_pid="$STATE_pid"
 kill -USR1 "$root_pid"
 while kill -0 "$root_pid" 2> /dev/null; do sleep 1; done
 cat < "$STATE_log_file" > "$STATE_otel_shell_sdk_output_redirect"
-cat /tmp/debug.log
