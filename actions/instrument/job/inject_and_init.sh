806420a6a202aa101ba0adf5c336cec8224f475c
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-22 22:35:53 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -167,20 +167,23 @@ echo "Guessing GitHub job id to be $GITHUB_JOB_ID" >&2
 observe_rate_limit() {
   used_gauge_handle="$(otel_counter_create standard gauge github.api.rate_limit.used 1 "The amount of rate limited requests used")"
   remaining_gauge_handle="$(otel_counter_create standard gauge github.api.rate_limit.remaining 1 "The amount of rate limited requests remaining")"
-  while true; do gh_rate_limit; sleep 5; done | jq --unbuffered -r '.resources | to_entries[] | [.key, .value.used, .value.remaining] | @tsv' | sed 's/\t/ /g' | while read -r resource used remaining; do
-    observation_handle="$(otel_observation_create "$used")"
-    otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
-    otel_counter_observe "$used_gauge_handle" "$observation_handle"
-    observation_handle="$(otel_observation_create "$remaining")"
-    otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
-    otel_counter_observe "$used_gauge_handle" "$observation_handle"
+  while true; do
+    gh_rate_limit | jq --unbuffered -r '.resources | to_entries[] | [.key, .value.used, .value.remaining] | @tsv' | sed 's/\t/ /g' | while read -r resource used remaining; do
+      observation_handle="$(otel_observation_create "$used")"
+      otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
+      otel_counter_observe "$used_gauge_handle" "$observation_handle"
+      observation_handle="$(otel_observation_create "$remaining")"
+      otel_observation_attribute_typed "$observation_handle" string github.api.resource="$resource"
+      otel_counter_observe "$used_gauge_handle" "$observation_handle"
+    done
+    sleep 5
   done
 }
 export -f observe_rate_limit
-. otelapi.sh
-otel_init
-set -x
-observe_rate_limit
+#. otelapi.sh
+#otel_init
+#set -x
+#observe_rate_limit
 root4job_end() {
   if [ -f /tmp/opentelemetry_shell.github.error ]; then
     otel_span_attribute_typed "$span_handle" string github.actions.job.conclusion=failure
