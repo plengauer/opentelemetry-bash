0e44d67a5c68d96df9886ce60b870cd8c302f12f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-22 19:25:54 +0100

    Update decorate_action.sh

--- actions/instrument/job/decorate_action.sh
+++ actions/instrument/job/decorate_action.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ -z "$GITHUB_RUN_ID" ] || [ "$(cat /proc/$PPID/cmdline | tr '\000-\037' ' ' | cut -d ' ' -f 1 | rev | cut -d / -f 1 | rev)" != "Runner.Worker" ]; then exec "$@"; fi
-variable_name_to_attribute_key() {
+variable_name_2_attribute_key() {
   printf '%s' "$1" | tr '[:upper:]' '[:lower:]'
 }
 variable_2_attribute_value() {
@@ -18,8 +18,8 @@ otel_init
 span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
 otel_span_attribute_typed $span_handle string github.actions.type=step
 otel_span_attribute_typed $span_handle string github.actions.step.name="${GITHUB_STEP:-$GITHUB_ACTION}"
-printenv | grep '^INPUT_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.input."$(variable_name_to_attribute_key "${key#INPUT_}")"="$(variable_2_attribute_value "$key")"; done
-printenv | grep '^STATE_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.state.before."$(variable_name_to_attribute_key "${key#STATE_}")"="$(variable_2_attribute_value "$key")"; done
+printenv | grep '^INPUT_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.input."$(variable_name_2_attribute_key "${key#INPUT_}")"="$(variable_2_attribute_value "$key")"; done
+printenv | grep '^STATE_' | cut -d '=' -f 1 | while read -r key; do otel_span_attribute_typed $span_handle string github.actions.step.state.before."$(variable_name_2_attribute_key "${key#STATE_}")"="$(variable_2_attribute_value "$key")"; done
 [ -z "${GITHUB_ACTION_PATH:-}" ] || ! [ -d "$GITHUB_ACTION_PATH" ] || _OTEL_GITHUB_STEP_ACTION_TYPE=composite/"$_OTEL_GITHUB_STEP_ACTION_TYPE"
 otel_span_attribute_typed $span_handle string github.actions.action.type="$_OTEL_GITHUB_STEP_ACTION_TYPE"
 otel_span_attribute_typed $span_handle string github.actions.action.name="$GITHUB_ACTION_REPOSITORY"
@@ -28,8 +28,8 @@ otel_span_attribute_typed $span_handle string github.actions.action.ref="$GITHUB
 otel_span_activate "$span_handle"
 otel_observe "$_OTEL_GITHUB_STEP_AGENT_INJECTION_FUNCTION" "$@"
 exit_code="$?"
-cat "$GITHUB_STATE" | while read -r kvp; do otel_span_attribute_typed $span_handle string github.actions.step.state.after."$(variable_name_to_attribute_key "${kvp%%=*}")"="${kvp#*=}"; done
-cat "$GITHUB_OUTPUT" | while read -r kvp; do otel_span_attribute_typed $span_handle string github.actions.step.output."$(variable_name_to_attribute_key "${kvp%%=*}")"="${kvp#*=}"; done
+cat "$GITHUB_STATE" | while read -r kvp; do otel_span_attribute_typed $span_handle string github.actions.step.state.after."$(variable_name_2_attribute_key "${kvp%%=*}")"="${kvp#*=}"; done
+cat "$GITHUB_OUTPUT" | while read -r kvp; do otel_span_attribute_typed $span_handle string github.actions.step.output."$(variable_name_2_attribute_key "${kvp%%=*}")"="${kvp#*=}"; done
 if [ "$exit_code" != 0 ]; then
   otel_span_attribute_typed $span_handle string github.actions.step.conclusion=failure
   otel_span_error "$span_handle"
