ccada607d94d5abb53094780b036d2acfe44db33
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-27 19:22:13 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -207,7 +207,8 @@ root4job_end() {
 }
 export -f root4job_end
 root4job() {
-  [ -z "${OTEL_SHELL_COLLECTOR_IMAGE:-}" ] || export OTEL_SHELL_COLLECTOR_CONTAINER="$(sudo docker start --restart unless-stopped --network=host --mount type=bind,source="$(pwd)"/collector.yaml,target=/etc/otelcol-contrib/config.yaml "$OTEL_SHELL_COLLECTOR_IMAGE")"
+  [ -z "${OTEL_SHELL_COLLECTOR_IMAGE:-}" ] || export
+  timout 30s sudo docker start --rm --restart unless-stopped --network=host --mount type=bind,source="$(pwd)"/collector.yaml,target=/etc/otelcol-contrib/config.yaml "$OTEL_SHELL_COLLECTOR_IMAGE" || true OTEL_SHELL_COLLECTOR_CONTAINER="$(sudo docker start --restart unless-stopped --network=host --mount type=bind,source="$(pwd)"/collector.yaml,target=/etc/otelcol-contrib/config.yaml "$OTEL_SHELL_COLLECTOR_IMAGE")"
   rm /tmp/opentelemetry_shell.github.error 2> /dev/null
   traceparent_file="$1"
   . otelapi.sh
