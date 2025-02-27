44aad9515aaa56f9c7cd815c3dda4679b9362d34
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-02-27 21:01:10 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -23,7 +23,7 @@ echo "log_file=$log_file" >> "$GITHUB_STATE"
 
 # install dependencies
 . ../shared/github.sh
-bash -e ../shared/install.sh
+. ../shared/install.sh
 
 # configure collector if required
 if [ "$INPUT_COLLECTOR" = true ] || ([ "$INPUT_COLLECTOR" = auto ] && ([ -n "${OTEL_EXPORTER_OTLP_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_LOGS_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_METRICS_HEADERS:-}" ] || [ -n "${OTEL_EXPORTER_OTLP_TRACES_HEADERS:-}" ] || [ "$INPUT_SECRETS_TO_REDACT" != '{}' ])); then
