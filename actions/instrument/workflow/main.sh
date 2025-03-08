6346933f5b5c9442f027738a4653b429108224e6
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-08 17:09:35 +0100

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -37,6 +37,9 @@ _otel_resource_attributes_process() {
   _otel_resource_attribute string github.actions.workflow.ref="$(jq < "$workflow_json" -r .repository.owner.login)"/"$(jq < "$workflow_json" -r .repository.name)"/"$(jq < "$workflow_json" -r .path)"@/refs/heads/"$(jq < "$workflow_json" -r .head_branch)"
   _otel_resource_attribute string github.actions.workflow.sha="$(jq < "$workflow_json" -r .head_sha)"
 }
+_otel_resource_attributes_custom() {
+  _otel_resource_attribute string telemetry.sdk.language=github
+}
 workflow_run_dir="$(mktemp -d)"
 gh_artifact_download "$INPUT_WORKFLOW_RUN_ID" "$INPUT_WORKFLOW_RUN_ATTEMPT" opentelemetry_workflow_run_"$INPUT_WORKFLOW_RUN_ATTEMPT" "$workflow_run_dir" || true
 if [ -r "$workflow_run_dir"/traceparent ]; then export OTEL_ID_GENERATOR_OVERRIDE_TRACEPARENT="$(cat "$workflow_run_dir"/traceparent)"; fi
