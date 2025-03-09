c05b3d55e059d08101ab2eeb1848ac24a8d87fb8
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-09 17:54:24 +0100

    Update agent.instrumentation.java.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
@@ -1,7 +1,7 @@
 #!/bin/false
 
 _otel_inject_java() {
-  JAVA_TOOL_OPTIONS=/opt/opentelemetry_shell/opentelemetry-javaagent.jar _otel_call "$@"
+  JAVA_TOOL_OPTIONS=-javagent:/opt/opentelemetry_shell/opentelemetry-javaagent.jar _otel_call "$@"
 }
 
 _otel_alias_prepend java _otel_inject_java
