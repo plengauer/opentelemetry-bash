33bf32599eb415bd343c433f54afe66b8b25c426
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-09 15:00:34 +0100

    Update index.js

--- demos/injection_deep_node/index.js
+++ demos/injection_deep_node/index.js
@@ -3,10 +3,10 @@ const options = {
   hostname: 'example.com',
   port: 443,
   path: '/',
-  method: 'GET',
-  headers: {
-    Host: 'example.com'
-  }
+  method: 'GET'//,
+//  headers: {
+//    Host: 'example.com'
+//  }
 };
 https.request(options, response => {
   response.on('data', function (chunk) {});
