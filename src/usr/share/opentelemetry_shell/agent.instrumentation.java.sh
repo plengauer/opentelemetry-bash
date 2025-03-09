67ad540f5cd73832e697879a618dc39ff1a7fedc
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-09 18:01:17 +0100

    Update agent.instrumentation.java.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
@@ -1,6 +1,8 @@
 #!/bin/false
 
 _otel_inject_java() {
+  # TODO injection into subprocesses
+  # TODO activate context from env vars
   JAVA_TOOL_OPTIONS=-javagent:/opt/opentelemetry_shell/opentelemetry-javaagent.jar _otel_call "$@"
 }
 
