928ab700d23b62cf0394b8c96779faf26f8e8f21
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-10 17:39:23 +0100

    Remove obsolete safety check for docker actions in GitHub with dashes in inputs

--- src/usr/share/opentelemetry_shell/agent.instrumentation.docker.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.docker.sh
@@ -29,6 +29,18 @@ _otel_is_docker_image_injected() {
   "$executable" run --rm --entrypoint which "$image" otel.sh 1> /dev/null 2> /dev/null
 }
 
+_otel_is_docker_image_github_input_injectable() {
+  local executable="$1"
+  local image="$2"
+  ! \[ "$GITHUB_ACTIONS" = true ] || ! \printenv | \cut -d = -f 1 | \grep -E '^INPUT_' | \grep -q - || "$executable" run --rm --entrypoint which "$image" bash 1> /dev/null 2> /dev/null
+}
+
+_otel_resolve_docker_image_shell() {
+  local executable="$1"
+  local image="$2"
+  \[ "$GITHUB_ACTIONS" = true ] && \printenv | \cut -d = -f 1 | \grep -E '^INPUT_' | \grep -q - && \echo bash || \echo sh
+}
+
 _otel_inject_docker_args() {
   # docker command
   local executable="$1"
@@ -67,7 +79,7 @@ _otel_inject_docker_args() {
   if \[ "$#" = 0 ]; then return 0; fi
   # extract image
   local image="$1"
-  if _otel_is_docker_image_injectable "$executable" "$image" && ! _otel_is_docker_image_injected "$executable" "$image" && ( ! \[ "$GITHUB_ACTIONS" = true ] || ! \printenv | \cut -d = -f 1 | \grep -E '^INPUT_' | \grep -q - ); then
+  if _otel_is_docker_image_injectable "$executable" "$image" && ! _otel_is_docker_image_injected "$executable" "$image" && _otel_is_docker_image_github_input_injectable "$executable" "$image"; then
     \echo -n ' '; _otel_escape_args --env TRACEPARENT="$TRACEPARENT" --env TRACESTATE="$TRACESTATE"
     for file in $(\dpkg -L opentelemetry-shell | \grep -E '^/usr/bin/'); do \echo -n ' '; _otel_escape_args --mount type=bind,source="$file",target="$file",readonly; done
     \echo -n ' '; _otel_escape_args --mount type=bind,source=/usr/share/opentelemetry_shell,target=/usr/share/opentelemetry_shell
@@ -83,7 +95,7 @@ _otel_inject_docker_args() {
     if \[ -z "${docker_network:-}" && [ "$GITHUB_ACTIONS" = true ]; then
       \echo -n ' '; _otel_escape_args --network host
     fi
-    \echo -n ' '; _otel_escape_args --entrypoint /bin/sh
+    \echo -n ' '; _otel_escape_args --entrypoint "$(_otel_resolve_docker_image_shell "$executable" "$image")"
     \echo -n ' '; _otel_escape_arg "$1"; shift
     \echo -n ' '; _otel_escape_args -c '. otel.sh
 eval _otel_inject "$(_otel_escape_args "$@")"' sh
