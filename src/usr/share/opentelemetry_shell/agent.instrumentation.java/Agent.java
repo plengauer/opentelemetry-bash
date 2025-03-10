2b76152ad0928f13a3baaba24a0f972e5a4cadcb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-10 17:15:18 +0100

    Update Agent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/Agent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/Agent.java
@@ -1,7 +1,15 @@
 import java.lang.instrument.Instrumentation;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.TraceFlags;
+import io.opentelemetry.api.trace.TraceState;
+import io.opentelemetry.context.Context;
 
 public class Agent {
     public static void premain(String args, Instrumentation instrumentation) {
-      
+        String traceparent = System.getenv("TRACEPARENT");
+        if (traceparent == null) return;
+        String[] parts = traceparent.split("-");
+        Context.root().with(SpanContext.create(parts[1], parts[2], TraceFlags.fromHex(parts[3]), TraceState.getDefault())).makeCurrent();
     }
 }
