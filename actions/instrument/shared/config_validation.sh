e6bf2a7e85c4144b5827da16a4b66d6756df7481
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-11 16:56:42 +0100

    Fix config simplification with conflicting exporters in GitHub actions

--- actions/instrument/shared/config_validation.sh
+++ actions/instrument/shared/config_validation.sh
@@ -9,18 +9,15 @@ if [ "${OTEL_TRACES_EXPORTER:-otlp}" != otlp ] && [ "${OTEL_TRACES_EXPORTER:-otl
   echo "::error ::OpenTelemetry for GitHub actions only supports otlp exporters ($OTEL_TRACES_EXPORTER). For other exporters, pipe the data through a collector outside of GitHub to translate the data to a different protocol." && false
 fi
 if [ -z "${OTEL_EXPORTER_OTLP_LOGS_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ] && [ -z "${OTEL_LOGS_EXPORTER:-}" ]; then
-  export OTEL_LOGS_EXPORTER=console
-  export OTEL_SHELL_SDK_OUTPUT_REDIRECT="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}"
+  export OTEL_LOGS_EXPORTER=none
   echo "::notice ::OpenTelemetry for GitHub actions has no logs export configured. Consult the documentation for instructions."
 fi
 if [ -z "${OTEL_EXPORTER_OTLP_METRICS_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ] && [ -z "${OTEL_METRICS_EXPORTER:-}" ]; then
-  export OTEL_METRICS_EXPORTER=console
-  export OTEL_SHELL_SDK_OUTPUT_REDIRECT="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}"
+  export OTEL_METRICS_EXPORTER=none
   echo "::notice ::OpenTelemetry for GitHub actions has no metrics export configured. Consult the documentation for instructions."
 fi
 if [ -z "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-}}" ] && [ -z "${OTEL_TRACES_EXPORTER:-}" ]; then
-  export OTEL_TRACES_EXPORTER=console
-  export OTEL_SHELL_SDK_OUTPUT_REDIRECT="${OTEL_SHELL_SDK_OUTPUT_REDIRECT:-/dev/null}"
+  export OTEL_TRACES_EXPORTER=none
   echo "::notice ::OpenTelemetry for GitHub actions has no traces export configured. Consult the documentation for instructions."
 fi
 if [ "${OTEL_LOGS_EXPORTER:-}" = console ] || [ "${OTEL_METRICS_EXPORTER:-}" = console ] || [ "${OTEL_TRACES_EXPORTER:-}" = console ]; then
