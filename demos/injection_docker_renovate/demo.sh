152602be1f25f80ffc63019fce41fcb882c9c2b0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-11 11:31:21 +0100

    Fix docker renovate demo

--- demos/injection_docker_renovate/demo.sh
+++ demos/injection_docker_renovate/demo.sh
@@ -1,3 +1,3 @@
 export RENOVATE_TOKEN="$GITHUB_TOKEN"
 . otel.sh
-sudo -E docker run --env RENOVATE_TOKEN renovate/renovate --dry-run --rm plengauer/opentelemetry-bash
+sudo -E docker run --env RENOVATE_TOKEN renovate/renovate --network host --dry-run --rm plengauer/opentelemetry-bash
