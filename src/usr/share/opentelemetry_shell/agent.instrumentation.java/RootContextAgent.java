443be85cae87355993ba8546d33ddf6e7ce8b5fa
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-14 22:35:53 +0100

    Update RootContextAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/RootContextAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/RootContextAgent.java
@@ -1,5 +1,6 @@
 import java.lang.instrument.Instrumentation;
 import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.Span;
 import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.SpanContext;
 import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.TraceFlags;
 import io.opentelemetry.javaagent.shaded.io.opentelemetry.api.trace.TraceState;
@@ -10,6 +11,6 @@ public class RootContextAgent {
         String traceparent = System.getenv("TRACEPARENT");
         if (traceparent == null) return;
         String[] parts = traceparent.split("-");
-        Context.root().with(SpanContext.create(parts[1], parts[2], TraceFlags.fromHex(parts[3], 0), TraceState.getDefault())).makeCurrent();
+        Context.current().with(Span.wrap(SpanContext.create(parts[1], parts[2], TraceFlags.fromHex(parts[3], 0), TraceState.getDefault()))).makeCurrent();
     }
 }
