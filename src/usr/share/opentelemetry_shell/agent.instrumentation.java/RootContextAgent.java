8c12a9ceab147616a94c28e9649a788fcfe2f2de
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-14 16:25:33 +0100

    Update RootContextAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/RootContextAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/RootContextAgent.java
@@ -10,6 +10,6 @@ public class RootContextAgent {
         String traceparent = System.getenv("TRACEPARENT");
         if (traceparent == null) return;
         String[] parts = traceparent.split("-");
-        Context.root().with(SpanContext.create(parts[1], parts[2], TraceFlags.fromHex(parts[3]), TraceState.getDefault())).makeCurrent();
+        Context.root().with(SpanContext.create(parts[1], parts[2], TraceFlags.fromHex(parts[3], 0), TraceState.getDefault())).makeCurrent();
     }
 }
