ec2e624349fd987d2f4bb565c9ca8172215185f0
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-15 16:29:48 +0100

    Update demo.sh

--- demos/injection_deep_gradle/demo.sh
+++ demos/injection_deep_gradle/demo.sh
@@ -1,5 +1,5 @@
 export GRADLE_HOME=/opt/gradle/latest
-export PATH="$GRADLE_HOME"/bin:"$PATH"
+export PATH="$(pwd)":"$GRADLE_HOME"/bin:"$PATH"
 export OTEL_SHELL_CONFIG_INJECT_DEEP=TRUE
 . otel.sh
-gradle init <<< /dev/null
+gradlew build
