f10e77f6d7fdfa05a456ad17a8d020788c86dd2e
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-03-30 00:27:06 +0100

    Update agent.sh

--- src/usr/share/opentelemetry_shell/agent.sh
+++ src/usr/share/opentelemetry_shell/agent.sh
@@ -447,22 +447,10 @@ command() {
 
 _otel_inject() {
   if _otel_string_contains "$1" /; then
-    local path="$1"; shift
-    local command="$(\echo "$path" | \rev | \cut -d / -f 1 | \rev)"
-    set -- "$command" "$@"
-    if ! \[ "$(\which "$command")" = "$path" ]; then
-      local PATH="$(\readlink -f "$path" | \rev | \cut -d / -f 2- | \rev):$PATH"
-      _otel_auto_instrument "$_otel_shell_auto_instrumentation_hint"
-    fi
-  fi
-  \eval "$(_otel_escape_args "$@")"
-}
-
-_otel_inject() {
-  if _otel_string_contains "$1" /; then
     local path="$1"
     if ! \which "${path##*/}" 1> /dev/null 2> /dev/null; then # in case its an absolute command that is not on the path at all, we need to make sure it is to have proper shebang resolution and the resulting instrumentation on hand
-      PATH=""${path%/*}":$PATH" _otel_auto_instrument "$_otel_shell_auto_instrumentation_hint"
+      local PATH="${path%/*}:$PATH"
+      _otel_auto_instrument "$_otel_shell_auto_instrumentation_hint"
     fi
     local instrumentation="$(_otel_resolve_alias "${path##*/}")"
     if \[ -n "$instrumentation" ]; then
