f8305389462604cc6b7ab8df2c770136d59c24bb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-04-05 19:24:02 +0200

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -301,4 +301,4 @@ done | sed 's/\t/ /g' | while read -r TRACEPARENT job_id step_number step_conclu
 done
 
 otel_shutdown
-while [ "$(pgrep -f /opt/opentelemetry_shell/ | wc -l)" -gt 0 ]; then sleep 1; fi
+while [ "$(pgrep -f /opt/opentelemetry_shell/ | wc -l)" -gt 0 ]; do sleep 1; done
